7a86929d27874b12e873bbd908dbfc0353b0c068
kevincheng96 <kevincheng96@hotmail.com>
2021-11-01 23:11:38 -0700

    Add yarn to project and scripts for testing/building

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('solidity-coverage')
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
