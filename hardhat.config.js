1230acac974eb149fdadd517102fbb6a6b5141a3
Antonina Norair <antonina.norair@gmail.com>
2021-11-10 15:34:58 -0800

    Add https://github.com/cgewecke/hardhat-gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
-require('solidity-coverage')
+require('solidity-coverage');
+require('hardhat-gas-reporter');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,5 +19,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    coinmarketcap: "b52b18a2-d44f-4646-9949-0eb0e9c68574",
+    // denominated in gwei
+    gasPrice: 200 
+  }
 };
