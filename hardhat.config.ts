f6087bc99a72041b20aba73700db322151a3c20e
Antonina Norair <antonina.norair@gmail.com>
2021-11-11 11:35:10 -0800

    Small readability fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 export default {
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
