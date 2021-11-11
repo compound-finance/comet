fe47bce8903d407c372161403abc90ab2a75182a
Kevin Le-Hsiu Cheng <kevincheng96@hotmail.com>
2021-11-11 11:10:36 -0800

    Use typescript for project and install Typechain plugin (#3)
    This patch sets up typescript support for the project and installs the Typechain plugin. The Typechain plugin generates typed wrappers for smart contracts for easier test writing.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
-require('solidity-coverage')
+import { task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers'
+import '@typechain/hardhat'
+import 'solidity-coverage';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -17,6 +20,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+export default {
+  solidity: '0.8.4',
+  typechain: {
+    outDir: 'build/types',
+    target: 'ethers-v5',
+  },
 };
