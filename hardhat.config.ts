2db6abc546662692bb910d36aac94667951a6b71
Jared Flatow <jflatow@gmail.com>
2021-11-11 15:07:18 -0800

    Cleanup whitespace, scripts, and README v2.5 -> Comet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,18 +5,10 @@ import '@typechain/hardhat'
 import 'solidity-coverage';
 import 'hardhat-gas-reporter';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
+  for (const account of await hre.ethers.getSigners())
     console.log(account.address);
-  }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -35,8 +27,7 @@ export default {
     enabled: (process.env.REPORT_GAS) ? true : false,
     currency: 'USD',
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-    // denominated in gwei
-    gasPrice: 200 
+    gasPrice: 200, // gwei
   },
   typechain: {
     outDir: 'build/types',
