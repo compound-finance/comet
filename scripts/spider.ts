850ea8fe87b81410efaf98af5f13f1f66c60a28d
kevincheng96 <kevincheng96@hotmail.com>
2021-11-11 21:49:51 -0800

    Implement initial web expansion

--- scripts/spider.ts
+++ scripts/spider.ts
@@ -1,6 +1,23 @@
 import { ethers } from 'hardhat';
 import axios from 'axios';
 
+const PROVIDER = new ethers.providers.InfuraProvider;
+
+interface ContractNode {
+  name: string,
+  contractName: string,
+  // abi,
+  address: string,
+  children: ContractNode[],
+}
+
+async function main() {
+
+  let web = await expand('0x4b0181102a0112a2ef11abee5563bb4a3176c9d7');
+
+  console.log(web);
+}
+
 async function getAbi(address: string): Promise<string> {
   return await axios.get('https://api.etherscan.io/api?module=contract', {
     params: {
@@ -17,12 +34,42 @@ async function getAbi(address: string): Promise<string> {
     })
 }
 
-async function main() {
-  const abi = await getAbi('0x4b0181102a0112a2ef11abee5563bb4a3176c9d7');
-  const provider = new ethers.providers.InfuraProvider;
-  const contract = new ethers.Contract('0x4b0181102a0112a2ef11abee5563bb4a3176c9d7', abi, provider);
+async function getContractName(address: string): Promise<string> {
+  return await axios.get('https://api.etherscan.io/api?module=contract', {
+    params: {
+      address,
+      action: 'getsourcecode',
+      apikey: process.env.ETHERSCAN_KEY,
+    }
+  }).then((response) => {
+    console.log(response.data.result[0]);
+    return response.data.result[0].ContractName;
+  })
+    .catch((error) => {
+      console.log(error);
+    })
+}
 
-  console.log(await contract.name());
+// DFS expansion starting from root contract
+async function expand(startingAddress: string): Promise<ContractNode> {
+  let contractNode: ContractNode;
+  const abi = await getAbi(startingAddress);
+  const contractName = await getContractName(startingAddress);
+  const contract = new ethers.Contract(startingAddress, abi, PROVIDER);
+  const name = await contract.name();
+  let children = [];
+  try {
+    // TODO: Add more rules here for fetching dependent contracts.
+    // One idea is to have a list of rules that are iterated over. Each
+    // rule can add more children to the `children` list.
+    const implementationAddress = await contract.implementation();
+    children.push(await expand(implementationAddress));
+  } catch {
+    // do nothing
+    console.log('no such field for ' + contractName);
+  }
+  contractNode = {name, contractName, address: startingAddress, children}
+  return contractNode;
 }
 
 // We recommend this pattern to be able to use async/await everywhere
