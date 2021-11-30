b1bd48a96c2df01075509d6a2df93a1a03a0471d
Scott <scott@Scotts-MacBook-Pro.local>
2021-11-30 09:19:51 -0800

    simplify config

--- hardhat-forked.config.ts
+++ hardhat-forked.config.ts
@@ -1,18 +1,5 @@
-import { task } from 'hardhat/config';
-import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-ethers'
-import '@typechain/hardhat'
-import 'solidity-coverage';
-import 'hardhat-gas-reporter';
+// TODO: import config from hardhat.config.ts; copy all properties
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  for (const account of await hre.ethers.getSigners())
-    console.log(account.address);
-});
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 export default {
   networks: {
     hardhat: {
@@ -21,24 +8,5 @@ export default {
         blockNumber: 11095000
       }
     }
-  },
-  solidity: {
-    version: '0.8.4',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
-    },
-  },
-  gasReporter: {
-    enabled: (process.env.REPORT_GAS) ? true : false,
-    currency: 'USD',
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-    gasPrice: 200, // gwei
-  },
-  typechain: {
-    outDir: 'build/types',
-    target: 'ethers-v5',
   },
 };
