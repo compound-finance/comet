51a54fb75f7154249eeb21c44687a29e83b64252
Scott Silver <silversh@gmail.com>
2021-12-01 14:05:14 -0800

    more tabs to spaces

--- contracts/Protocol.sol
+++ contracts/Protocol.sol
@@ -8,23 +8,23 @@ interface ConfigInterface {
 }
 
 contract Protocol {
-	ConfigInterface immutable config;
+    ConfigInterface immutable config;
 
-	event Values(uint targetReserves, uint borrowMin);
+    event Values(uint targetReserves, uint borrowMin);
 
-	constructor(address config_) {
-		config = ConfigInterface(config_);
-	}
+    constructor(address config_) {
+        config = ConfigInterface(config_);
+    }
 
-	function getData() public returns (uint, uint) {
-		// Ignore success here
-		(bool success1, bytes memory data1) = address(config).call(abi.encodePacked(config.targetReserves.selector));
-		// Ignore success here
-		(bool success2, bytes memory data2) = address(config).call(abi.encodePacked(config.borrowMin.selector));
-		
-		uint targetReserves = abi.decode(data1, (uint));
-		uint borrowMin = abi.decode(data2, (uint));
-		emit Values(targetReserves, borrowMin);
-		return (targetReserves, borrowMin);
-	}
+    function getData() public returns (uint, uint) {
+        // Ignore success here
+        (bool success1, bytes memory data1) = address(config).call(abi.encodePacked(config.targetReserves.selector));
+        // Ignore success here
+        (bool success2, bytes memory data2) = address(config).call(abi.encodePacked(config.borrowMin.selector));
+        
+        uint targetReserves = abi.decode(data1, (uint));
+        uint borrowMin = abi.decode(data2, (uint));
+        emit Values(targetReserves, borrowMin);
+        return (targetReserves, borrowMin);
+    }
 }
