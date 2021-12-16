0f6da5a957bd4e11f60fca858d63feaf746bab49
Jared Flatow <jflatow@gmail.com>
2021-12-15 16:05:29 -0800

    Working through CometContext / BalanceConstraint

--- test/scen2/Scenario.ts
+++ test/scen2/Scenario.ts
@@ -18,11 +18,15 @@ export class World {
   }
 }
 
+// A solution modifies a given context and world in a way that satisfies a constraint.
 export type Solution<T> = (T, World) => Promise<T>;
 
+// A constraint is capable of producing solutions for a context and world *like* the ones provided.
+// A constraint can also check a given context and world to see if they *actually* satisfy it.
+// Note: `solve` and `check` are expected to treat the context and world as immutable.
 export interface Constraint<T> {
-  solve(requirements: object, world: World): Promise<Solution<T>[]>;
-  check(requirements: object, world: World): Promise<void>;
+  solve(requirements: object, context: T, world: World): Promise<Solution<T>[]>;
+  check(requirements: object, context: T, world: World): Promise<void>;
 }
 
 export type Property<T> = (context: T, world: World) => Promise<any>;
