aadcbd25b1016b9c4b6ade368f1927d34a9ecb83
scott-silver <silversh@gmail.com>
2021-12-17 11:44:54 -0800

    move scenario configuration into hardhat.config.ts (#32)
    * move scenario configuration into hardhat.config.ts
    
    * remove rebase artifacts

---
+++ plugins/scenario/type-extensions.ts
@@ -0,0 +1,13 @@
+import "hardhat/types/config";
+import { ScenarioConfig } from "./types";
+
+declare module "hardhat/types/config" {
+  interface HardhatUserConfig {
+    // optional?
+    scenario: ScenarioConfig;
+  }
+
+  interface HardhatConfig {
+    scenario: ScenarioConfig;
+  }
+}
--- plugins/scenario/worker/BootstrapWorker.js
+++ plugins/scenario/worker/BootstrapWorker.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { workerData } = require('worker_threads');
- 
+
 // Note: this is bootstrap code to run `worker.ts` from a worker thread that only knows nodejs at birth
 require('ts-node').register();
 let { run } = require(path.resolve(__dirname, './Worker.ts'));
