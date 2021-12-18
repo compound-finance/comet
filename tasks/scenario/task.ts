e9efc2c7f05ca3c24a7796f8595328bc4bcf6faa
Geoff Hayes <hayesgm@gmail.com>
2021-12-17 17:22:28 -0800

    Add Bases to CLI and Scenario Runner (#44)
    This patch adds and passes bases through from the CLI, to the Runner and back to Reporting. We are currently doing a pairwise combination (every base against every scenario), but we could easily add in base-filtering for scenarios. You can specify the bases you want to run by the cli by doing `--bases development,goerli` or just `--bases goerli`. Everything is working pretty well right now.

--- tasks/scenario/task.ts
+++ tasks/scenario/task.ts
@@ -1,8 +1,22 @@
 import { task } from 'hardhat/config';
 import { run } from '../../plugins/scenario/worker/Parent';
 import "../../plugins/scenario/type-extensions";
+import { ForkSpec } from '../../plugins/scenario/Runner';
 
 task("scenario", "Runs scenario tests")
-  .setAction(async (_taskArgs, env) => {
-    await run(env.config.scenario);
+  .addOptionalParam("bases", "Bases to run on [defaults to all]")
+  .setAction(async (taskArgs, env) => {
+    let bases: ForkSpec[] = env.config.scenario.bases;
+    if (taskArgs.bases) {
+      let baseMap = Object.fromEntries(env.config.scenario.bases.map((base) => [base.name, base]));
+      bases = taskArgs.bases.split(',').map((baseName) => {
+        let base = baseMap[baseName];
+        if (!base) {
+          throw new Error(`Unknown base: ${baseName}`);
+        }
+        return base;
+      });
+    }
+
+    await run(env.config.scenario, bases);
   });
