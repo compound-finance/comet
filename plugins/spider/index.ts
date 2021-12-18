c91a0a8ec1677cf6ff388256fa0382365e6ab9c8
Scott Silver <silversh@gmail.com>
2021-12-18 08:41:02 -0800

    restore newline

--- plugins/spider/index.ts
+++ plugins/spider/index.ts
@@ -150,6 +150,7 @@ async function expand({
   writeToCache = false
 }: ExpandParameters): Promise<ContractNode> {
   if (address === '0x0000000000000000000000000000000000000000') return null;
+
   const loadedContract = await loadContractConfig({network, hre, address, writeToCache});
   const key = Object.keys(loadedContract.contracts)[0]; // TODO: assert contracts length is 1
   const abi = loadedContract.contracts[key].abi;
