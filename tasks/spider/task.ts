02b2942c64bb4dce7b38f854166564e345e37df5
scott-silver <silversh@gmail.com>
2021-12-19 06:56:12 -0800

    add spider --clean flag (#47)
    * add spider --clean flag
    
    * delete newline
    
    * update README

--- tasks/spider/task.ts
+++ tasks/spider/task.ts
@@ -1,7 +1,24 @@
 import { task } from 'hardhat/config';
 import { pullConfigs } from '../../plugins/spider';
+import { execSync } from "child_process";
+
+async function deleteSpiderArtifacts() {
+  [
+    "rm -rf deployments/*/cache",
+    "rm deployments/*/config.json",
+    "rm deployments/*/proxies.json"
+  ].forEach(async (command) => {
+    console.log(command);
+    execSync(command);
+  });
+}
 
 task('spider', 'Use Spider method to pull in contract configs')
-  .setAction(async (taskArgs, hre) => {
-    await pullConfigs(hre);
+  .addFlag("clean", "Deletes spider artifacts")
+  .setAction(async ({clean}, hre) => {
+    if (clean) {
+      await deleteSpiderArtifacts();
+    } else {
+      await pullConfigs(hre);
+    }
   });
