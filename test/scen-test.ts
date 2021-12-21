ae850934cc1c65ce50e31457edea665606931300
Jared Flatow <jflatow@gmail.com>
2021-12-21 13:59:56 -0800

    Add scen-test.ts test for scenario compilation

---
+++ test/scen-test.ts
@@ -0,0 +1,15 @@
+/* Check basic scenario compilation */
+
+import { Runner } from '../plugins/scenario/Runner'
+import { CometContext } from '../scenario/Context'
+
+const scenarios = [];
+new Runner({
+  bases: [
+    {
+      name: "development"
+    },
+  ],
+}).run(scenarios, (...args) => console.log('Result', args))
+  .then(r => { /* console.trace(r) */ })
+  .catch(e => { throw(e) });
