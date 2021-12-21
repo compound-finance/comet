a60a62ceb4ca4c64d4f8939d86d95092d86bb6db
Antonina Norair <antonina.norair@gmail.com>
2021-12-21 15:08:10 -0800

    Fix address for now

--- scenario/SimpleScenario.ts
+++ scenario/SimpleScenario.ts
@@ -2,7 +2,7 @@ import { scenario } from './Context';
 import { expect } from 'chai';
 
 scenario.only("my scenario", {}, async ({players}, world) => {
-  expect(await players()).to.eql(["0x29e31E1eE143a76039F00860d3Bd25804357f0b2"]);
+  expect(await players()).to.eql(["0x29e31E1eE143a76039F00860d3Bd25804357f0b3"]);
 });
 
 scenario("scen 2", {}, async (context, world) => {
