a554487562edb81e6b2074ab5c54060dbfeb0e8a
Scott Silver <silversh@gmail.com>
2021-12-29 11:22:35 -0800

    add memoized utils; memoize loadEtherscanContract and hreForBase

---
+++ shared/memoize.ts
@@ -0,0 +1,51 @@
+interface MemoizeOptions {
+  debug: boolean;
+}
+
+export function memoize<T extends any[], U>(
+  fn: (...args: T) => U,
+  { debug }: MemoizeOptions = { debug: false }
+) {
+  const cache = {};
+
+  return function(...args: T): U {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      if (debug) {
+        console.log(`Returning from ${fn.name} cache: ${key}`);
+      }
+      return cache[key]
+    } else {
+      if (debug) {
+        console.log(`Populating ${fn.name} cache for: ${key}`);
+      }
+      const result = fn.apply(undefined, arguments);
+      cache[key] = result;
+      return result;
+    }
+  }
+}
+
+export function memoizeAsync<T extends any[], U>(
+  fn: (...args: T) => Promise<U>,
+  { debug }: MemoizeOptions = { debug: false }
+) {
+  const cache = {};
+
+  return async function(...args: T): Promise<U> {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      if (debug) {
+        console.log(`Returning from ${fn.name} cache: ${key}`);
+      }
+      return cache[key]
+    } else {
+      if (debug) {
+        console.log(`Populating ${fn.name} cache for: ${key}`);
+      }
+      const result = await fn.apply(undefined, arguments);
+      cache[key] = result;
+      return result;
+    }
+  }
+}
