f31f88529b8e2bf902ffe5c1f53df5e94dac5b06
Antonina Norair <antonina.norair@gmail.com>
2022-01-04 10:49:09 -0800

    Fix formatting

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -2,60 +2,58 @@
 pragma solidity ^0.8.0;
 
 contract Comet {
+    struct Configuration {
+        address governor;
+        address priceOracle;
+        address baseToken;
+        address[] collateralAssets;
 
-	struct Configuration {
-		address governor;
-	    address priceOracle;
-	    address baseToken;
-		address[] collateralAssets;
+        // mapping(address => uint) borrowCollateralFactor;
+        // mapping(address => uint) liquidateCollateralFactor;
+        // mapping(address => uint) liquidationPenalty;
+        // mapping(address => uint) storeFrontDiscountFactor;
+        // mapping(address => uint) supplyCap;
 
-	    // mapping(address => uint) borrowCollateralFactor;
-	    // mapping(address => uint) liquidateCollateralFactor;
-	    // mapping(address => uint) liquidationPenalty;
-	    // mapping(address => uint) storeFrontDiscountFactor;
-	    // mapping(address => uint) supplyCap;
+        uint256 targetReserves;
+        uint256 absorbTip;
+        uint256 absorbBaseGas;
+        uint256 borrowMin;
+        uint256 baseTrackingSupplySpeed;
+        uint256 baseTrackingBorrowSpeed;
+        uint256 kink;
+        uint256 interestRateSlopeLow;
+        uint256 interestRateSlopeHigh;
+        uint256 interestRateBase;
+        uint256 reserveRate;
+    }
 
-		uint targetReserves;
-	    uint absorbTip;
-	    uint absorbBaseGas;
-	    uint borrowMin;
+    // Configuration constants
+    address public immutable governor;
+    address public immutable priceOracle;
+    address public immutable baseToken;
 
-	    uint baseTrackingSupplySpeed;
-	    uint baseTrackingBorrowSpeed;
-	    uint kink;
-	    uint interestRateSlopeLow;
-	    uint interestRateSlopeHigh;
-	    uint interestRateBase;
-	    uint reserveRate;
-	}
+    // Immutable ??
+    address[] public collateralAssets;
+    mapping(address => uint256) public borrowCollateralFactor;
+    mapping(address => uint256) public liquidateCollateralFactor;
+    mapping(address => uint256) public liquidationPenalty;
+    mapping(address => uint256) public storeFrontDiscountFactor;
+    mapping(address => uint256) public supplyCap;
 
-	// Configuration constants
-	address immutable public governor;
-	address immutable public priceOracle;
-	address immutable public baseToken;
+    uint256 public immutable targetReserves;
+    uint256 public immutable absorbTip;
+    uint256 public immutable absorbBaseGas;
+    uint256 public immutable borrowMin;
 
-	// Immutable ??
-	address[] public collateralAssets;
-	mapping(address => uint) public borrowCollateralFactor;
-	mapping(address => uint) public liquidateCollateralFactor;
-	mapping(address => uint) public liquidationPenalty;
-	mapping(address => uint) public storeFrontDiscountFactor;
-	mapping(address => uint) public supplyCap;
+    uint256 public immutable baseTrackingSupplySpeed;
+    uint256 public immutable baseTrackingBorrowSpeed;
+    uint256 public immutable kink;
+    uint256 public immutable interestRateSlopeLow;
+    uint256 public immutable interestRateSlopeHigh;
+    uint256 public immutable interestRateBase;
+    uint256 public immutable reserveRate;
 
-	uint immutable public targetReserves;
-	uint immutable public absorbTip;
-	uint immutable public absorbBaseGas;
-	uint immutable public borrowMin;
-
-	uint immutable public baseTrackingSupplySpeed;
-	uint immutable public baseTrackingBorrowSpeed;
-	uint immutable public kink;
-	uint immutable public interestRateSlopeLow;
-	uint immutable public interestRateSlopeHigh;
-	uint immutable public interestRateBase;
-	uint immutable public reserveRate;
-
-	// Storage
+    // Storage
 
     // 512 bits total = 2 slots
     struct Totals {
@@ -91,46 +89,44 @@ contract Comet {
     mapping(address => Asset) public assets;
 
     // 256 bits total
-	struct UserCollateral {
-		uint128 amount;
-		uint128 trackingIndex;
-	}
-	// asset => user => collateral data
-	mapping(address => mapping(address => UserCollateral)) public collateral;
+    struct UserCollateral {
+        uint128 amount;
+        uint128 trackingIndex;
+    }
+    // asset => user => collateral data
+    mapping(address => mapping(address => UserCollateral)) public collateral;
 
     mapping(address => uint256) public userNonces;
 
     uint256 public constant FACTOR = 1e18;
 
     constructor(Configuration memory config) {
-		// Set configuration variables
-		governor = config.governor;
-	    priceOracle = config.priceOracle;
-	    baseToken = config.baseToken;
+        // Set configuration variables
+        governor = config.governor;
+        priceOracle = config.priceOracle;
+        baseToken = config.baseToken;
         collateralAssets = config.collateralAssets;
-		targetReserves = config.targetReserves;
-	    absorbTip = config.absorbTip;
-	    absorbBaseGas = config.absorbBaseGas;
+        targetReserves = config.targetReserves;
+        absorbTip = config.absorbTip;
+        absorbBaseGas = config.absorbBaseGas;
         borrowMin = config.borrowMin;
-		baseTrackingSupplySpeed = config.baseTrackingSupplySpeed;
-	    baseTrackingBorrowSpeed = config.baseTrackingBorrowSpeed;
-	    kink = config.kink;
-	    interestRateSlopeLow = config.interestRateSlopeLow;
-	    interestRateSlopeHigh = config.interestRateSlopeHigh;
-	    interestRateBase = config.interestRateBase;
-	    reserveRate = config.reserveRate;
+        baseTrackingSupplySpeed = config.baseTrackingSupplySpeed;
+        baseTrackingBorrowSpeed = config.baseTrackingBorrowSpeed;
+        kink = config.kink;
+        interestRateSlopeLow = config.interestRateSlopeLow;
+        interestRateSlopeHigh = config.interestRateSlopeHigh;
+        interestRateBase = config.interestRateBase;
+        reserveRate = config.reserveRate;
 
         totals = Totals({
-			lastAccrualTime: uint40(block.timestamp),
-			baseSupplyIndex: uint64(FACTOR),
-			baseBorrowIndex: uint64(FACTOR),
-			trackingSupplyIndex: uint96(FACTOR),
-			trackingBorrowIndex: uint96(FACTOR),
-
+            lastAccrualTime: uint40(block.timestamp),
+            baseSupplyIndex: uint64(FACTOR),
+            baseBorrowIndex: uint64(FACTOR),
+            trackingSupplyIndex: uint96(FACTOR),
+            trackingBorrowIndex: uint96(FACTOR),
             totalSupplyBase: 0,
             totalBorrowBase: 0,
             pauseFlags: 0
         });
     }
-
 }
