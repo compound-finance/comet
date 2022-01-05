3c68121a6822b6042c797b9b93a7594ef0088e1b
Antonina Norair <antonina.norair@gmail.com>
2022-01-04 16:34:01 -0800

    Delete even more

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -13,39 +13,10 @@ contract Comet {
     address public immutable priceOracle;
     address public immutable baseToken;
 
-    // Storage
-    uint256 public constant FACTOR = 1e18;
-
-    // 512 bits total = 2 slots
-    struct Totals {
-        // 1st slot
-        uint96 trackingSupplyIndex;
-        uint96 trackingBorrowIndex;
-        uint64 baseSupplyIndex;
-        // 2nd slot
-        uint64 baseBorrowIndex;
-        uint72 totalSupplyBase;
-        uint72 totalBorrowBase;
-        uint40 lastAccrualTime;
-        uint8 pauseFlags;
-    }
-    Totals public totals;
-
     constructor(Configuration memory config) {
         // Set configuration variables
         governor = config.governor;
         priceOracle = config.priceOracle;
         baseToken = config.baseToken;
-
-        totals = Totals({
-            lastAccrualTime: uint40(block.timestamp),
-            baseSupplyIndex: uint64(FACTOR),
-            baseBorrowIndex: uint64(FACTOR),
-            trackingSupplyIndex: uint96(FACTOR),
-            trackingBorrowIndex: uint96(FACTOR),
-            totalSupplyBase: 0,
-            totalBorrowBase: 0,
-            pauseFlags: 0
-        });
     }
 }
