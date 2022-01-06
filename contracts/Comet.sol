d2c1a24a56f42c01a7046309369cfc51bfb06971
scott-silver <silversh@gmail.com>
2022-01-06 10:50:22 -0800

    add Comet#allow function, unit tests, scenario tests (#89)
    * add Comet#allow function, tests
    
    * update ContractMap type; s/comet/Comet
    
    * update goerli address
    
    * deploy to fuji
    
    * review feedback
    
    * run prettier
    
    * replace zero addresses in unit tests

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -13,10 +13,21 @@ contract Comet {
     address public immutable priceOracle;
     address public immutable baseToken;
 
+    // Storage
+    mapping(address => mapping(address => bool)) public isAllowed;
+
     constructor(Configuration memory config) {
         // Set configuration variables
         governor = config.governor;
         priceOracle = config.priceOracle;
         baseToken = config.baseToken;
+    }
+
+    function allow(address manager, bool _isAllowed) external {
+      allowInternal(msg.sender, manager, _isAllowed);
+    }
+
+    function allowInternal(address owner, address manager, bool _isAllowed) internal {
+      isAllowed[owner][manager] = _isAllowed;
     }
 }
--- scenario/context/CometActor.ts
+++ scenario/context/CometActor.ts
@@ -1,15 +1,22 @@
 import { Signer } from 'ethers';
+import { Comet } from '../../build/types';
 
 export default class CometActor {
   signer: Signer;
   address: string;
+  cometContract: Comet;
 
-  constructor(signer, address) {
+  constructor(signer: Signer, address: string, cometContract: Comet) {
     this.signer = signer;
     this.address = address;
+    this.cometContract = cometContract;
   }
 
   async getEthBalance() {
     return this.signer.getBalance();
+  }
+
+  async allow(manager: CometActor, isAllowed: boolean) {
+    await (await this.cometContract.connect(this.signer).allow(manager.address, isAllowed)).wait();
   }
 }
