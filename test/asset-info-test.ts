23888ba2581705f1071b0f049a80956fff831f2b
Jared Flatow <jflatow@gmail.com>
2022-02-04 14:10:28 -0800

    Enforce collateral factor constraints

--- test/asset-info-test.ts
+++ test/asset-info-test.ts
@@ -1,9 +1,7 @@
-import { Comet, ethers, expect, exp, makeProtocol, wait } from './helpers';
+import { Comet, ethers, expect, exp, makeProtocol, wait, ONE } from './helpers';
 
-const FACTOR = exp(1, 18);
-
-describe('Asset Info', function () {
-  it('Should properly initialize Comet protocol', async () => {
+describe('asset info', function () {
+  it('initializes protocol', async () => {
     const { comet, tokens } = await makeProtocol({
       assets: {
         USDC: {},
@@ -21,21 +19,21 @@ describe('Asset Info', function () {
 
     const assetInfo00 = await comet.getAssetInfo(0);
     expect(assetInfo00.asset).to.be.equal(tokens['ASSET1'].address);
-    expect(assetInfo00.borrowCollateralFactor).to.equal(FACTOR);
-    expect(assetInfo00.liquidateCollateralFactor).to.equal(FACTOR);
+    expect(assetInfo00.borrowCollateralFactor).to.equal(ONE - exp(1, 14));
+    expect(assetInfo00.liquidateCollateralFactor).to.equal(ONE);
 
     const assetInfo01 = await comet.getAssetInfo(1);
     expect(assetInfo01.asset).to.be.equal(tokens['ASSET2'].address);
-    expect(assetInfo01.borrowCollateralFactor).to.equal(FACTOR);
-    expect(assetInfo01.liquidateCollateralFactor).to.equal(FACTOR);
+    expect(assetInfo01.borrowCollateralFactor).to.equal(ONE - exp(1, 14));
+    expect(assetInfo01.liquidateCollateralFactor).to.equal(ONE);
 
     const assetInfo02 = await comet.getAssetInfo(2);
     expect(assetInfo02.asset).to.be.equal(tokens['ASSET3'].address);
-    expect(assetInfo02.borrowCollateralFactor).to.equal(FACTOR);
-    expect(assetInfo02.liquidateCollateralFactor).to.equal(FACTOR);
+    expect(assetInfo02.borrowCollateralFactor).to.equal(ONE - exp(1, 14));
+    expect(assetInfo02.liquidateCollateralFactor).to.equal(ONE);
   });
 
-  it('Should fail if too many assets are passed', async () => {
+  it('reverts if too many assets are passed', async () => {
     await expect(
       makeProtocol({
         assets: {
@@ -62,44 +60,35 @@ describe('Asset Info', function () {
     ).to.be.revertedWith('too many asset configs');
   });
 
-  it('Should revert if index is greater that numAssets', async () => {
+  it('reverts if index is greater that numAssets', async () => {
     const { comet } = await makeProtocol();
     await expect(comet.getAssetInfo(3)).to.be.revertedWith('asset info not found');
   });
 
-  // XXX assets and addresses needed for spider?
-  // it('Should get valid assets', async () => {
-  //   const { comet } = await makeProtocol({
-  //     assets: {
-  //       USDC: {},
-  //       ASSET1: {},
-  //       ASSET2: {},
-  //     },
-  //   });
-  //   const assetInfo00 = await comet.getAssetInfo(0);
-  //   const assetInfo01 = await comet.getAssetInfo(1);
-  //   const assets = await comet.assets();
-  //   expect(assets[0].asset).to.be.equal(assetInfo00.asset);
-  //   expect(assets[0].borrowCollateralFactor).to.be.equal(assetInfo00.borrowCollateralFactor);
-  //   expect(assets[0].liquidateCollateralFactor).to.be.equal(assetInfo00.liquidateCollateralFactor);
+  it('reverts if collateral factors are out of range', async () => {
+    await expect(makeProtocol({
+      assets: {
+        USDC: {},
+        ASSET1: {borrowCF: exp(0.9, 18), liquidateCF: exp(0.9, 18)},
+        ASSET2: {},
+      },
+    })).to.be.revertedWith("borrow CF must be < liquidate CF");
 
-  //   expect(assets[1].asset).to.be.equal(assetInfo01.asset);
-  //   expect(assets[1].borrowCollateralFactor).to.be.equal(assetInfo01.borrowCollateralFactor);
-  //   expect(assets[1].liquidateCollateralFactor).to.be.equal(assetInfo01.liquidateCollateralFactor);
-  // });
+    // check descaled factors
+    await expect(makeProtocol({
+      assets: {
+        USDC: {},
+        ASSET1: {borrowCF: exp(0.9, 18), liquidateCF: exp(0.9, 18) + 1n},
+        ASSET2: {},
+      },
+    })).to.be.revertedWith("borrow CF must be < liquidate CF");
 
-  // it('Should get valid asset addresses', async () => {
-  //   const { comet } = await makeProtocol({
-  //     assets: {
-  //       USDC: {},
-  //       ASSET1: {},
-  //       ASSET2: {},
-  //     },
-  //   });
-  //   const assetInfo00 = await comet.getAssetInfo(0);
-  //   const assetInfo01 = await comet.getAssetInfo(1);
-  //   const assets = await comet.assetAddresses();
-  //   expect(assets[0]).to.be.equal(assetInfo00.asset);
-  //   expect(assets[1]).to.be.equal(assetInfo01.asset);
-  // });
+    await expect(makeProtocol({
+      assets: {
+        USDC: {},
+        ASSET1: {borrowCF: exp(0.99, 18), liquidateCF: exp(1.1, 18)},
+        ASSET2: {},
+      },
+    })).to.be.revertedWith("liquidate CF too high");
+  });
 });
--- test/helpers.ts
+++ test/helpers.ts
@@ -199,7 +199,7 @@ export async function makeProtocol(opts: ProtocolOpts = {}): Promise<Protocol> {
           asset: tokens[symbol].address,
           priceFeed: priceFeeds[symbol].address,
           decimals: dfn(assets[symbol].decimals, 18),
-          borrowCollateralFactor: dfn(config.borrowCF, ONE),
+          borrowCollateralFactor: dfn(config.borrowCF, ONE - 1n),
           liquidateCollateralFactor: dfn(config.liquidateCF, ONE),
           liquidationFactor: dfn(config.liquidationFactor, ONE),
           supplyCap: dfn(config.supplyCap, exp(100, dfn(config.decimals, 18))),
