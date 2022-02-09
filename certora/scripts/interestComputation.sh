65a76a6aa8111d22dafcba4398db8a2e151547ce
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-02-09 15:18:15 +0200

    verify Interest

--- certora/scripts/interestComputation.sh
+++ certora/scripts/interestComputation.sh
@@ -1,4 +1,7 @@
-certoraRun certora/harness/CometHarness.sol \
-    --verify CometHarness:certora/specs/interestComputation.spec  \
-    --solc solc8.10 --staging --optimistic_loop --rule=$1\
+certoraRun certora/harness/CometHarnessInterest.sol \
+    --verify CometHarnessGetters:certora/specs/interestComputation.spec  \
+    --solc solc8.10 \
+    --staging \
+    --optimistic_loop \
+    --rule=$1 \
     --msg "Comet $1"
