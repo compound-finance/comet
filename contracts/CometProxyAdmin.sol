92193703aec3d266d484f3d7488092a306fffa40
kevincheng96 <kevincheng96@hotmail.com>
2022-02-09 11:54:05 -0800

    Set up CometProxyAdmin and get deployment and scenarios working

---
+++ contracts/CometProxyAdmin.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: XXX ADD VALID LICENSE
+pragma solidity ^0.8.11;
+
+import "./CometConfiguration.sol";
+import "./CometStorage.sol";
+import "./TransparentUpgradeableFactoryProxy.sol";
+import "./vendor/proxy/ProxyAdmin.sol";
+
+contract CometProxyAdmin is ProxyAdmin, CometConfiguration {
+    /**
+     * @dev Deploy and upgrade the implementation of the proxy.
+     *
+     * Requirements:
+     *
+     * - This contract must be the admin of `proxy`.
+     */
+    function deployAndUpgrade(TransparentUpgradeableFactoryProxy proxy) public virtual onlyOwner {
+        proxy.deployAndUpgrade();
+    }
+
+    /**
+     * @dev Set the entire configuration param in the configurator.
+     *
+     * Requirements:
+     *
+     * - This contract must be the admin of `proxy`.
+     */
+    function setConfiguration(TransparentUpgradeableFactoryProxy proxy, Configuration memory config) public virtual onlyOwner {
+        proxy.setConfiguration(config);
+    }
+    
+    /**
+     * @dev Set the governor param in the configurator.
+     *
+     * Requirements:
+     *
+     * - This contract must be the admin of `proxy`.
+     */
+    function setGovernor(TransparentUpgradeableFactoryProxy proxy, address governor) public virtual onlyOwner {
+        proxy.setGovernor(governor);
+    }
+}
