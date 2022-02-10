e1a14ac47ac925236c915bff98111b25071854e3
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-02-10 12:14:38 +0200

    creating governance spec

--- certora/scripts/verifyInAsset.sh
+++
@@ -1,7 +0,0 @@
-certoraRun certora/harness/CometHarnessGetters.sol \
-    --verify CometHarnessGetters:certora/specs/InAsset.spec  \
-    --solc solc8.11 \
-    --staging \
-    --optimistic_loop \
-    --settings -useBitVectorTheory \
-    --msg "$1"
