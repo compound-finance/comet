de18bbcb5835eb99fb072b6f0e95d4c7f2b2c2fb
Jared Flatow <jflatow@gmail.com>
2022-02-14 22:22:29 -0800

    Move allow* to ext (not hooked up yet)
    Saves 1.5KB (without delegate fallback)

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -57,12 +57,6 @@ contract Comet is CometMath, CometStorage {
 
     /** General configuration constants **/
 
-    /// @notice The name of this contract
-    string public constant name = "Compound Comet";
-
-    /// @notice The major version of this contract
-    string public constant version = "0";
-
     /// @notice The admin of the protocol
     address public immutable governor;
 
@@ -181,16 +175,6 @@ contract Comet is CometMath, CometStorage {
     /// @dev 365 days * 24 hours * 60 minutes * 60 seconds
     uint64 internal constant SECONDS_PER_YEAR = 31_536_000;
 
-    /// @dev The EIP-712 typehash for the contract's domain
-    bytes32 internal constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-
-    /// @dev The EIP-712 typehash for allowBySig Authorization
-    bytes32 internal constant AUTHORIZATION_TYPEHASH = keccak256("Authorization(address owner,address manager,bool isAllowed,uint256 nonce,uint256 expiry)");
-
-    /// @dev The highest valid value for s in an ECDSA signature pair (0 < s < secp256k1n ÷ 2 + 1)
-    ///  See https://ethereum.github.io/yellowpaper/paper.pdf #307)
-    uint internal constant MAX_VALID_ECDSA_S = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0;
-
     /**
      * @notice Construct a new protocol instance
      * @param config The mapping of initial/constant parameters
@@ -263,7 +247,6 @@ contract Comet is CometMath, CometStorage {
         totalsBasic.trackingSupplyIndex = 0;
         totalsBasic.trackingBorrowIndex = 0;
     }
-
 
     /**
      * @dev Gets the info for an asset or empty, for initialization
@@ -463,56 +446,6 @@ contract Comet is CometMath, CometStorage {
     }
 
     /**
-     * @notice Allow or disallow another address to withdraw, or transfer from the sender
-     * @param manager The account which will be allowed or disallowed
-     * @param isAllowed_ Whether to allow or disallow
-     */
-    function allow(address manager, bool isAllowed_) external {
-        allowInternal(msg.sender, manager, isAllowed_);
-    }
-
-    /**
-     * @dev Stores the flag marking whether the manager is allowed to act on behalf of owner
-     */
-    function allowInternal(address owner, address manager, bool isAllowed_) internal {
-        isAllowed[owner][manager] = isAllowed_;
-    }
-
-    /**
-     * @notice Sets authorization status for a manager via signature from signatory
-     * @param owner The address that signed the signature
-     * @param manager The address to authorize (or rescind authorization from)
-     * @param isAllowed_ Whether to authorize or rescind authorization from manager
-     * @param nonce The next expected nonce value for the signatory
-     * @param expiry Expiration time for the signature
-     * @param v The recovery byte of the signature
-     * @param r Half of the ECDSA signature pair
-     * @param s Half of the ECDSA signature pair
-     */
-    function allowBySig(
-        address owner,
-        address manager,
-        bool isAllowed_,
-        uint256 nonce,
-        uint256 expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external {
-        require(uint256(s) <= MAX_VALID_ECDSA_S, "invalid value: s");
-        // v ∈ {27, 28} (source: https://ethereum.github.io/yellowpaper/paper.pdf #308)
-        require(v == 27 || v == 28, "invalid value: v");
-        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), keccak256(bytes(version)), block.chainid, address(this)));
-        bytes32 structHash = keccak256(abi.encode(AUTHORIZATION_TYPEHASH, owner, manager, isAllowed_, nonce, expiry));
-        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-        address signatory = ecrecover(digest, v, r, s);
-        require(owner == signatory, "owner is not signatory");
-        require(nonce == userNonce[signatory]++, "invalid nonce");
-        require(block.timestamp < expiry, "signed transaction expired");
-        allowInternal(signatory, manager, isAllowed_);
-    }
-
-    /**
      * @notice Determine if the manager has permission to act on behalf of the owner
      * @param owner The owner account
      * @param manager The manager account
@@ -525,7 +458,7 @@ contract Comet is CometMath, CometStorage {
     /**
      * @return The current per second supply rate
      */
-    function getSupplyRate() public view returns (uint64) {
+    function getSupplyRate() external view returns (uint64) {
         return getSupplyRateInternal(totalsBasic);
     }
 
@@ -547,7 +480,7 @@ contract Comet is CometMath, CometStorage {
     /**
      * @return The current per second borrow rate
      */
-    function getBorrowRate() public view returns (uint64) {
+    function getBorrowRate() external view returns (uint64) {
         return getBorrowRateInternal(totalsBasic);
     }
 
---
+++ contracts/CometExt.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: XXX ADD VALID LICENSE
+pragma solidity ^0.8.11;
+
+import "./CometStorage.sol";
+
+contract CometExt is CometStorage {
+    /// @notice The name of this contract
+    string public constant name = "Compound Comet";
+
+    /// @notice The major version of this contract
+    string public constant version = "0";
+
+    /// @dev The EIP-712 typehash for the contract's domain
+    bytes32 internal constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+
+    /// @dev The EIP-712 typehash for allowBySig Authorization
+    bytes32 internal constant AUTHORIZATION_TYPEHASH = keccak256("Authorization(address owner,address manager,bool isAllowed,uint256 nonce,uint256 expiry)");
+
+    /// @dev The highest valid value for s in an ECDSA signature pair (0 < s < secp256k1n ÷ 2 + 1)
+    ///  See https://ethereum.github.io/yellowpaper/paper.pdf #307)
+    uint internal constant MAX_VALID_ECDSA_S = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0;
+
+    /**
+     * @notice Allow or disallow another address to withdraw, or transfer from the sender
+     * @param manager The account which will be allowed or disallowed
+     * @param isAllowed_ Whether to allow or disallow
+     */
+    function allow(address manager, bool isAllowed_) external {
+        allowInternal(msg.sender, manager, isAllowed_);
+    }
+
+    /**
+     * @dev Stores the flag marking whether the manager is allowed to act on behalf of owner
+     */
+    function allowInternal(address owner, address manager, bool isAllowed_) internal {
+        isAllowed[owner][manager] = isAllowed_;
+    }
+
+    /**
+     * @notice Sets authorization status for a manager via signature from signatory
+     * @param owner The address that signed the signature
+     * @param manager The address to authorize (or rescind authorization from)
+     * @param isAllowed_ Whether to authorize or rescind authorization from manager
+     * @param nonce The next expected nonce value for the signatory
+     * @param expiry Expiration time for the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function allowBySig(
+        address owner,
+        address manager,
+        bool isAllowed_,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(uint256(s) <= MAX_VALID_ECDSA_S, "invalid value: s");
+        // v ∈ {27, 28} (source: https://ethereum.github.io/yellowpaper/paper.pdf #308)
+        require(v == 27 || v == 28, "invalid value: v");
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), keccak256(bytes(version)), block.chainid, address(this)));
+        bytes32 structHash = keccak256(abi.encode(AUTHORIZATION_TYPEHASH, owner, manager, isAllowed_, nonce, expiry));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        address signatory = ecrecover(digest, v, r, s);
+        require(owner == signatory, "owner is not signatory");
+        require(nonce == userNonce[signatory]++, "invalid nonce");
+        require(block.timestamp < expiry, "signed transaction expired");
+        allowInternal(signatory, manager, isAllowed_);
+    }
+}
