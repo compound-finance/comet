dfa95b536ec6f515b705f3c22ee26248eca869ce
Jared Flatow <jflatow@gmail.com>
2022-02-15 13:55:10 -0800

    Kevin's assembly loop optimization
    Saves >1KB on factory

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -218,9 +218,11 @@ contract Comet is CometMath, CometStorage {
     /**
      * @dev Gets the info for an asset or empty, for initialization
      */
-    function _getAssetConfig(AssetConfig[] memory assetConfigs, uint i) internal pure returns (AssetConfig memory) {
+    function _getAssetConfig(AssetConfig[] memory assetConfigs, uint i) internal pure returns (AssetConfig memory c) {
         if (i < assetConfigs.length)
-            return assetConfigs[i];
+            assembly {
+                c := mload(add(add(assetConfigs, 0x20), mul(i, 0x20)))
+            }
         return AssetConfig({
             word_a: uint256(0),
             word_b: uint256(0)
