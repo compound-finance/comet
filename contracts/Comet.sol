1465a7b8ea1447bbee57071d381e9914e0e68962
kevincheng96 <kevincheng96@hotmail.com>
2022-02-15 11:54:26 -0800

    Use assembly to load from array

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -268,18 +268,23 @@ contract Comet is CometMath, CometStorage {
     /**
      * @dev Gets the info for an asset or empty, for initialization
      */
-    function _getAssetConfig(AssetConfig[] memory assetConfigs, uint i) internal pure returns (AssetConfig memory) {
-        if (i < assetConfigs.length)
-            return assetConfigs[i];
-        return AssetConfig({
-            asset: address(0),
-            priceFeed: address(0),
-            decimals: uint8(0),
-            borrowCollateralFactor: uint64(0),
-            liquidateCollateralFactor: uint64(0),
-            liquidationFactor: uint64(0),
-            supplyCap: uint128(0)
-        });
+    function _getAssetConfig(AssetConfig[] memory assetConfigs, uint i) internal pure returns (AssetConfig memory c) {
+        if (i < assetConfigs.length) {
+            assembly {
+                c := mload(add(add(assetConfigs, 0x20), mul(i, 0x20)))
+            }
+            // return assetConfigs[i];
+        } else {
+            return AssetConfig({
+                asset: address(0),
+                priceFeed: address(0),
+                decimals: uint8(0),
+                borrowCollateralFactor: uint64(0),
+                liquidateCollateralFactor: uint64(0),
+                liquidationFactor: uint64(0),
+                supplyCap: uint128(0)
+            });
+        }
     }
 
     /**
