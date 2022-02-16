16ec59ce689ec1f1b56c22bcbfe55f83631408da
Jared Flatow <jflatow@gmail.com>
2022-02-15 16:31:08 -0800

    Other cleanup / reductions
    Internalize true constants, in which case we can keep 'true widths' for free
    
    Saves ~200B

--- contracts/test/CometHarness.sol
+++ contracts/test/CometHarness.sol
@@ -8,27 +8,35 @@ contract CometHarness is Comet {
 
     constructor(Configuration memory config) Comet(config) {}
 
+    function baseIndexScale() external view returns (uint64) {
+        return BASE_INDEX_SCALE;
+    }
+
+    function maxAssets() external view returns (uint8) {
+        return MAX_ASSETS;
+    }
+
     function getNow() override public view returns (uint40) {
         return nowOverride > 0 ? uint40(nowOverride) : super.getNow();
     }
 
-    function setNow(uint now_) public {
+    function setNow(uint now_) external {
         nowOverride = now_;
     }
 
-    function setTotalsBasic(TotalsBasic memory totals) public {
+    function setTotalsBasic(TotalsBasic memory totals) external {
         totalsBasic = totals;
     }
 
-    function setTotalsCollateral(address asset, TotalsCollateral memory totals) public {
+    function setTotalsCollateral(address asset, TotalsCollateral memory totals) external {
         totalsCollateral[asset] = totals;
     }
 
-    function setBasePrincipal(address account, int104 principal) public {
+    function setBasePrincipal(address account, int104 principal) external {
         userBasic[account].principal = principal;
     }
 
-    function setCollateralBalance(address account, address asset, uint128 balance) public {
+    function setCollateralBalance(address account, address asset, uint128 balance) external {
         uint128 oldBalance = userCollateral[account][asset].balance;
         userCollateral[account][asset].balance = balance;
         updateAssetsIn(account, asset, oldBalance, balance);
@@ -39,11 +47,11 @@ contract CometHarness is Comet {
         address asset,
         uint128 initialUserBalance,
         uint128 finalUserBalance
-    ) public {
+    ) external {
         updateAssetsIn(account, asset, initialUserBalance, finalUserBalance);
     }
 
-    function getAssetList(address account) public view returns (address[] memory result) {
+    function getAssetList(address account) external view returns (address[] memory result) {
         uint16 assetsIn = userBasic[account].assetsIn;
 
         uint8 count = 0;
