7329bb57448617a2a1d2292e7192d1007d0b8d42
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-02-20 12:41:11 +0200

    fixing check_update

--- certora/scripts/verifyPause.sh
+++ certora/scripts/verifyPause.sh
@@ -2,6 +2,7 @@ certoraRun certora/harness/CometHarnessGetters.sol \
     --verify CometHarnessGetters:certora/specs/pause.spec  \
     --solc solc8.11 \
     --staging \
+    --rule check_flag_updates \
     --optimistic_loop \
     --settings -useBitVectorTheory \
     --msg "$1"
