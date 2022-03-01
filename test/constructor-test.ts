332a6361e178c3dc8f502fc6855b032a55172ddf
Scott Silver <silversh@gmail.com>
2022-03-01 13:26:26 -0800

    MAX_BASE_DECIMALS test

--- test/constructor-test.ts
+++ test/constructor-test.ts
@@ -1,4 +1,4 @@
-import { Comet, ethers, expect, exp, makeProtocol, wait } from './helpers';
+import { defaultAssets, Comet, ethers, expect, exp, makeProtocol, wait } from './helpers';
 
 describe('constructor', function () {
   it.skip('sets the baseBorrowMin', async function () {
@@ -7,6 +7,18 @@ describe('constructor', function () {
 
   it.skip('verifies asset scales', async function () {
     // XXX
+  });
+
+  it('reverts if base token has more than 18 decimals', async () => {
+    await expect(
+      makeProtocol({
+        assets: {
+          USDC: {
+            decimals: 19,
+          },
+        },
+      })
+    ).to.be.revertedWith("custom error 'BadDecimals()'");
   });
 
   it('reverts if baseTokenPriceFeed does not have 8 decimals', async () => {
