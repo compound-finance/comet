fb05d4a7b29552d29e6cee77549d4482f2c8a17f
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-03-02 15:21:09 +0200

    changing rule name

--- certora/scripts/verifyGlobalCollateralAsset.sh
+++ certora/scripts/verifyGlobalCollateralAsset.sh
@@ -3,6 +3,6 @@ certoraRun certora/harness/CometHarnessWrappers.sol \
     --solc solc8.11 \
     --staging shelly/integrateJohnsBranches \
     --optimistic_loop \
-    --rule  \
+    --rule reversability_of_packing \
     --settings -useBitVectorTheory,-smt_hashingScheme=plainInjectivity,-deleteSMTFile=false,-postProcessCounterExamples=false \
     --msg "$1"
