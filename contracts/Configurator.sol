c8788d1023ee8b916df304f9f21011d4e44d7275
kevincheng96 <kevincheng96@hotmail.com>
2022-03-08 11:49:06 -0800

    Get rid of setConfiguration()

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -41,41 +41,6 @@ contract Configurator is ConfiguratorStorage {
         return newComet;
     }
 
-    // XXX Test this is only callable by an admin
-    // XXX See if there is a cleaner way to do this
-    /// @dev only callable by governor
-    function setConfiguration(Configuration memory config) external {
-        if (msg.sender != admin) revert Unauthorized();
-        Configuration storage _configuratorParams = configuratorParams;
-        _configuratorParams.governor = config.governor;
-        _configuratorParams.pauseGuardian = config.pauseGuardian;
-        _configuratorParams.baseToken = config.baseToken;
-        _configuratorParams.baseTokenPriceFeed = config.baseTokenPriceFeed;
-        _configuratorParams.extensionDelegate = config.extensionDelegate;
-        _configuratorParams.kink = config.kink;
-        _configuratorParams.perYearInterestRateSlopeLow = config.perYearInterestRateSlopeLow;
-        _configuratorParams.perYearInterestRateSlopeHigh = config.perYearInterestRateSlopeHigh;
-        _configuratorParams.perYearInterestRateBase = config.perYearInterestRateBase;
-        _configuratorParams.reserveRate = config.reserveRate;
-        _configuratorParams.trackingIndexScale = config.trackingIndexScale;
-        _configuratorParams.baseTrackingSupplySpeed = config.baseTrackingSupplySpeed;
-        _configuratorParams.baseTrackingBorrowSpeed = config.baseTrackingBorrowSpeed;
-        _configuratorParams.baseMinForRewards = config.baseMinForRewards;
-        _configuratorParams.baseBorrowMin = config.baseBorrowMin;
-        _configuratorParams.targetReserves = config.targetReserves;
-        _configuratorParams.governor = config.governor;
-        _configuratorParams.governor = config.governor;
-
-        // Need to copy using this loop because directly copying of an array of structs is not supported
-        for (uint256 i = 0; i < config.assetConfigs.length; i++) {
-            if (i < _configuratorParams.assetConfigs.length) {
-                _configuratorParams.assetConfigs[i] = config.assetConfigs[i];
-            } else {
-                _configuratorParams.assetConfigs.push(config.assetConfigs[i]);
-            }
-        }
-    }
-
     // XXX Define other setters for setting params
     /// @dev only callable by governor
     function setGovernor(address _governor) external {
