0e16cf5c57b3504d1877d9cec33ac4caa51f6a56
Nurit Dor <57101353+nd-certora@users.noreply.github.com>
2022-03-13 22:09:54 +0200

    script with --rule flag

--- certora/scripts/verifyAbsorbBuyCollateral.sh
+++ certora/scripts/verifyAbsorbBuyCollateral.sh
@@ -1,6 +1,10 @@
+if [[ "$1" ]]
+then
+    RULE="--rule $1"
+fi
 
 certoraRun contracts/CometExt.sol certora/harness/CometHarness.sol certora/harness/SymbolicBaseToken.sol certora/harness/SymbolicAssetTokenA.sol certora/harness/SymbolicAssetTokenB.sol certora/harness/SymbolicPriceOracleA.sol certora/harness/SymbolicPriceOracleB.sol \
-    --verify CometHarness:certora/specs/comet_absorbBuyCollateral.spec  \
+    --verify CometHarness:certora/specs/comet_absorbBuyCollateral.spec  $RULE \
     --link CometHarness:baseToken=SymbolicBaseToken CometHarness:extensionDelegate=CometExt \
     --solc solc8.11 \
     --staging \
@@ -8,4 +12,4 @@ certoraRun contracts/CometExt.sol certora/harness/CometHarness.sol certora/harne
     --settings -enableEqualitySaturation=false,-solver=z3,-smt_usePz3=true,-smt_z3PreprocessorTimeout=2 \
     --solc_args '["--experimental-via-ir"]' \
     --loop_iter 2 \
-    --msg "comet_absorbBuyCollateral $1"
+    --msg "comet_absorbBuyCollateral $RULE "
