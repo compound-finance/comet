e1de2ab5b3116c5996b655701ddb01bcd3998c14
Jared Flatow <jflatow@gmail.com>
2022-03-21 13:59:05 -0700

    Bump to solidity 0.8.13
    Originally to resolve the coverage issue in CI, since they now try to avoid stack depth issues:
    
     https://blog.soliditylang.org/2022/03/16/solidity-0.8.13-release-announcement/
    
    However it didn't help, and now we have a brand new non-instrumented coverage.
    
    Also seems to save ~200 bytes on contract size.

--- contracts/CometExt.sol
+++ contracts/CometExt.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: XXX ADD VALID LICENSE
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.13;
 
 import "./CometCore.sol";
 
--- contracts/test/EvilToken.sol
+++ contracts/test/EvilToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: XXX
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.13;
 
 import "./../ERC20.sol";
 import "./../Comet.sol";
