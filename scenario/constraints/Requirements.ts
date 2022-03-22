c7f9ea1b41bf75cb8dcacd726e2bc81acd8c79a1
kevincheng96 <kevincheng96@hotmail.com>
2022-03-22 14:25:31 -0700

    Update properties in Requirements object

--- scenario/constraints/Requirements.ts
+++ scenario/constraints/Requirements.ts
@@ -1,11 +1,10 @@
 // XXX Define strict types for these objects
 export interface Requirements {
-    tokenBalances?: object, // XXX Balance constraint
+    tokenBalances?: object, // XXX Token balance constraint
     cometBalances?: object, // XXX Comet balance constraint
     upgrade?: boolean, // Modern constraint
     cometConfig?: object, // XXX Modern constraint
     pause?: object, // XXX Pause constraint
     remoteToken?: object, // XXX Remote token constraint
     utilization?: number, // Utilization constraint
-    baseToken?: object, // XXX Base token protocol balance constraint
 };
