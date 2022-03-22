2e9841e734e171a61bee96c92e9867b109823cd7
Geoffrey Hayes <hayesgm@gmail.com>
2022-03-22 16:41:47 -0700

    Upgrade to the newest OZ proxy contracts

--- contracts/vendor/utils/Context.sol
+++ contracts/vendor/utils/Context.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.0;
 
-/*
+/**
  * @dev Provides information about the current execution context, including the
  * sender of the transaction and its data. While these are generally available
  * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with GSN meta-transactions the account sending and
+ * manner, since when dealing with meta-transactions the account sending and
  * paying for execution may not be the actual sender (as far as an application
  * is concerned).
  *
@@ -18,7 +19,6 @@ abstract contract Context {
     }
 
     function _msgData() internal view virtual returns (bytes calldata) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
     }
 }
