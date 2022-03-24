1a0e2ac571be3acaeeadbd0820106618c1d23dde
Scott Silver <scott@compound.finance>
2022-03-23 17:24:58 -0700

    LiquidationScenario cont'd

--- scenario/LiquidationScenario.ts
+++ scenario/LiquidationScenario.ts
@@ -1,19 +1,16 @@
 import { scenario } from './context/CometContext';
-import { expect } from 'chai';
-import { exp } from '../test/helpers';
 import { CometInterface } from '../build/types';
 import CometActor from './context/CometActor';
+import { event, expect } from '../test/helpers';
 
 /*
 invariant:
-((borrowRate / factorScale) * timeElapsed) * (baseBalanceOf * price / baseScale) = -(getBorrowLiquidity)
+((borrowRate / factorScale) * timeElapsed) * (baseBalanceOf * price / baseScale) = -liquidationMargin
 
 isolating for timeElapsed:
-timeElapsed = -borrowLiquidity / (baseBalanceOf * price / baseScale) / (borrowRate / factorScale);
-
-// const timeElapsed = -borrowLiquidity * factorScale * baseScale / (baseBalanceOf * price) / borrowRate; // fudge factor
+timeElapsed = -liquidationMargin / (baseBalanceOf * price / baseScale) / (borrowRate / factorScale);
 */
-async function timeUntilUnderwater({comet, actor}: {comet: CometInterface, actor: CometActor}): Promise<number> {
+async function timeUntilUnderwater({comet, actor, fudgeFactor = 0n}: {comet: CometInterface, actor: CometActor, fudgeFactor?: bigint}): Promise<number> {
   const liquidationMargin = (await (await comet.getLiquidationMargin(actor.address)).toBigInt());
   const baseBalanceOf = (await comet.baseBalanceOf(actor.address)).toBigInt();
   const basePrice = (await comet.getPrice(await comet.baseTokenPriceFeed())).toBigInt();
@@ -21,11 +18,7 @@ async function timeUntilUnderwater({comet, actor}: {comet: CometInterface, actor
   const baseScale = (await comet.baseScale()).toBigInt();
   const factorScale = (await comet.factorScale()).toBigInt();
 
-  // XXX add as parameter
-  const fudgeFactor = 0n;
-
-  // XXX throw error if baseBalanceOf is positive
-
+  // XXX throw error if baseBalanceOf is positive and liquidationMargin is positive
   return Number((-liquidationMargin * factorScale * baseScale /
           (baseBalanceOf * basePrice) /
           borrowRate) + fudgeFactor);
@@ -35,8 +28,8 @@ scenario(
   'Comet#liquidation > isLiquidatable=true for underwater position',
   {
     cometBalances: {
-      albert: { $base: -10000 }, // in units of asset, not wei
-      betty: { $base: 100 } // in units of asset, not wei
+      albert: { $base: -10000 },
+      betty: { $base: 100 }
     },
     utilization: 1
   },
@@ -61,8 +54,8 @@ scenario(
   'Comet#liquidation > prevents liquidation when absord is paused',
   {
     cometBalances: {
-      albert: { $base: -10000 }, // in units of asset, not wei
-      betty: { $base: 100 } // in units of asset, not wei
+      albert: { $base: -10000 },
+      betty: { $base: 100 }
     },
     pause: {
       absorbPaused: true,
@@ -88,12 +81,12 @@ scenario(
   }
 );
 
-scenario.only(
+scenario(
   'Comet#liquidation > allows liquidation of underwater positions',
   {
     cometBalances: {
-      albert: { $base: -10000 }, // in units of asset, not wei
-      betty: { $base: 100 } // in units of asset, not wei
+      albert: { $base: -10000 },
+      betty: { $base: 100 }
     },
     utilization: 1
   },
@@ -127,68 +120,56 @@ scenario.only(
   }
 );
 
-
-
-// XXX liquidation scenarios
-// XXX ensure governor can withdrawCollateral after successful liquidation
-//   'Comet#liquidation > governor can withdraw collateral after',
-/*
-    // console.log(`comet.isLiquidatable(albert.address): ${await comet.isLiquidatable(albert.address)}`);
-    // console.log(`await comet.getLiquidationMargin(): ${await comet.getLiquidationMargin(albert.address)}`);
-    // console.log(`comet.getBorrowLiquidity(albert.address): ${await comet.getBorrowLiquidity(albert.address)}`);
-
-
-
-scenario.only(
-  'Comet#liquidation > governor can withdraw collateral after',
+scenario(
+  'Comet#liquidation > governor can withdraw collateral after successful liquidation',
   {
-    // baseToken: {
-    //   balance: 100e6,
-    // },
+    balances: {
+      albert: { $asset0: .001 }, // low value, to make it easy to source
+    },
     cometBalances: {
-      albert: { $base: -10000 }, // in units of asset, not wei // storage value in Comet
-      betty: { $base: 100}
+      albert: { $base: -10000 },
+      betty: { $base: 100 }
     },
     utilization: 1
   },
   async ({ comet, actors }, world) => {
-    const { albert, betty } = actors;
+    const { albert, betty, admin } = actors;
     const baseToken = await comet.baseToken();
+    const { asset: asset0Address, scale } = await comet.getAssetInfo(0);
 
-    const borrowLiquidity = (await comet.getBorrowLiquidity(albert.address)).toBigInt();
-    const liquidationMargin = (await (await comet.getLiquidationMargin(albert.address)).toBigInt());
-    const baseBalanceOf = (await comet.baseBalanceOf(albert.address)).toBigInt();
-    const price = (await comet.getPrice(await comet.baseTokenPriceFeed())).toBigInt();
-    const borrowRate = (await comet.getBorrowRate()).toBigInt();
-    const baseScale = (await comet.baseScale()).toBigInt();
-    const factorScale = (await comet.factorScale()).toBigInt();
+    const collateralBalance = scale.toBigInt() / 1000n; // .001
 
-    const timeElapsed = timeUntilUnderwater({
-      baseBalanceOf,
-      baseScale,
-      liquidationMargin,
-      borrowRate,
-      factorScale,
-      price,
+    await world.increaseTime(
+      await timeUntilUnderwater({
+        comet,
+        actor: albert
+      })
+    );
+
+    await comet.absorb(betty.address, [albert.address]);
+
+    const txReceipt = await admin.withdrawFrom({
+      src: comet.address,
+      to: admin.address,
+      asset: asset0Address,
+      amount: collateralBalance
     });
 
-    const timeElapsed2 = await timeUntilUnderwater2({
-      comet,
-      actor: albert
+    expect(event({receipt: txReceipt}, 0)).to.deep.equal({
+      Transfer: {
+        from: comet.address,
+        to: admin.address,
+        amount: collateralBalance
+      }
     });
 
-    console.log(`timeElapsed: ${timeElapsed}`);
-    console.log(`timeElapsed2: ${timeElapsed2}`);
-
-    await world.increaseTime(Number(timeElapsed));
-
-    await comet.connect(betty.signer).withdraw(baseToken, 10); // force accrue
-
-    console.log(`comet.isLiquidatable(albert.address): ${await comet.isLiquidatable(albert.address)}`);
-    console.log(`await comet.getLiquidationMargin(): ${await comet.getLiquidationMargin(albert.address)}`);
-    console.log(`comet.getBorrowLiquidity(albert.address): ${await comet.getBorrowLiquidity(albert.address)}`);
-
-    expect(true).to.be.false;
+    expect(event({receipt: txReceipt}, 1)).to.deep.equal({
+      WithdrawCollateral: {
+        src: comet.address,
+        to: admin.address,
+        asset: asset0Address,
+        amount: collateralBalance
+      }
+    });
   }
 );
-*/
