5b891e4cb194cdbbb83612a0a8a76a6f867087d6
kevincheng96 <kevincheng96@hotmail.com>
2022-04-07 22:18:53 -0700

    Remove duplicate test

--- test/allow-test.ts
+++ test/allow-test.ts
@@ -39,23 +39,6 @@ describe('allow', function () {
     expect(await comet.isAllowed(userAddress, managerAddress)).to.be.false;
   });
 
-  it('allows a user to rescind authorization', async () => {
-    const { comet } = await makeProtocol();
-    const [_admin, user, manager] = await ethers.getSigners();
-    const userAddress = user.address;
-    const managerAddress = manager.address;
-
-    const authorizeTx = await comet.connect(user).allow(managerAddress, true);
-    await authorizeTx.wait();
-
-    expect(await comet.isAllowed(userAddress, managerAddress)).to.be.true;
-
-    const rescindTx = await comet.connect(user).allow(managerAddress, false);
-    await rescindTx.wait();
-
-    expect(await comet.isAllowed(userAddress, managerAddress)).to.be.false;
-  });
-
   it('has permission only if the user is allowed or self', async () => {
     // XXX
   });
