35d950039238d542c63f947957913a84ff7ab05c
Scott Silver <scott@compound.finance>
2022-04-07 20:47:16 -0700

    add and remove GovernorSimple admins

---
+++ test/governor-simple-test.ts
@@ -0,0 +1,38 @@
+import { GovernorSimple__factory } from '../build/types';
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+
+describe('GovernorSimple', function () {
+  it('adds a new admin', async () => {
+    const [alice, bob] = await ethers.getSigners();
+    const GovernorSimpleFactory = (await ethers.getContractFactory('GovernorSimple')) as GovernorSimple__factory;
+    const governorSimple = await GovernorSimpleFactory.deploy();
+    await governorSimple.deployed();
+    await governorSimple.initialize(
+      ethers.constants.AddressZero,
+      [alice.address]
+    );
+
+    expect(await governorSimple.isAdmin(bob.address)).to.be.false;
+
+    await governorSimple.connect(alice).addAdmin(bob.address);
+
+    expect(await governorSimple.isAdmin(bob.address)).to.be.true;
+  });
+
+  it('removes an existing admin', async () => {
+    const [alice, bob] = await ethers.getSigners();
+    const GovernorSimpleFactory = (await ethers.getContractFactory('GovernorSimple')) as GovernorSimple__factory;
+    const governorSimple = await GovernorSimpleFactory.deploy();
+    await governorSimple.initialize(
+      ethers.constants.AddressZero,
+      [alice.address, bob.address]
+    );
+
+    expect(await governorSimple.isAdmin(bob.address)).to.be.true;
+
+    await governorSimple.connect(alice).removeAdmin(bob.address);
+
+    expect(await governorSimple.isAdmin(bob.address)).to.be.false;
+  });
+});
