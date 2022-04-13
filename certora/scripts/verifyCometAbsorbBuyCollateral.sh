1760b745110e0129e52ba1b625a7a1febc708dc0
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-04-14 00:46:09 +0300

    adding bash declaration

--- certora/scripts/verifyCometAbsorbBuyCollateral.sh
+++ certora/scripts/verifyCometAbsorbBuyCollateral.sh
@@ -1,16 +1,18 @@
-# if [[ "$1" ]]
-# then
-#     RULE="--rule $1"
-# fi
+#!/bin/bash
+
+if [[ "$1" ]]
+then
+    RULE="--rule $1"
+fi
 
 certoraRun contracts/CometExt.sol certora/harness/CometHarness.sol certora/harness/SymbolicBaseToken.sol certora/harness/SymbolicAssetTokenA.sol certora/harness/SymbolicAssetTokenB.sol certora/harness/SymbolicPriceOracleA.sol certora/harness/SymbolicPriceOracleB.sol \
     --verify CometHarness:certora/specs/cometAbsorbBuyCollateral.spec \
     --link CometHarness:baseToken=SymbolicBaseToken CometHarness:extensionDelegate=CometExt \
     --solc solc8.11 \
     --cloud \
-    --rule $1 \
+    $RULE \
     --optimistic_loop \
     --loop_iter 2 \
     --settings -enableEqualitySaturation=false,-solver=z3,-smt_usePz3=true,-smt_z3PreprocessorTimeout=2 \
     --solc_args '["--experimental-via-ir"]' \
-    --msg "CometHarness:cometAbsorbBuyCollateral.spec $1 "
+    --msg "CometHarness:cometAbsorbBuyCollateral.spec $RULE "
