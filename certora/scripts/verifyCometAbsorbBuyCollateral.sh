3e8a52d7fe74aa11d94a28a7b52ba4748389cbb0
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-04-14 00:43:24 +0300

    trying explicit --rule for CI

--- certora/scripts/verifyCometAbsorbBuyCollateral.sh
+++ certora/scripts/verifyCometAbsorbBuyCollateral.sh
@@ -1,16 +1,16 @@
-if [[ "$1" ]]
-then
-    RULE="--rule $1"
-fi
+# if [[ "$1" ]]
+# then
+#     RULE="--rule $1"
+# fi
 
 certoraRun contracts/CometExt.sol certora/harness/CometHarness.sol certora/harness/SymbolicBaseToken.sol certora/harness/SymbolicAssetTokenA.sol certora/harness/SymbolicAssetTokenB.sol certora/harness/SymbolicPriceOracleA.sol certora/harness/SymbolicPriceOracleB.sol \
     --verify CometHarness:certora/specs/cometAbsorbBuyCollateral.spec \
     --link CometHarness:baseToken=SymbolicBaseToken CometHarness:extensionDelegate=CometExt \
     --solc solc8.11 \
     --cloud \
-    $RULE \
+    --rule $1 \
     --optimistic_loop \
     --loop_iter 2 \
     --settings -enableEqualitySaturation=false,-solver=z3,-smt_usePz3=true,-smt_z3PreprocessorTimeout=2 \
     --solc_args '["--experimental-via-ir"]' \
-    --msg "CometHarness:cometAbsorbBuyCollateral.spec $RULE "
+    --msg "CometHarness:cometAbsorbBuyCollateral.spec $1 "
