ab8febf3c070c891a58fdf2e207485366a4c1a37
kevincheng96 <kevincheng96@hotmail.com>
2022-04-18 15:02:45 -0700

    Check events in pause actions unit tests

--- contracts/CometInterface.sol
+++ contracts/CometInterface.sol
@@ -17,6 +17,10 @@ abstract contract CometInterface is CometCore, ERC20 {
     event TransferCollateral(address indexed from, address indexed to, address indexed asset, uint256 amount);
     event WithdrawCollateral(address indexed src, address indexed to, address indexed asset, uint256 amount);
 
+    event PauseAction(bool supplyPaused, bool transferPaused, bool withdrawPaused, bool absorbPaused, bool buyPaused);
+    event Absorb(address absorber, address borrower, uint104 debtAbsorbed);
+    event BuyCollateral(address buyer, address asset, uint baseAmount, uint collateralAmount);
+
     function allow(address manager, bool isAllowed) virtual external;
     function allowBySig(address owner, address manager, bool isAllowed, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) virtual external;
 
