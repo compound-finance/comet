4cf544ce72125bf1e35ba93ed4960b293c53cfbf
kevincheng96 <kevincheng96@hotmail.com>
2022-04-25 14:22:18 -0700

    Change wording of disclosure

--- contracts/CometCore.sol
+++ contracts/CometCore.sol
@@ -69,7 +69,7 @@ abstract contract CometCore is CometConfiguration, CometStorage, CometMath {
 
     /**
      * @dev The positive present supply balance if positive or the negative borrow balance if negative
-     *  Note This has a hard cap of 2^104/1e18=~20 trillion for assets with 18 decimals.
+     *  Note This will overflow at 2^104/1e18=~20 trillion for assets with 18 decimals.
      */
     function presentValue(int104 principalValue_) internal view returns (int104) {
         if (principalValue_ >= 0) {
@@ -81,7 +81,7 @@ abstract contract CometCore is CometConfiguration, CometStorage, CometMath {
 
     /**
      * @dev The principal amount projected forward by the supply index
-     *  Note This has a hard cap of 2^104/1e18=~20 trillion for assets with 18 decimals.
+     *  Note This will overflow at 2^104/1e18=~20 trillion for assets with 18 decimals.
      */
     function presentValueSupply(uint64 baseSupplyIndex_, uint104 principalValue_) internal pure returns (uint104) {
         return uint104(uint(principalValue_) * baseSupplyIndex_ / BASE_INDEX_SCALE);
@@ -89,7 +89,7 @@ abstract contract CometCore is CometConfiguration, CometStorage, CometMath {
 
     /**
      * @dev The principal amount projected forward by the borrow index
-     *  Note This has a hard cap of 2^104/1e18=~20 trillion for assets with 18 decimals.
+     *  Note This will overflow at 2^104/1e18=~20 trillion for assets with 18 decimals.
      */
     function presentValueBorrow(uint64 baseBorrowIndex_, uint104 principalValue_) internal pure returns (uint104) {
         return uint104(uint(principalValue_) * baseBorrowIndex_ / BASE_INDEX_SCALE);
