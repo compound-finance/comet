d857a3ecb2adc5ae8aec8a0841f031863df9e63e
Scott Silver <scott@compound.finance>
2022-04-26 09:49:02 -0700

    first test

--- contracts/test/Fauceteer.sol
+++ contracts/test/Fauceteer.sol
@@ -8,8 +8,14 @@ contract Fauceteer {
     /// received that asset
     mapping(address => mapping(address => uint)) public lastReceived;
 
+    /* errors */
+    error TransferFailed();
+
     function drip(ERC20 token) public {
+        uint balance = token.balanceOf(address(this));
+        // require(balance > 0, "Fauceteer is empty");
 
+        bool success = ERC20(token).transfer(msg.sender, balance / 10000); // 0.01%
+        if (!success) revert TransferFailed();
     }
-
 }
--- test/fauceteer-test.ts
+++ test/fauceteer-test.ts
@@ -1,19 +1,46 @@
-import { ethers, expect } from './helpers';
+import { ethers, exp, expect } from './helpers';
 import {
   Fauceteer,
-  Fauceteer__factory
+  Fauceteer__factory,
+  FaucetToken,
+  FaucetToken__factory
 } from '../build/types';
 
 async function makeFauceteer() {
   const FauceteerFactory = (await ethers.getContractFactory('Fauceteer')) as Fauceteer__factory;
-  const fauceteer = FauceteerFactory.deploy();
-  return fauceteer;
+  const fauceteer = await FauceteerFactory.deploy() as Fauceteer;
+
+  const FaucetTokenFactory = (await ethers.getContractFactory('FaucetToken')) as FaucetToken__factory;
+
+  return {
+    fauceteer,
+    tokens: {
+      USDC: await FaucetTokenFactory.deploy(1e6, 'USDC', 18, 'USDC')
+    }
+  };
 }
 
 describe.only('Fauceteer', function () {
-  // DELETE
-  it('runs a test', async () => {
-    const fauceteer = await makeFauceteer();
-    expect(true).to.be.true;
+  it('issues a small amount of requested token to requester', async () => {
+    const [_minter, requester] = await ethers.getSigners();
+    const { fauceteer, tokens: { USDC } } = await makeFauceteer();
+    // expect(true).to.be.true;
+    await USDC.allocateTo(fauceteer.address, exp(100, 18));
+
+    expect(await USDC.balanceOf(requester.address)).to.eq(0);
+
+    await fauceteer.connect(requester).drip(USDC.address);
+
+    expect(await USDC.balanceOf(fauceteer.address)).to.eq(99990000000000000000n); // 99.99% of initial balance
+    expect(await USDC.balanceOf(requester.address)).to.eq(10000000000000000n); // .01% of initial balance
+  });
+
+  it.skip('throws an error if transfer fails', async () => {
+
+  });
+
+
+  it.skip('throws an error if it has none of asset', async () => {
+
   });
 });
