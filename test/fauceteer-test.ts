8a54238f598e4679597d8181ed9ec93aee728d79
Scott Silver <scott@compound.finance>
2022-04-26 09:11:59 -0700

    makeFauceteer

--- test/fauceteer-test.ts
+++ test/fauceteer-test.ts
@@ -1,7 +1,19 @@
-import { expect } from './helpers';
+import { ethers, expect } from './helpers';
+import {
+  Fauceteer,
+  Fauceteer__factory
+} from '../build/types';
+
+async function makeFauceteer() {
+  const FauceteerFactory = (await ethers.getContractFactory('Fauceteer')) as Fauceteer__factory;
+  const fauceteer = FauceteerFactory.deploy();
+  return fauceteer;
+}
 
 describe.only('Fauceteer', function () {
+  // DELETE
   it('runs a test', async () => {
+    const fauceteer = await makeFauceteer();
     expect(true).to.be.true;
   });
 });
