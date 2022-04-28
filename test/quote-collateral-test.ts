a4bbfe039d39479ed7bf01724e4da27b3b9f1567
kevincheng96 <kevincheng96@hotmail.com>
2022-04-28 13:27:23 -0700

    Add new test case for when storeFrontPriceFactor is 0

--- test/quote-collateral-test.ts
+++ test/quote-collateral-test.ts
@@ -64,6 +64,43 @@ describe('quoteCollateral', function () {
     expect(q0).to.be.equal(0n);
   });
 
+  it('quotes the collateral at market price when storeFrontPriceFactor is 0%', async () => {
+    const protocol = await makeProtocol({
+      base: 'USDC',
+      storeFrontPriceFactor: exp(0, 18),
+      targetReserves: 100,
+      assets: {
+        USDC: {
+          initial: 1e6,
+          decimals: 6,
+          initialPrice: 1,
+        },
+        COMP: {
+          initial: 1e7,
+          decimals: 18,
+          initialPrice: 200,
+          liquidationFactor: exp(0.6, 18),
+        },
+      }
+    });
+    const { comet, tokens } = protocol;
+    const { USDC, COMP } = tokens;
+
+    const baseAmount = exp(200, 6);
+    const q0 = await comet.quoteCollateral(COMP.address, baseAmount);
+
+    // Store front discount is 0 * (1 - 0.6) = 0 = 0%
+    // Discounted COMP price is 200 * 1 = 200
+    // 200 USDC should give 200 * (1/200) COMP
+    const assetPriceDiscounted = exp(200, 8);
+    const basePrice = exp(1, 8);
+    const assetScale = exp(1, 18);
+    const assetWeiPerUnitBase = assetScale * basePrice / assetPriceDiscounted;
+    const baseScale = exp(1, 6);
+    expect(q0).to.be.equal(assetWeiPerUnitBase * baseAmount / baseScale);
+    expect(q0).to.be.equal(exp(1, 18));
+  });
+
   // Should fail before PR 303
   it('properly calculates price without truncating integer during intermediate calculations', async () => {
     const protocol = await makeProtocol({
