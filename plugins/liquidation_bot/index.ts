e50307ce713920f5836069b74dc9eefe12ce1f93
Scott Silver <scott@compound.finance>
2022-05-02 16:43:33 -0700

    setup deploymentManager

--- plugins/liquidation_bot/index.ts
+++ plugins/liquidation_bot/index.ts
@@ -1,6 +1,32 @@
-type Config = {};
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeploymentManager } from '../../plugins/deployment_manager/DeploymentManager';
 
-function main(config: Config) {
+type Config = {
+  hre: HardhatRuntimeEnvironment
+};
+
+async function main(config: Config) {
+  const { hre } = config;
+  const network = hre.network.name;
+
+  console.log(`network: ${network}`);
+
+  const dm = new DeploymentManager(network, hre, {
+    writeCacheToDisk: false,
+    debug: true,
+    verifyContracts: true,
+  });
+  await dm.spider();
+
+  const contracts = await dm.contracts();
+  const comet = contracts.get('comet');
+
+  console.log(`comet: ${comet.address}`);
+
+
+
+
+
 }
 
 export default main;
--- tasks/liquidation_bot/task.ts
+++ tasks/liquidation_bot/task.ts
@@ -3,7 +3,9 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import main from "../../plugins/liquidation_bot/index";
 
 task('liquidation-bot', 'Runs liquidation bot')
-  .setAction(async ({}, env: HardhatRuntimeEnvironment) => {
+  .setAction(async ({}, hre: HardhatRuntimeEnvironment) => {
     console.log(`liquidation bot running`);
-    main({});
+    await main({
+      hre
+    });
   });
