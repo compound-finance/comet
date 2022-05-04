9492c92d257495ecfaefce1efc77ff43c545062a
Scott Silver <scott@compound.finance>
2022-05-04 09:16:34 -0700

    skip, not only

--- plugins/liquidation_bot/test/liquidation-bot-test.ts
+++ plugins/liquidation_bot/test/liquidation-bot-test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
-describe.only('LiquidationBot', () => {
-  it.only('test', async () => {
+describe.skip('LiquidationBot', () => {
+  it.skip('test', async () => {
     expect(true).to.be.true;
   });
 });
