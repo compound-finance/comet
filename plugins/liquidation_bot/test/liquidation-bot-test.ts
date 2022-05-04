99c85a670de3f8cce1a3f8468f7684aa4e4b5190
Scott Silver <scott@compound.finance>
2022-05-04 15:00:23 -0700

    first test

--- plugins/liquidation_bot/test/liquidation-bot-test.ts
+++ plugins/liquidation_bot/test/liquidation-bot-test.ts
@@ -1,7 +1,17 @@
+import { ethers, BigNumber } from 'ethers';
 import { expect } from 'chai';
+import { loop, Borrower, BorrowerMap } from '../index';
 
-describe.skip('LiquidationBot', () => {
-  it.skip('test', async () => {
-    expect(true).to.be.true;
+describe('LiquidationBot > loop', () => {
+  it.only('returns unchanged borrowerMap when currentBlock <= lastBlockChecked', async () => {
+    const borrowerMap: BorrowerMap = {
+      '0x01': {
+        address: '0x01',
+        liquidationMargin: BigNumber.from(100),
+        lastUpdated: undefined
+      }
+    };
+    const { updatedBorrowerMap } = await loop(0, 0, borrowerMap);
+    expect(updatedBorrowerMap).to.deep.eq(borrowerMap);
   });
 });
