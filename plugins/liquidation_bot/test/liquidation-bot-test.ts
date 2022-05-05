55b65e8cbf88031e93f112bc14454f11b5250bb0
Scott Silver <scott@compound.finance>
2022-05-05 09:02:29 -0700

    add smock; rework first test

--- plugins/liquidation_bot/test/liquidation-bot-test.ts
+++ plugins/liquidation_bot/test/liquidation-bot-test.ts
@@ -1,9 +1,18 @@
-import { ethers, BigNumber } from 'ethers';
-import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { BigNumber } from 'ethers';
+import { expect, use } from 'chai';
 import { loop, Borrower, BorrowerMap } from '../index';
+import { FakeContract, smock } from '@defi-wonderland/smock';
+import { CometInterface } from '../../../build/types';
+
+use(smock.matchers);
 
 describe('LiquidationBot > loop', () => {
   it.only('returns unchanged borrowerMap when currentBlock <= lastBlockChecked', async () => {
+    const [signer] = await ethers.getSigners();
+
+    const fakeComet: FakeContract<CometInterface> = await smock.fake('Comet');
+
     const borrowerMap: BorrowerMap = {
       '0x01': {
         address: '0x01',
@@ -11,7 +20,15 @@ describe('LiquidationBot > loop', () => {
         lastUpdated: undefined
       }
     };
-    const { updatedBorrowerMap } = await loop(0, 0, borrowerMap);
+
+    const { updatedBorrowerMap } = await loop({
+      absorber: signer,
+      comet: fakeComet,
+      currentBlock: 0,
+      lastBlockChecked: 0,
+      borrowerMap
+    });
+
     expect(updatedBorrowerMap).to.deep.eq(borrowerMap);
   });
 });
