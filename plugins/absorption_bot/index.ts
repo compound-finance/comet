b5f887cd71e2a8ca116e7b663724ba1655afdd9f
Scott Silver <scott@compound.finance>
2022-05-06 10:13:03 -0700

    add debug

--- plugins/absorption_bot/index.ts
+++ plugins/absorption_bot/index.ts
@@ -11,27 +11,44 @@ To run: `yarn hardhat absorption-bot --network kovan`
 
 type CometOrMock = Comet | FakeContract<Comet>;
 
+function debug(shouldLog: boolean, message?, ...optionalParams) {
+  if (shouldLog) {
+    console.log(message, optionalParams);
+  }
+}
+
+async function attemptAbsorb(comet: CometOrMock, absorber: SignerWithAddress, targetAddresses: string[], log = false) {
+  try {
+    await comet.connect(absorber).absorb(absorber.address, targetAddresses);
+    debug(log, `Successfully absorbed ${targetAddresses}`);
+  } catch (e) {
+    debug(log, `Failed to absorb ${targetAddresses}`);
+    debug(log, e.message);
+  }
+}
+
 async function getUniqueAddresses(comet: CometOrMock): Promise<Set<string>> {
   const withdrawEvents = await comet.queryFilter(comet.filters.Withdraw());
   return new Set(withdrawEvents.map(event => event.args.src));
 }
 
-export async function absorbLiquidatableBorrowers(comet: CometOrMock, absorber: SignerWithAddress) {
+export async function absorbLiquidatableBorrowers(comet: CometOrMock, absorber: SignerWithAddress, log = false) {
   const uniqueAddresses = await getUniqueAddresses(comet);
+
+  debug(log, `${uniqueAddresses.size} unique addresses found`);
+
   for (const address of uniqueAddresses) {
-    try {
-      const liquidationMargin = await comet.getLiquidationMargin(address);
-      console.log(`${address} liquidation margin=${liquidationMargin}`)
-      if (liquidationMargin.lt(0)) {
-        await comet.connect(absorber).absorb(absorber.address, [address]);
-      }
-    } catch (error) {
-      console.log(error.message)
+    const liquidationMargin = await comet.getLiquidationMargin(address);
+
+    debug(log, `${address} liquidation margin=${liquidationMargin}`);
+
+    if (liquidationMargin.lt(0)) {
+      await attemptAbsorb(comet, absorber, [address], log);
     }
   }
 }
 
-async function main({ hre }) {
+async function main({ hre, debug = true }) {
   const network = hre.network.name;
   const [absorber] = await hre.ethers.getSigners();
 
@@ -46,7 +63,7 @@ async function main({ hre }) {
   const comet = contracts.get('comet') as Comet;
 
   while (true) {
-    await absorbLiquidatableBorrowers(comet, absorber);
+    await absorbLiquidatableBorrowers(comet, absorber, debug);
   }
 }
 
