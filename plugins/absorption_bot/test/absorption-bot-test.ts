bb35b1ca288108e5ae9ed6c896025f45861a7e23
Scott Silver <scott@compound.finance>
2022-05-09 14:37:56 -0700

    rework tests; remove smock

--- plugins/absorption_bot/test/absorption-bot-test.ts
+++ plugins/absorption_bot/test/absorption-bot-test.ts
@@ -1,94 +1,36 @@
-import { ethers } from 'hardhat';
-import { expect, use } from 'chai';
-import { FakeContract, smock } from '@defi-wonderland/smock';
-import { Comet } from '../../../build/types';
 import { absorbLiquidatableBorrowers } from "../index";
-import { BigNumber, Contract, EventFilter } from 'ethers';
+import { expect, exp, makeProtocol } from '../../../test/helpers';
 
-use(smock.matchers);
-
-type MockEvent = {
-  eventSignature: string;
-  args: {
-    src: string;
-    to: string;
-    amount: BigNumber;
-  };
-}
-
-function mockEvent({ src, to, amount }: { src: string, to: string, amount: BigNumber }): MockEvent {
-  return {
-    eventSignature: 'Withdraw(address,address,uint256)',
-    args: { src, to, amount }
-  }
-}
-
-async function mockComet(events = []): Promise<Contract> {
-  // type as any to allow casting to Contract
-  const comet = await smock.fake('Comet') as any;
-
-  comet.queryFilter = (_event: EventFilter) => {
-    return new Promise<any>((resolve, _reject) => {
-      resolve(events);
+describe('Absorption Bot#absorbLiquidatableBorrowers', () => {
+  it('absorbs all liquidatable borrowers', async () => {
+    const { comet, tokens, priceFeeds, users: [absorber, underwaterBorrower] } = await makeProtocol({
+      base: 'USDC',
+      assets: {
+        USDC: { decimals: 6 },
+        COMP: { decimals: 18, initialPrice: 1 },
+      },
     });
-  };
+    const { USDC, COMP } = tokens;
 
-  return comet;
-}
+    await USDC.allocateTo(comet.address, exp(1000, 6));
+    await COMP.allocateTo(underwaterBorrower.address, exp(100, 18));
+    await COMP.connect(underwaterBorrower).approve(comet.address, exp(100, 18));
 
-describe('Absorption Bot', () => {
-  describe('absorbLiquidatableBorrowers', () => {
-    it('queries liquidationMargin for all Withdraw sources', async () => {
-      const [ absorber, user1, user2, user3 ] = await ethers.getSigners();
-      const comet = await mockComet([
-        mockEvent({
-          src: user1.address,
-          to: user2.address,
-          amount: BigNumber.from(1000)
-        }),
-        mockEvent({
-          src: user2.address,
-          to: user3.address,
-          amount: BigNumber.from(1000)
-        }),
-      ]);
+    // user supplies and takes out loan
+    await comet.connect(underwaterBorrower).supply(COMP.address, exp(100, 18));
+    await comet.connect(underwaterBorrower).withdraw(USDC.address, exp(80, 6));
 
-      await absorbLiquidatableBorrowers(comet, absorber);
+    // position becomes liquidatable
+    await priceFeeds['COMP'].setPrice(exp(5,7));
 
-      expect(comet.getLiquidationMargin).to.have.been.calledTwice;
-      expect(comet.getLiquidationMargin.atCall(0)).to.have.been.calledWith(user1.address);
-      expect(comet.getLiquidationMargin.atCall(1)).to.have.been.calledWith(user2.address);
-    });
+    await absorbLiquidatableBorrowers(comet, absorber);
 
-    it('attempts absorb for liquidatable positions', async () => {
-      const [ absorber, solventUser, insolventUser ] = await ethers.getSigners();
-      const comet = await mockComet([
-        mockEvent({
-          src: solventUser.address,
-          to: solventUser.address,
-          amount: BigNumber.from(1000)
-        }),
-        mockEvent({
-          src: insolventUser.address,
-          to: insolventUser.address,
-          amount: BigNumber.from(1000)
-        }),
-      ]);
+    // position has been absorbed
+    expect(await comet.getLiquidationMargin(underwaterBorrower.address)).to.eq(0);
+    expect(await comet.isLiquidatable(underwaterBorrower.address)).to.be.false;
 
-      comet.getLiquidationMargin.whenCalledWith(solventUser.address).returns(100);
-      comet.getLiquidationMargin.whenCalledWith(insolventUser.address).returns(-100);
-
-      await absorbLiquidatableBorrowers(comet, absorber);
-
-      expect(comet.getLiquidationMargin).to.have.been.calledTwice;
-      expect(comet.getLiquidationMargin.atCall(0)).to.have.been.calledWith(solventUser.address);
-      expect(comet.getLiquidationMargin.atCall(1)).to.have.been.calledWith(insolventUser.address);
-
-      expect(comet.absorb).to.have.been.calledOnce;
-      expect(comet.absorb.atCall(0)).to.have.been.calledWith(
-        absorber.address,
-        [insolventUser.address]
-      );
-    });
+    // absorber receives liquidator points
+    expect((await comet.liquidatorPoints(absorber.address)).numAbsorbs).to.eq(1);
+    expect((await comet.liquidatorPoints(absorber.address)).numAbsorbed).to.eq(1);
   });
 });
