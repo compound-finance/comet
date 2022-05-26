f4cb236e5e79395dfa9b1c21bbccae7d6038f323
kevincheng96 <kevincheng96@hotmail.com>
2022-05-26 12:54:00 -0700

    Add logic to Fuji migration

--- deployments/fuji/migrations/1653512197_seed-rewards-with-comp.ts
+++ deployments/fuji/migrations/1653512197_seed-rewards-with-comp.ts
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers';
+import { CometInterface, CometRewards, ERC20, GovernorSimple, ProxyAdmin, SimpleTimelock } from '../../../build/types';
 import { DeploymentManager } from '../../../plugins/deployment_manager/DeploymentManager';
 import { migration } from '../../../plugins/deployment_manager/Migration';
 
@@ -9,7 +11,62 @@ migration<Vars>('1653512197_seed-rewards-with-comp', {
     return {};
   },
   enact: async (deploymentManager: DeploymentManager, vars: Vars) => {
+    let [signer] = await deploymentManager.hre.ethers.getSigners();
 
+    const timelock = await deploymentManager.contract('timelock') as SimpleTimelock;
+    const governor = await deploymentManager.contract('governor') as GovernorSimple;
+    const comet = await deploymentManager.contract('comet') as CometInterface;
+    const rewards = await deploymentManager.contract('rewards') as CometRewards;
+    const COMP = await deploymentManager.contract('COMP') as ERC20;
+
+    console.log('Governor ', governor.address)
+    console.log('Timelock ', timelock.address)
+    console.log('Rewards ', rewards.address)
+
+    const timelockCompBalance = await COMP.balanceOf(timelock.address);
+    console.log('COMP balance of Timelock: ', timelockCompBalance);
+
+    // Steps:
+    // 1. Set reward config in CometRewards.
+    // 2. Send half of the Timelock's COMP to CometRewards.
+    const setRewardConfigCalldata = ethers.utils.defaultAbiCoder.encode(["address", "address"], [comet.address, COMP.address]);
+    const transferCompCalldata = ethers.utils.defaultAbiCoder.encode(["address", "uint"], [rewards.address, timelockCompBalance.div(2)]);
+
+    const governorAsAdmin = governor.connect(signer);
+
+    // Create a new proposal and queue it up. Execution can be done manually or in a third step.
+    let tx = await (await governorAsAdmin.propose(
+      [
+        rewards.address,
+        COMP.address,
+      ],
+      [
+        0,
+        0,
+      ],
+      [
+        "_setRewardConfig(address,address)",
+        "transfer(address,uint256)",
+      ],
+      [
+        setRewardConfigCalldata,
+        transferCompCalldata,
+      ],
+      'Set RewardConfig and transfer COMP to CometRewards')
+    ).wait();
+    let event = tx.events.find(event => event.event === 'ProposalCreated');
+    let [proposalId] = event.args;
+
+    await governorAsAdmin.queue(proposalId);
+
+    console.log(`Created proposal ${proposalId} and queued it. Proposal still needs to be executed.`);
+
+    // XXX create a third step that actually executes the proposal on testnet and logs the results
+    // await governorAsAdmin.execute(proposalId);
+    // // Log out new states to manually verify (helpful to verify via simulation)
+    // console.log("COMP balance of Timelock: ", await COMP.balanceOf(timelock.address));
+    // console.log("COMP balance of CometRewards: ", await COMP.balanceOf(rewards.address));
+    // console.log("RewardConfig: ", await rewards.rewardConfig(comet.address));
   },
   enacted: async (deploymentManager: DeploymentManager) => {
     return false;
