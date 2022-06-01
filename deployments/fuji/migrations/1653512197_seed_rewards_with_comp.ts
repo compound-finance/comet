ca49a027bf718a2e5230a0299fd5fc34991d9da6
kevincheng96 <kevincheng96@hotmail.com>
2022-05-31 21:16:39 -0700

    Remove XXX

--- deployments/fuji/migrations/1653512197_seed_rewards_with_comp.ts
+++ deployments/fuji/migrations/1653512197_seed_rewards_with_comp.ts
@@ -5,7 +5,6 @@ import { migration } from '../../../plugins/deployment_manager/Migration';
 
 interface Vars { };
 
-// XXX implement
 migration<Vars>('1653512197_seed_rewards_with_comp', {
   prepare: async (deploymentManager: DeploymentManager) => {
     return {};
