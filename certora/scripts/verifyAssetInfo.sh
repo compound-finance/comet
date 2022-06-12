01f9158c2e709695d4f81c5865340e1dfe7d7d52
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-06-12 16:22:33 +0300

    sorting verifyAssetInfo

--- certora/scripts/verifyAssetInfo.sh
+++ certora/scripts/verifyAssetInfo.sh
@@ -5,11 +5,11 @@ fi
 
 certoraRun certora/harness/CometHarnessWrappers.sol \
     --verify CometHarnessWrappers:certora/specs/assetInfo.spec  \
-    --solc solc8.11 \
+    --solc solc8.13 \
     --cloud \
+    --disable_auto_cache_key_gen \
     $RULE \
-    --send_only \
     --optimistic_loop \
     --settings -useBitVectorTheory,-smt_hashingScheme=plainInjectivity,-deleteSMTFile=false,-postProcessCounterExamples=false \
     --solc_args '["--experimental-via-ir"]' \
-    --msg "CometHarnessWrappers:assetInfo.spec $RULE"
+    --msg "CometHarnessWrappers:assetInfo.spec rule $RULE"
