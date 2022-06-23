74a9768bd5595fc5dff51cd498bb76bc709f8d7e
kevincheng96 <kevincheng96@hotmail.com>
2022-06-23 11:19:00 -0700

    Create scenario for testing upgrade + initialize governance flow

--- contracts/test/CometModified.sol
+++ contracts/test/CometModified.sol
@@ -20,35 +20,7 @@ contract CometModified is Comet {
         liquidatorPoints[liquidator].numAbsorbs = type(uint32).max;
     }
 
-    /**
-     * @notice Calculate the amount of liquidation margin for account
-     * @param account The address to check margin for
-     * @return The common price quantity of liquidation margin
-     */
-    function getLiquidationMargin(address account) external view returns (int) {
-        uint16 assetsIn = userBasic[account].assetsIn;
-
-        int liquidity = signedMulPrice(
-            presentValue(userBasic[account].principal),
-            getPrice(baseTokenPriceFeed),
-            uint64(baseScale)
-        );
-
-        for (uint8 i = 0; i < numAssets; i++) {
-            if (isInAsset(assetsIn, i)) {
-                AssetInfo memory asset = getAssetInfo(i);
-                uint newAmount = mulPrice(
-                    userCollateral[account][asset.asset].balance,
-                    getPrice(asset.priceFeed),
-                    asset.scale
-                );
-                liquidity += signed256(mulFactor(
-                    newAmount,
-                    asset.liquidateCollateralFactor
-                ));
-            }
-        }
-
-        return liquidity;
+    function newFunction() external pure returns (int) {
+        return 101;
     }
 }
