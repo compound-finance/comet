70a2f85c6c9bfd3a2b3587448ab61e6e95bd1bd4
Kevin Cheng <kevincheng96@hotmail.com>
2022-06-24 10:07:40 -0700

    Fix CometActor/NonceManager forking (#435)
    This PR fixes a bug where the wrong nonces were being used in a new scenario I was trying to write (the one created in this PR).
    
    The root issue is that when a scenario context is forked, the CometActors in the forked context use the signers from the old deployment manager instead of the new deployment manager. This is a problem because we recently started using NonceManager to represent signers in deployment manager. NonceManagers are stateful and therefore can have their nonces get out-of-sync if different NonceManagers are used for the same signer. In the new scenario introduced in this PR, we use deployment manager to deploy a new contract directly in the scenario, which uses the signer from the new deployment manager. Then, when we try to fast execute a proposal, the old signer from the CometContext's CometActor is used, causing a nonce mismatch.
    
    The fix in this PR constructs CometActor using the new deployment manager's signers when forking CometContext.

--- scenario/GovernanceScenario.ts
+++ scenario/GovernanceScenario.ts
@@ -31,3 +31,29 @@ scenario('upgrade Comet implementation and initialize', {}, async ({ comet, conf
   // LiquidatorPoints.numAbsorbs for address ZERO should now be set as UInt32.MAX
   expect((await comet.liquidatorPoints(constants.AddressZero)).numAbsorbs).to.be.equal(2 ** 32 - 1);
 });
+
+scenario('upgrade Comet implementation and call new function', {}, async ({ comet, configurator, proxyAdmin, timelock, actors }, world, context) => {
+  // Deploy new version of Comet Factory
+  const dm = context.deploymentManager;
+  const cometModifiedFactory = await dm.deploy<CometModifiedFactory, CometModifiedFactory__factory, []>(
+    'test/CometModifiedFactory.sol',
+    []
+  );
+
+  // Upgrade Comet implementation
+  let setFactoryCalldata = utils.defaultAbiCoder.encode(["address"], [cometModifiedFactory.address]);
+  let deployAndUpgradeToCalldata = utils.defaultAbiCoder.encode(["address", "address"], [configurator.address, comet.address]);
+  await context.fastGovernanceExecute(
+    [configurator.address, proxyAdmin.address],
+    [0, 0],
+    ["setFactory(address)", "deployAndUpgradeTo(address,address)"],
+    [setFactoryCalldata, deployAndUpgradeToCalldata]
+  );
+
+  const CometModified = await dm.hre.ethers.getContractFactory('CometModified');
+  const modifiedComet = CometModified.attach(comet.address).connect(await dm.getSigner());
+
+  // Call new functions on Comet
+  await modifiedComet.initialize(constants.AddressZero);
+  expect(await modifiedComet.newFunction()).to.be.equal(101n);
+});
