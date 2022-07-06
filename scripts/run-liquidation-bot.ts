a056675684413e1808b66b04188b1c95fd6bce80
Scott Silver <scott@compound.finance>
2022-07-06 13:52:20 -0700

    add liquidator to Kovan roots; add script to package.json; flesh out run-liquidation-bot

--- scripts/run-liquidation-bot.ts
+++ scripts/run-liquidation-bot.ts
@@ -1,42 +1,102 @@
 import hre from 'hardhat';
 import { DeploymentManager } from '../plugins/deployment_manager/DeploymentManager';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import {
   CometInterface,
   Liquidator
 } from '../build/types';
 
-// deploy_liquidator_contract migration?
+// XXX this is Kovan-specific
+const daiPool = {
+  tokenAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+  poolFee: 500,
+  reversedPair: false
+};
 
-// async function getUniqueAddresses(comet: Contract): Promise<Set<string>> {
-//   const withdrawEvents = await comet.queryFilter(comet.filters.Withdraw());
-//   return new Set(withdrawEvents.map(event => event.args.src));
-// }
+async function attemptLiquidation(
+  liquidator: Liquidator,
+  signer: SignerWithAddress,
+  targetAddresses: string[]
+) {
+  try {
+    await liquidator.connect(signer).initFlash({
+      accounts: targetAddresses,
+      pairToken: daiPool.tokenAddress,
+      poolFee: daiPool.poolFee,
+      reversedPair: daiPool.reversedPair
+    });
+    console.log(`Successfully liquidated ${targetAddresses}`);
+  } catch (e) {
+    console.log(`Failed to liquidate ${targetAddresses}`);
+    console.log(e.message);
+  }
+}
+
+async function getUniqueAddresses(comet: CometInterface): Promise<Set<string>> {
+  const withdrawEvents = await comet.queryFilter(comet.filters.Withdraw());
+  return new Set(withdrawEvents.map(event => event.args.src));
+}
+
+export async function absorbLiquidatableBorrowers(
+  comet: CometInterface,
+  liquidator: Liquidator,
+  signer: SignerWithAddress
+) {
+  const uniqueAddresses = await getUniqueAddresses(comet);
+
+  console.log(`${uniqueAddresses.size} unique addresses found`);
+
+  for (const address of uniqueAddresses) {
+    const isLiquidatable = await comet.isLiquidatable(address);
+
+    console.log(`${address} isLiquidatble=${isLiquidatable}`);
+
+    if (isLiquidatable) {
+      await attemptLiquidation(
+        liquidator,
+        signer,
+        [address]
+      );
+    }
+  }
+}
+
+const loopDelay = 5000;
 
 async function main() {
-  const network = hre.network.name;
-  const [absorber] = await hre.ethers.getSigners();
-
-  const dm = new DeploymentManager(network, hre, {
+  const dm = new DeploymentManager(hre.network.name, hre, {
     writeCacheToDisk: false,
     debug: true,
     verifyContracts: true,
   });
   await dm.spider();
 
+  const signer = await dm.getSigner();
   const contracts = await dm.contracts();
   const comet = contracts.get('comet') as CometInterface;
   const liquidator = contracts.get('liquidator') as Liquidator;
 
-  console.log(`liquidator.address: ${liquidator.address}`);
+  let lastBlockNumber: number;
 
-  // while (true) {
-  //   console.log(`loop`);
-  // }
+  while (true) {
+    const currentBlockNumber = await hre.ethers.provider.getBlockNumber();
 
+    console.log(`currentBlockNumber: ${currentBlockNumber}`);
+
+    if (currentBlockNumber !== lastBlockNumber) {
+      lastBlockNumber = currentBlockNumber;
+      await absorbLiquidatableBorrowers(
+        comet,
+        liquidator,
+        signer
+      );
+    } else {
+      console.log(`block already checked; waiting ${loopDelay}ms`);
+      await new Promise(resolve => setTimeout(resolve, loopDelay));
+    }
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
