2f78db4f44cb2232a96cac00287174dd2153b9d7
Scott Silver <scott@compound.finance>
2022-07-07 14:08:34 -0700

    return assets; add test for base balance

--- test/liquidation/liquidation-bot.ts
+++ test/liquidation/liquidation-bot.ts
@@ -14,22 +14,36 @@ describe('Liquidator', function () {
   });
 
   it('Should execute DAI flash swap', async () => {
-    const { comet, liquidator, users: [owner, underwater] } = await makeLiquidatableProtocol();
-
-    // console.log(`BEFORE mockUSDC.balanceOf(owner.address): ${await mockUSDC.balanceOf(owner.address)}`);
+    const { liquidator, users: [owner, underwater] } = await makeLiquidatableProtocol();
 
     const tx = await liquidator.connect(owner).initFlash({
-      // XXX add accounts
       accounts: [underwater.address],
       pairToken: ethers.utils.getAddress(DAI),
       poolFee: 500,
       reversedPair: false,
     });
 
-    // console.log(`AFTER mockUSDC.balanceOf(owner.address): ${await mockUSDC.balanceOf(owner.address)}`);
-
-    // XXX test the liquidating user's balance before and after
-
     expect(tx.hash).to.be.not.null;
+  });
+
+  it('Successful execution should increase base token balance of liquidating user', async () => {
+    const {
+      liquidator,
+      users: [owner, underwater],
+      assets: { usdc }
+    } = await makeLiquidatableProtocol();
+
+    const baseBalanceBefore = await usdc.balanceOf(owner.address);
+
+    await liquidator.connect(owner).initFlash({
+      accounts: [underwater.address],
+      pairToken: ethers.utils.getAddress(DAI),
+      poolFee: 500,
+      reversedPair: false,
+    });
+
+    const baseBalanceAfter = await usdc.balanceOf(owner.address);
+
+    expect(baseBalanceAfter.toNumber()).to.be.greaterThan(baseBalanceBefore.toNumber());
   });
 });
