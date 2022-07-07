97260b3af5cfbf95f5328d643e4b40f37e876fb2
Scott Silver <scott@compound.finance>
2022-07-07 08:42:34 -0700

    delete setup test

--- test/liquidation/liquidation-bot-script.ts
+++ test/liquidation/liquidation-bot-script.ts
@@ -6,11 +6,6 @@ describe('Liquidation Bot', function () {
   before(forkMainnet);
   after(resetHardhatNetwork);
 
-  it('sets up the test', async function () {
-    const { comet, liquidator, users: [signer, underwater] } = await makeLiquidatableProtocol();
-    expect(await comet.isLiquidatable(underwater.address)).to.be.true;
-  });
-
   describe('liquidateUnderwaterBorrowers', function () {
     it('liquidates underwater borrowers', async function () {
       const { comet, liquidator, users: [signer, underwater] } = await makeLiquidatableProtocol();
