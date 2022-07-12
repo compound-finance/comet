393711d6fa7aef412db7b58a8fb6099ad7a0ccbe
Scott Silver <scott@compound.finance>
2022-07-12 16:19:29 -0700

    MIT license for Liquidator.sol

--- contracts/liquidator/Liquidator.sol
+++ contracts/liquidator/Liquidator.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
 import "./vendor/@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol";
