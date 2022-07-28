7d72c2eb084ac7ccd3a609d7962a3485d2abc8b5
kevincheng96 <kevincheng96@hotmail.com>
2022-07-28 14:55:25 -0700

    Rebase on main and move deployments back to migrations

--- deployments/kovan/migrations/1644388553_deploy_kovan.ts
+++ deployments/kovan/migrations/1644388553_deploy_kovan.ts
@@ -5,15 +5,10 @@ import { exp, wait } from '../../../test/helpers';
 import {
   Bulker,
   Bulker__factory,
-  CometInterface,
-  CometRewards,
-  ERC20,
   Fauceteer,
   Fauceteer__factory,
-  GovernorSimple,
   ProxyAdmin,
   ProxyAdmin__factory,
-  SimpleTimelock
 } from '../../../build/types';
 import { Contract } from 'ethers';
 import { debug } from '../../../plugins/deployment_manager/Utils';
@@ -54,8 +49,6 @@ migration<Vars>('1644388553_deploy_kovan', {
     await deploymentManager.spider();
 
     await mintToFauceteer(deploymentManager);
-
-    await seedRewardsWithComp(deploymentManager);
 
     return newRoots;
   },
@@ -251,64 +244,4 @@ async function mintToFauceteer(deploymentManager: DeploymentManager) {
     (signer_) => wait(LINK.connect(signer_).transfer(fauceteerAddress, signerLinkBalance.div(100))) // transfer 1% of total supply
   );
   debug(`LINK.balanceOf(fauceteerAddress): ${await LINK.balanceOf(fauceteerAddress)}`);
-}
-
-async function seedRewardsWithComp(deploymentManager: DeploymentManager) {
-  const { ethers } = deploymentManager.hre;
-
-  const timelock = await deploymentManager.contract('timelock') as SimpleTimelock;
-  const governor = await deploymentManager.contract('governor') as GovernorSimple;
-  const comet = await deploymentManager.contract('comet') as CometInterface;
-  const rewards = await deploymentManager.contract('rewards') as CometRewards;
-  const COMP = await deploymentManager.contract('COMP') as ERC20;
-
-  const timelockCompBalance = await COMP.balanceOf(timelock.address);
-  debug(`COMP balance of Timelock: ${timelockCompBalance}`);
-
-  // Steps:
-  // 1. Set reward config in CometRewards.
-  // 2. Send half of the Timelock's COMP to CometRewards.
-  const setRewardConfigCalldata = ethers.utils.defaultAbiCoder.encode(["address", "address"], [comet.address, COMP.address]);
-  const transferCompCalldata = ethers.utils.defaultAbiCoder.encode(["address", "uint"], [rewards.address, timelockCompBalance.div(2)]);
-
-  // Create a new proposal and queue it up. Execution can be done manually or in a third step.
-  const txn = await deploymentManager.asyncCallWithRetry(
-    async (signer_) => (await governor.connect(signer_).propose(
-      [
-        rewards.address,
-        COMP.address,
-      ],
-      [
-        0,
-        0,
-      ],
-      [
-        "setRewardConfig(address,address)",
-        "transfer(address,uint256)",
-      ],
-      [
-        setRewardConfigCalldata,
-        transferCompCalldata,
-      ],
-      'Set RewardConfig and transfer COMP to CometRewards')
-    ).wait()
-  );
-
-  const event = txn.events.find(event => event.event === 'ProposalCreated');
-  const [proposalId] = event.args;
-
-  await deploymentManager.asyncCallWithRetry(
-    (signer_) => wait(governor.connect(signer_).queue(proposalId))
-  );
-
-  debug(`Created proposal ${proposalId} and queued it. Proposal still needs to be executed.`);
-
-  await deploymentManager.asyncCallWithRetry(
-    (signer_) => wait(governor.connect(signer_).execute(proposalId))
-  );
-
-  // Log out new states to manually verify (helpful to verify via simulation)
-  debug("COMP balance of Timelock: ", await COMP.balanceOf(timelock.address));
-  debug("COMP balance of CometRewards: ", await COMP.balanceOf(rewards.address));
-  debug("RewardConfig: ", await rewards.rewardConfig(comet.address));
 }
