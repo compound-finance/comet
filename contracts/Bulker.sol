a2fa255d012a6f112735067a9d9ec7438cbc6a1a
kevincheng96 <kevincheng96@hotmail.com>
2022-08-15 16:44:55 -0700

    Make bulker generic to Comet deployments

--- contracts/Bulker.sol
+++ contracts/Bulker.sol
@@ -14,10 +14,7 @@ interface IClaimable {
 contract Bulker {
     /** General configuration constants **/
     address public immutable admin;
-    address public immutable comet;
-    address public immutable rewards;
     address payable public immutable weth;
-    address public immutable baseToken;
 
     /** Actions **/
     uint public constant ACTION_SUPPLY_ASSET = 1;
@@ -32,12 +29,9 @@ contract Bulker {
     error FailedToSendEther();
     error Unauthorized();
 
-    constructor(address admin_, address comet_, address rewards_, address payable weth_) {
+    constructor(address admin_, address payable weth_) {
         admin = admin_;
-        comet = comet_;
-        rewards = rewards_;
         weth = weth_;
-        baseToken = CometInterface(comet_).baseToken();
     }
 
     /**
@@ -81,24 +75,24 @@ contract Bulker {
         for (uint i = 0; i < actions.length; ) {
             uint action = actions[i];
             if (action == ACTION_SUPPLY_ASSET) {
-                (address to, address asset, uint amount) = abi.decode(data[i], (address, address, uint));
-                supplyTo(to, asset, amount);
+                (address comet, address to, address asset, uint amount) = abi.decode(data[i], (address, address, address, uint));
+                supplyTo(comet, to, asset, amount);
             } else if (action == ACTION_SUPPLY_ETH) {
-                (address to, uint amount) = abi.decode(data[i], (address, uint));
+                (address comet, address to, uint amount) = abi.decode(data[i], (address, address, uint));
                 unusedEth -= amount;
-                supplyEthTo(to, amount);
+                supplyEthTo(comet, to, amount);
             } else if (action == ACTION_TRANSFER_ASSET) {
-                (address to, address asset, uint amount) = abi.decode(data[i], (address, address, uint));
-                transferTo(to, asset, amount);
+                (address comet, address to, address asset, uint amount) = abi.decode(data[i], (address, address, address, uint));
+                transferTo(comet, to, asset, amount);
             } else if (action == ACTION_WITHDRAW_ASSET) {
-                (address to, address asset, uint amount) = abi.decode(data[i], (address, address, uint));
-                withdrawTo(to, asset, amount);
+                (address comet, address to, address asset, uint amount) = abi.decode(data[i], (address, address, address, uint));
+                withdrawTo(comet, to, asset, amount);
             } else if (action == ACTION_WITHDRAW_ETH) {
-                (address to, uint amount) = abi.decode(data[i], (address, uint));
-                withdrawEthTo(to, amount);
+                (address comet, address to, uint amount) = abi.decode(data[i], (address, address, uint));
+                withdrawEthTo(comet, to, amount);
             } else if (action == ACTION_CLAIM_REWARD) {
-                (address src, bool shouldAccrue) = abi.decode(data[i], (address, bool));
-                claimReward(src, shouldAccrue);
+                (address comet, address rewards, address src, bool shouldAccrue) = abi.decode(data[i], (address, address, address, bool));
+                claimReward(comet, rewards, src, shouldAccrue);
             }
             unchecked { i++; }
         }
@@ -113,14 +107,14 @@ contract Bulker {
     /**
      * @notice Supplies an asset to a user in Comet
      */
-    function supplyTo(address to, address asset, uint amount) internal {
+    function supplyTo(address comet, address to, address asset, uint amount) internal {
         CometInterface(comet).supplyFrom(msg.sender, to, asset, amount);
     }
 
     /**
      * @notice Wraps ETH and supplies WETH to a user in Comet
      */
-    function supplyEthTo(address to, uint amount) internal {
+    function supplyEthTo(address comet, address to, uint amount) internal {
         IWETH9(weth).deposit{ value: amount }();
         IWETH9(weth).approve(comet, amount);
         CometInterface(comet).supplyFrom(address(this), to, weth, amount);
@@ -129,21 +123,21 @@ contract Bulker {
     /**
      * @notice Transfers an asset to a user in Comet
      */
-    function transferTo(address to, address asset, uint amount) internal {
+    function transferTo(address comet, address to, address asset, uint amount) internal {
         CometInterface(comet).transferAssetFrom(msg.sender, to, asset, amount);
     }
 
     /**
      * @notice Withdraws an asset to a user in Comet
      */
-    function withdrawTo(address to, address asset, uint amount) internal {
+    function withdrawTo(address comet, address to, address asset, uint amount) internal {
         CometInterface(comet).withdrawFrom(msg.sender, to, asset, amount);
     }
 
     /**
      * @notice Withdraws WETH from Comet to a user after unwrapping it to ETH
      */
-    function withdrawEthTo(address to, uint amount) internal {
+    function withdrawEthTo(address comet, address to, uint amount) internal {
         CometInterface(comet).withdrawFrom(msg.sender, address(this), weth, amount);
         IWETH9(weth).withdraw(amount);
         (bool success, ) = to.call{ value: amount }("");
@@ -153,7 +147,7 @@ contract Bulker {
     /**
      * @notice Claim reward for a user
      */
-    function claimReward(address src, bool shouldAccrue) internal {
+    function claimReward(address comet, address rewards, address src, bool shouldAccrue) internal {
         IClaimable(rewards).claim(comet, src, shouldAccrue);
     }
 }
