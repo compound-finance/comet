cf7cbd3194e3f30a83a794cb5fd6e7b16bb71dce
Scott Silver <scott@compound.finance>
2022-09-08 17:24:06 -0700

    add USDC

--- deployments/mumbai/usdc/deploy.ts
+++ deployments/mumbai/usdc/deploy.ts
@@ -1,6 +1,15 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { DeploySpec, cloneGov, deployComet, exp, sameAddress, wait } from '../../../src/deploy';
 
+const clone = {
+  usdcImpl: '0xa2327a938Febf5FEC13baCFb16Ae10EcBc4cbDCF',
+  usdcProxy: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  wbtc: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+  weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  // link: '0x514910771af9ca656af840dff83e8264ecf986ca',
+};
+
+
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
   const deployed = await deployContracts(deploymentManager, deploySpec);
   // XXX mint tokens
@@ -44,14 +53,42 @@ async function deployContracts(deploymentManager: DeploymentManager, deploySpec:
   );
   trace(`PolygonBridgeReceiver initialized`);
 
+  // USDC
+  const usdcProxyAdmin = await deploymentManager.deploy('USDC:admin', 'vendor/proxy/transparent/ProxyAdmin.sol', []);
+  const usdcImpl = await deploymentManager.clone('USDC:implementation', clone.usdcImpl, []);
+  const usdcProxy = await deploymentManager.clone('USDC', clone.usdcProxy, [usdcImpl.address]);
+  const usdcProxyAdminSlot = '0x10d6a54a4754c8869d6886b5f5d7fbfa5b4522237ea5c60d11bc4e7a1ff9390b';
+  const USDC = usdcImpl.attach(usdcProxy.address);
+
+  await deploymentManager.idempotent(
+    async () => !sameAddress(await ethers.provider.getStorageAt(usdcProxy.address, usdcProxyAdminSlot), usdcProxyAdmin.address),
+    async () => {
+      trace(`Changing admin of USDC proxy to ${usdcProxyAdmin.address}`);
+      trace(await wait(usdcProxy.connect(signer).changeAdmin(usdcProxyAdmin.address)));
+
+      trace(`Initializing USDC`);
+      trace(await wait(USDC.connect(signer).initialize(
+        'USD Coin',     // name
+        'USDC',         // symbol
+        'USD',          // currency
+        6,              // decimals
+        signer.address, // Master Minter
+        signer.address, // Pauser
+        signer.address, // Blacklister
+        signer.address  // Owner
+      )));
+    }
+  );
+
   // Deploy Comet
-  // const deployed = await deployComet(
-  //   deploymentManager,
-  //   deploySpec,
-  //   {
-  //     governor: bridgeTimelock.address
-  //   }
-  // );
+  const deployed = await deployComet(
+    deploymentManager,
+    deploySpec,
+    {
+      governor: bridgeTimelock.address,
+      pauseGuardian: bridgeTimelock.address
+    }
+  );
 
   // deploy bulker
   // deploy fauceteer
@@ -60,6 +97,6 @@ async function deployContracts(deploymentManager: DeploymentManager, deploySpec:
   return {
     bridgeTimelock,
     polygonBridgeReceiver,
-    // ...deployed
+    ...deployed
   };
 }
