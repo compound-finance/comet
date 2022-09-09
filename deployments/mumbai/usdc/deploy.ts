31af108ec7291e11dee435a575ae7575f6477253
Scott Silver <scott@compound.finance>
2022-09-08 17:32:00 -0700

    add WMATIC

--- deployments/mumbai/usdc/deploy.ts
+++ deployments/mumbai/usdc/deploy.ts
@@ -6,6 +6,7 @@ const clone = {
   usdcProxy: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
   wbtc: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
   weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  wmatic: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
   // link: '0x514910771af9ca656af840dff83e8264ecf986ca',
 };
 
@@ -82,6 +83,12 @@ async function deployContracts(deploymentManager: DeploymentManager, deploySpec:
 
   const WBTC = await deploymentManager.clone('WBTC', clone.wbtc, []);
   const WETH = await deploymentManager.clone('WETH', clone.weth, []);
+  const WMATIC = await deploymentManager.clone(
+    'WMATIC',
+    clone.wmatic,
+    [],
+    'polygon' // NOTE: cloned from Polygon, not mainnet
+  );
 
   // Deploy Comet
   const deployed = await deployComet(
