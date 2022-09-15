5bc0a64769594401ed830001841c0335678e7604
kevincheng96 <kevincheng96@hotmail.com>
2022-09-15 13:17:41 -0700

    Cleanup

--- plugins/deployment_manager/Enacted.ts
+++ plugins/deployment_manager/Enacted.ts
@@ -21,85 +21,9 @@ export async function writeEnacted<T>(migration: Migration<T>, deploymentManager
 }
 
 export function addEnactedToMigration(sourceFile: ts.SourceFile): string {
-  // XXX The following method directly modifying the AST seems less error-prone,
-  // but unfortunately messes with the original formatting of the file
-  // const enactedMethodDeclaration = ts.factory.createMethodDeclaration(
-  //   [],
-  //   [ts.factory.createModifier(ts.SyntaxKind.AsyncKeyword)],
-  //   undefined,
-  //   'enacted',
-  //   undefined,
-  //   [],
-  //   [
-  //     ts.factory.createParameterDeclaration(
-  //       [],
-  //       [],
-  //       undefined,
-  //       'deploymentManager',
-  //     )
-  //   ],
-  //   undefined,
-  //   ts.factory.createBlock([
-  //     ts.factory.createReturnStatement(ts.factory.createTrue())
-  //   ])
-  // ts.factory.createVariableDeclarationList(
-  //   [ts.factory.createVariableDeclaration(
-  //     ts.factory.createIdentifier("testVar"),
-  //     undefined,
-  //     ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
-  //     ts.factory.createStringLiteral("test")
-  //   )],
-  //   ts.NodeFlags.Const
-  // );
-
-  // const transformerFactory: ts.TransformerFactory<ts.Node> = (
-  //   context: ts.TransformationContext
-  // ) => {
-  //   return (rootNode) => {
-  //     function visit(node: ts.Node): ts.Node {
-  //       node = ts.visitEachChild(node, visit, context);
-  //       if (ts.isExportAssignment(node)) {
-  //         console.log('IS EXPORT ASSIGNMENT')
-  //         console.log(node)
-  //         const callExpression = node.expression as ts.CallExpression;
-  //         const objectLiteralExpression = callExpression.arguments.find(x => x.kind === ts.SyntaxKind.ObjectLiteralExpression) as ts.ObjectLiteralExpression;
-  //         console.log(objectLiteralExpression)
-  //         console.log('print properties')
-
-  //         const newProperties = objectLiteralExpression.properties.filter(x => (x.name as ts.Identifier).escapedText !== 'enacted');
-  //         newProperties.push(enactedMethodDeclaration)
-  //         const newObjectLiteralExpression = ts.factory.createObjectLiteralExpression(newProperties, true);
-  //         const newCallExpressionArguments = callExpression.arguments.filter(x => x.kind !== ts.SyntaxKind.ObjectLiteralExpression);
-  //         newCallExpressionArguments.push(newObjectLiteralExpression);
-  //         const newCallExpression = ts.factory.createCallExpression(
-  //           callExpression.expression, callExpression.typeArguments,
-  //           newCallExpressionArguments
-  //         )
-
-  //         return ts.factory.createExportAssignment(
-  //           node.decorators, node.modifiers, node.isExportEquals,
-  //           newCallExpression
-  //         )
-  //       }
-  //       return node;
-  //     }
-
-  //     return ts.visitNode(rootNode, visit);
-  //   };
-  // };
-
-  // const transformationResult = ts.transform(
-  //   sourceFile, [transformerFactory]
-  // );
-  // const transformedSourceFile = transformationResult.transformed[0];
-  // const printer = ts.createPrinter();
-  // const result = printer.printNode(
-  //   ts.EmitHint.Unspecified,
-  //   transformedSourceFile,
-  //   undefined
-  // );
-  // console.log(result)
-
+  // Note: Another approach is to directly modify the AST, but unfortunately this does not
+  // preserve the original formatting of the source code
+  // Example of the AST approach in commit 73e60480627230d84bb40ab0269722a3e839713a
   const sourceFileText = sourceFile.getFullText();
   const exportAssignment = sourceFile.statements.find(ts.isExportAssignment)!;
   const callExpression = exportAssignment.expression as ts.CallExpression;
@@ -113,6 +37,7 @@ export function addEnactedToMigration(sourceFile: ts.SourceFile): string {
     // If enacted already exists, just replace it
     let endPos = enacted.end;
     if (sourceFileText.charAt(enacted.end) === ',') {
+      // Skip the original comma to avoid double commas
       endPos = enacted.end + 1;
     }
     newSourceCode = sourceFileText.substring(0, enacted.pos)
@@ -124,8 +49,10 @@ export function addEnactedToMigration(sourceFile: ts.SourceFile): string {
     // the last comment end position instead of enact.end
     let insertPos;
     if (sourceFileText.charAt(enact.end) === ',') {
+      // Insert after the comma
       insertPos = enact.end + 1;
     } else {
+      // Prepend a comma
       insertPos = enact.end;
       code = ',' + code;
     }
