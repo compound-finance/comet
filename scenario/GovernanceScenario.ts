65a4acd10161a4f84a7b0de0969cec753b23416f
Scott Silver <scott@compound.finance>
2022-09-14 20:44:04 -0700

    extract fastGovernanceExecute

--- scenario/GovernanceScenario.ts
+++ scenario/GovernanceScenario.ts
@@ -1,8 +1,7 @@
-import { scenario, setNextBaseFeeToZero } from './context/CometContext';
+import { scenario, setNextBaseFeeToZero, fastGovernanceExecute } from './context/CometContext';
 import { expect } from 'chai';
 import { constants, EventFilter, utils } from 'ethers';
 import { COMP_WHALES } from "../src/deploy";
-import { ProposalState } from './context/Gov';
 import { impersonateAddress } from '../plugins/scenario/World';
 
 const FX_ROOT_GOERLI = '0x3d1d3E34f7fB6D26245E6640E1c50710eFFf15bA';
@@ -25,14 +24,6 @@ scenario.only('L2 Governance scenario', {}, async ({ comet }, context, world) =>
   const l2Timelock = await world.deploymentManager.contract('timelock');
   const polygonBridgeReceiver = await world.deploymentManager.contract('polygonBridgeReceiver');
 
-  async function setNextL1BlockTimestamp(timestamp: number) {
-    await l1Hre.ethers.provider.send('evm_setNextBlockTimestamp', [timestamp]);
-  }
-
-  async function mineL1Blocks(blocks: number) {
-    await l1Hre.network.provider.send('hardhat_mine', [`0x${blocks.toString(16)}`]);
-  }
-
   // l2 proposal
   const fiveDaysInSeconds = 5 * 24 * 60 * 60;
   const encodedL2Data = utils.defaultAbiCoder.encode(
@@ -53,58 +44,14 @@ scenario.only('L2 Governance scenario', {}, async ({ comet }, context, world) =>
     [polygonBridgeReceiver?.address, encodedL2Data]
   );
 
-  // propose
-  await setNextBaseFeeToZero(l1DeploymentManager);
-  const proposeTxn = await (
-    await l1Governor?.connect(proposer).propose(
-      [FX_ROOT_GOERLI],
-      [0],
-      ["sendMessageToChild(address,bytes)"],
-      [sendMessageToChildCalldata],
-      'FastExecuteProposal',
-      { gasPrice: 0 }
-    )
-  ).wait();
-  const proposeEvent = proposeTxn.events.find(event => event.event === 'ProposalCreated');
-  const [id, , , , , , startBlock, endBlock] = proposeEvent.args;
-
-  // execute open proposal
-  const blockNow = await l1Hre.ethers.provider.getBlockNumber();
-  const blocksUntilStart = startBlock - blockNow;
-  const blocksUntilEnd = endBlock - Math.max(startBlock, blockNow);
-
-  if (blocksUntilStart > 0) {
-    await mineL1Blocks(blocksUntilStart);
-  }
-
-  if (blocksUntilEnd > 0) {
-    for (const whale of COMP_WHALES) {
-      try {
-        // Voting can fail if voter has already voted
-
-        await l1Hre.network.provider.request({
-          method: 'hardhat_impersonateAccount',
-          params: [whale],
-        });
-        const voter = await l1DeploymentManager.getSigner(whale);
-        await setNextBaseFeeToZero(l1DeploymentManager);
-        await l1Governor?.connect(voter).castVote(id, 1, { gasPrice: 0 });
-      } catch (err) {
-        console.log(`Error while voting for ${whale}`, err.message);
-      }
-    }
-    await mineL1Blocks(blocksUntilEnd);
-  }
-
-  // Queue proposal (maybe)
-  const state = await l1Governor?.state(id);
-  if (state == ProposalState.Succeeded) {
-    await setNextBaseFeeToZero(l1DeploymentManager);
-    await l1Governor?.queue(id, { gasPrice: 0 });
-  }
-
-  const proposal = await l1Governor?.proposals(id);
-  await setNextL1BlockTimestamp(proposal.eta.toNumber() + 1);
+  await fastGovernanceExecute(
+    l1DeploymentManager,
+    proposer,
+    [FX_ROOT_GOERLI],
+    [0],
+    ["sendMessageToChild(address,bytes)"],
+    [sendMessageToChildCalldata]
+  );
 
   // listen on events on the fxRoot
   const filter: EventFilter = {
@@ -123,9 +70,6 @@ scenario.only('L2 Governance scenario', {}, async ({ comet }, context, world) =>
       reject(new Error('timeout'));
     }, 60000);
   });
-
-  await setNextBaseFeeToZero(l1DeploymentManager);
-  await l1Governor?.execute(id, { gasPrice: 0, gasLimit: 12000000 });
 
   // XXX type for stateSyncedEvent
   const stateSyncedEvent: any = await stateSyncedListenerPromise;
@@ -193,6 +137,7 @@ scenario.only('L2 Governance scenario', {}, async ({ comet }, context, world) =>
   await context.setNextBlockTimestamp(eta.toNumber() + 1);
 
   // check delay before
+  console.log(`await l2Timelock?.delay(): ${await l2Timelock?.delay()}`);
   expect(await l2Timelock?.delay()).to.eq(2 * 24 * 60 * 60);
 
   await setNextBaseFeeToZero(l2DeploymentManager);
@@ -209,6 +154,7 @@ scenario.only('L2 Governance scenario', {}, async ({ comet }, context, world) =>
   ).wait();
 
   // check delay before
+  console.log(`await l2Timelock?.delay(): ${await l2Timelock?.delay()}`);
   expect(await l2Timelock?.delay()).to.eq(fiveDaysInSeconds);
 });
 
--- scenario/context/CometContext.ts
+++ scenario/context/CometContext.ts
@@ -31,6 +31,7 @@ import { ProtocolConfiguration, deployComet, COMP_WHALES } from '../../src/deplo
 import { AddressLike, getAddressFromNumber, resolveAddress } from './Address';
 import { Requirements } from '../constraints/Requirements';
 import { DeploymentManager } from '../../plugins/deployment_manager';
+import { impersonateAddress } from '../../plugins/scenario/World';
 
 export type ActorMap = { [name: string]: CometActor };
 export type AssetMap = { [name: string]: CometAsset };
@@ -49,6 +50,94 @@ export interface CometProperties {
 
 export async function setNextBaseFeeToZero(dm: DeploymentManager) {
   await dm.hre.network.provider.send('hardhat_setNextBlockBaseFeePerGas', ['0x0']);
+}
+
+export async function mineBlocks(dm: DeploymentManager, blocks: number) {
+  await dm.hre.network.provider.send('hardhat_mine', [`0x${blocks.toString(16)}`]);
+}
+
+export async function setNextBlockTimestamp(dm: DeploymentManager, timestamp: number) {
+  await dm.hre.ethers.provider.send('evm_setNextBlockTimestamp', [timestamp]);
+}
+
+export async function executeOpenProposal(
+  dm: DeploymentManager,
+  { id, startBlock, endBlock }: OpenProposal
+) {
+  const governor = await dm.contract('governor');
+
+  if (!governor) {
+    throw new Error("cannot execute proposal without governor");
+  }
+
+  const blockNow = await dm.hre.ethers.provider.getBlockNumber();
+  const blocksUntilStart = startBlock - blockNow;
+  const blocksUntilEnd = endBlock - Math.max(startBlock, blockNow);
+
+  if (blocksUntilStart > 0) {
+    await mineBlocks(dm, blocksUntilStart);
+  }
+
+  if (blocksUntilEnd > 0) {
+    for (const whale of COMP_WHALES) {
+      try {
+        // Voting can fail if voter has already voted
+        const voter = await impersonateAddress(dm, whale);
+        await setNextBaseFeeToZero(dm);
+        await governor.connect(voter).castVote(id, 1, { gasPrice: 0 });
+      } catch (err) {
+        debug(`Error while voting for ${whale}`, err.message);
+      }
+    }
+    await mineBlocks(dm, blocksUntilEnd);
+  }
+
+  // Queue proposal (maybe)
+  const state = await governor.state(id);
+  if (state == ProposalState.Succeeded) {
+    await setNextBaseFeeToZero(dm);
+    await governor.queue(id, { gasPrice: 0 });
+  }
+
+  const proposal = await governor.proposals(id);
+  await setNextBlockTimestamp(dm, proposal.eta.toNumber() + 1);
+
+  // Execute proposal (w/ gas limit so we see if exec reverts, not a gas estimation error)
+  await setNextBaseFeeToZero(dm);
+  await governor.execute(id, { gasPrice: 0, gasLimit: 12000000 });
+}
+
+// Instantly executes some actions through the governance proposal process
+export async function fastGovernanceExecute(
+  dm: DeploymentManager,
+  proposer: SignerWithAddress,
+  targets: string[],
+  values: BigNumberish[],
+  signatures: string[],
+  calldatas: string[]
+) {
+  const governor = await dm.contract('governor');
+
+  if (!governor) {
+    throw new Error("cannot create governance proposal without governor");
+  }
+
+  await setNextBaseFeeToZero(dm);
+
+  const proposeTxn = await (
+    await governor.connect(proposer).propose(
+      targets,
+      values,
+      signatures,
+      calldatas,
+      'FastExecuteProposal',
+      { gasPrice: 0 }
+    )
+  ).wait();
+  const proposeEvent = proposeTxn.events.find(event => event.event === 'ProposalCreated');
+  const [id, , , , , , startBlock, endBlock] = proposeEvent.args;
+
+  await executeOpenProposal(dm, { id, startBlock, endBlock });
 }
 
 export class CometContext {
@@ -234,76 +323,28 @@ export class CometContext {
   }
 
   async setNextBlockTimestamp(timestamp: number) {
-    await this.world.deploymentManager.hre.ethers.provider.send('evm_setNextBlockTimestamp', [timestamp]);
+    await setNextBlockTimestamp(this.world.deploymentManager, timestamp);
   }
 
   async mineBlocks(blocks: number) {
-    await this.world.deploymentManager.hre.network.provider.send('hardhat_mine', [`0x${blocks.toString(16)}`]);
+    await mineBlocks(this.world.deploymentManager, blocks);
   }
 
-  async executeOpenProposal({ id, startBlock, endBlock }: OpenProposal) {
-    const governor = await this.getGovernor();
-    const blockNow = await this.world.deploymentManager.hre.ethers.provider.getBlockNumber();
-    const blocksUntilStart = startBlock - blockNow;
-    const blocksUntilEnd = endBlock - Math.max(startBlock, blockNow);
-
-    if (blocksUntilStart > 0) {
-      await this.mineBlocks(blocksUntilStart);
-    }
-
-    if (blocksUntilEnd > 0) {
-      for (const whale of COMP_WHALES) {
-        try {
-          // Voting can fail if voter has already voted
-          const voter = await this.world.impersonateAddress(whale);
-          await this.setNextBaseFeeToZero();
-          await governor.connect(voter).castVote(id, 1, { gasPrice: 0 });
-        } catch (err) {
-          debug(`Error while voting for ${whale}`, err.message);
-        }
-      }
-      await this.mineBlocks(blocksUntilEnd);
-    }
-
-    // Queue proposal (maybe)
-    const state = await governor.state(id);
-    if (state == ProposalState.Succeeded) {
-      await this.setNextBaseFeeToZero();
-      await governor.queue(id, { gasPrice: 0 });
-    }
-
-    const proposal = await governor.proposals(id);
-    await this.setNextBlockTimestamp(proposal.eta.toNumber() + 1);
-
-    // Execute proposal (w/ gas limit so we see if exec reverts, not a gas estimation error)
-    await this.setNextBaseFeeToZero();
-    await governor.execute(id, { gasPrice: 0, gasLimit: 12000000 });
+  async executeOpenProposal(openProposal: OpenProposal) {
+    await executeOpenProposal(this.world.deploymentManager, openProposal);
   }
 
   // Instantly executes some actions through the governance proposal process
-  // Note: `governor` must be connected to an `admin` signer
   async fastGovernanceExecute(targets: string[], values: BigNumberish[], signatures: string[], calldatas: string[]) {
-    // XXX throw if called by L1
-    const { world } = this;
-    const governor = await this.getGovernor();
     const proposer = await this.getProposer();
-
-    await this.setNextBaseFeeToZero();
-
-    const proposeTxn = await (
-      await governor.connect(proposer).propose(
-        targets,
-        values,
-        signatures,
-        calldatas,
-        'FastExecuteProposal',
-        { gasPrice: 0 }
-      )
-    ).wait();
-    const proposeEvent = proposeTxn.events.find(event => event.event === 'ProposalCreated');
-    const [id, , , , , , startBlock, endBlock] = proposeEvent.args;
-
-    await this.executeOpenProposal({ id, startBlock, endBlock });
+    await fastGovernanceExecute(
+      this.world.deploymentManager,
+      proposer,
+      targets,
+      values,
+      signatures,
+      calldatas
+    );
   }
 }
 
