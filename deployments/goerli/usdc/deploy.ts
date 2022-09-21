8f04b8bfc1bc31c86368485cf1958e9b1fc8cbb0
Jared Flatow <jflatow@gmail.com>
2022-09-21 13:52:21 -0700

    Re-deploy Goerli with other USDC (#569)
    * Redeploy comet with different USDC
    
    Keep the rewards contract since it has COMP already, though need to pass a proposal to set rewards config later.
    
    Also, the USDC base is not one we deployed and we cannot mint, so we should be ensure to grant the fauceteer.
    
    Re-deploy Goerli with other USDC
    
    Fix USDC address, which cannot be minted
    
    * Swap fauceteer and other fixes
    
    * Ensure roots are aliases
    
    * Re-deploy comet with 'roots are aliases' fix
    
    * Attempt 🥈s fix for annoying transient expiry errors in CI
    
    * Move some assets out of roots and fix
    
    * Do COMP also as an existing

--- deployments/goerli/usdc/deploy.ts
+++ deployments/goerli/usdc/deploy.ts
@@ -2,14 +2,6 @@ import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager
 import { DeploySpec, cloneGov, deployComet, exp, sameAddress, wait } from '../../../src/deploy';
 import { expect } from 'chai';
 
-const clone = {
-  usdcImpl: '0xa2327a938Febf5FEC13baCFb16Ae10EcBc4cbDCF',
-  usdcProxy: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  wbtc: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-  weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  link: '0x514910771af9ca656af840dff83e8264ecf986ca',
-};
-
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
   const deployed = await deployContracts(deploymentManager, deploySpec);
   await mintTokens(deploymentManager);
@@ -21,37 +13,14 @@ async function deployContracts(deploymentManager: DeploymentManager, deploySpec:
   const ethers = deploymentManager.hre.ethers;
   const signer = await deploymentManager.getSigner();
 
+  // Declare existing assets as aliases
+  const COMP = await deploymentManager.existing('COMP', '0x3587b2F7E0E2D6166d6C14230e7Fe160252B0ba4');
+  const USDC = await deploymentManager.existing('USDC', '0x07865c6E87B9F70255377e024ace6630C1Eaa37F');
+  const WBTC = await deploymentManager.existing('WBTC', '0xAAD4992D949f9214458594dF92B44165Fb84dC19');
+  const WETH = await deploymentManager.existing('WETH', '0x42a71137C09AE83D8d05974960fd607d40033499');
+
   // Deploy governance contracts
-  const { COMP, fauceteer, timelock } = await cloneGov(deploymentManager);
-
-  const usdcProxyAdmin = await deploymentManager.deploy('USDC:admin', 'vendor/proxy/transparent/ProxyAdmin.sol', []);
-  const usdcImpl = await deploymentManager.clone('USDC:implementation', clone.usdcImpl, []);
-  const usdcProxy = await deploymentManager.clone('USDC', clone.usdcProxy, [usdcImpl.address]);
-  const usdcProxyAdminSlot = '0x10d6a54a4754c8869d6886b5f5d7fbfa5b4522237ea5c60d11bc4e7a1ff9390b';
-  const USDC = usdcImpl.attach(usdcProxy.address);
-
-  await deploymentManager.idempotent(
-    async () => !sameAddress(await ethers.provider.getStorageAt(usdcProxy.address, usdcProxyAdminSlot), usdcProxyAdmin.address),
-    async () => {
-      trace(`Changing admin of USDC proxy to ${usdcProxyAdmin.address}`);
-      trace(await wait(usdcProxy.connect(signer).changeAdmin(usdcProxyAdmin.address)));
-
-      trace(`Initializing USDC`);
-      trace(await wait(USDC.connect(signer).initialize(
-        'USD Coin',     // name
-        'USDC',         // symbol
-        'USD',          // currency
-        6,              // decimals
-        signer.address, // Master Minter
-        signer.address, // Pauser
-        signer.address, // Blacklister
-        signer.address  // Owner
-      )));
-    }
-  );
-
-  const WBTC = await deploymentManager.clone('WBTC', clone.wbtc, []);
-  const WETH = await deploymentManager.clone('WETH', clone.weth, []);
+  const { fauceteer, timelock } = await cloneGov(deploymentManager);
 
   // Deploy all Comet-related contracts
   const deployed = await deployComet(deploymentManager, deploySpec);
@@ -93,21 +62,6 @@ async function mintTokens(deploymentManager: DeploymentManager) {
       trace(`Minting 0.01 WETH for signer (this is a precious resource!)`);
       trace(await wait(WETH.connect(signer).deposit({ value: exp(0.01, 18) })));
       trace(`WETH.balanceOf(${signer.address}): ${await WETH.balanceOf(signer.address)}`);
-    }
-  );
-
-  // If we haven't spidered new contracts (which we could before minting, but its slow),
-  //  then the proxy contract won't have the impl functions yet, so just do it explicitly
-  const usdcProxy = contracts.get('USDC'), usdcImpl = contracts.get('USDC:implementation');
-  const USDC = usdcImpl.attach(usdcProxy.address);
-  await deploymentManager.idempotent(
-    async () => (await USDC.balanceOf(fauceteer.address)).eq(0),
-    async () => {
-      trace(`Minting 100M USDC to fauceteer`);
-      const amount = exp(100_000_000, await USDC.decimals());
-      trace(await wait(USDC.connect(signer).configureMinter(signer.address, amount)));
-      trace(await wait(USDC.connect(signer).mint(fauceteer.address, amount)));
-      trace(`USDC.balanceOf(${fauceteer.address}): ${await USDC.balanceOf(fauceteer.address)}`);
     }
   );
 
