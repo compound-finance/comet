89d2bcf19aa01e5552ca95b185beda3e51ffafc1
scott-silver <scott@compound.finance>
2022-09-23 15:42:18 -0700

    BaseBridgeReceiver unit tests (#582)
    * BaseBridgeReceiver unit tests
    
    * notes
    
    * makeTimelock refactor
    
    * sets new timelock
    
    * setGovTimelock revert
    
    * sets gov timelock
    
    * harness, processMessage revert test
    
    * badData test
    
    * repeated transaction
    
    * queues transaction test
    
    * invalid proposal id tests
    
    * expired proposal test
    
    * already executed revert
    
    * execute transaction test
    
    * proposal state checks
    
    * refactor
    
    * delete .only
    
    * lint
    
    * rename TYPES
    
    * typo, consistency

---
+++ contracts/bridges/test/BaseBridgeReceiverHarness.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.15;
+
+import "../BaseBridgeReceiver.sol";
+
+contract BaseBridgeReceiverHarness is BaseBridgeReceiver {
+    function processMessageExternal(
+        address messageSender,
+        bytes calldata data
+    ) external {
+        processMessage(messageSender, data);
+    }
+}
