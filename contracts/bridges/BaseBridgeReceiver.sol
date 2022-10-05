a4b25c0a8459f0df42748cd221702559f71b1707
scott-silver <scott@compound.finance>
2022-10-05 09:39:26 -0700

    natspecs for BaseBridgeReceiver (#591)

--- contracts/bridges/BaseBridgeReceiver.sol
+++ contracts/bridges/BaseBridgeReceiver.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.15;
 import "../ITimelock.sol";
 
 contract BaseBridgeReceiver {
+    /** Custom errors **/
     error AlreadyInitialized();
     error BadData();
     error InvalidProposalId();
@@ -11,16 +12,27 @@ contract BaseBridgeReceiver {
     error TransactionAlreadyQueued();
     error Unauthorized();
 
+    /** Events **/
     event Initialized(address indexed govTimelock, address indexed localTimelock);
     event NewLocalTimelock(address indexed oldLocalTimelock, address indexed newLocalTimelock);
     event NewGovTimelock(address indexed oldGovTimelock, address indexed newGovTimelock);
     event ProposalCreated(address indexed messageSender, uint id, address[] targets, uint[] values, string[] signatures, bytes[] calldatas, uint eta);
     event ProposalExecuted(uint id);
 
+    /** Public variables **/
+
+    /// @notice Address of the governing contract that this bridge receiver expects to
+    ///  receive messages from; likely an address from another chain (e.g. mainnet)
     address public govTimelock;
+
+    /// @notice Address of the timelock on this chain that the bridge receiver
+    /// will send messages to
     address public localTimelock;
+
+    /// @notice Whether contract has been initialized
     bool public initialized;
 
+    /// @notice Total count of proposals generated
     uint public proposalCount;
 
     struct Proposal {
@@ -33,6 +45,7 @@ contract BaseBridgeReceiver {
         bool executed;
     }
 
+    /// @notice Mapping of proposal ids to their full proposal data
     mapping (uint => Proposal) public proposals;
 
     enum ProposalState {
@@ -41,6 +54,13 @@ contract BaseBridgeReceiver {
         Executed
     }
 
+    /**
+     * @notice Initialize the contract
+     * @param _govTimelock Address of the governing contract that this contract
+     * will receive messages from (likely on another chain)
+     * @param _localTimelock Address of the timelock contract that this contract
+     * will send messages to
+     */
     function initialize(address _govTimelock, address _localTimelock) external {
         if (initialized) revert AlreadyInitialized();
         govTimelock = _govTimelock;
@@ -49,11 +69,18 @@ contract BaseBridgeReceiver {
         emit Initialized(_govTimelock, _localTimelock);
     }
 
+    /**
+     * @notice Accept admin role for the localTimelock
+     */
     function acceptLocalTimelockAdmin() external {
         if (msg.sender != localTimelock) revert Unauthorized();
         ITimelock(localTimelock).acceptAdmin();
     }
 
+    /**
+     * @notice Set localTimelock address
+     * @param newTimelock Address to set as the localTimelock
+     */
     function setLocalTimelock(address newTimelock) public {
         if (msg.sender != localTimelock) revert Unauthorized();
         address oldLocalTimelock = localTimelock;
@@ -61,6 +88,10 @@ contract BaseBridgeReceiver {
         emit NewLocalTimelock(oldLocalTimelock, newTimelock);
     }
 
+    /**
+     * @notice Set govTimelock address
+     * @param newTimelock Address to set as the govTimelock
+     */
     function setGovTimelock(address newTimelock) public {
         if (msg.sender != localTimelock) revert Unauthorized();
         address oldGovTimelock = govTimelock;
@@ -68,6 +99,11 @@ contract BaseBridgeReceiver {
         emit NewGovTimelock(oldGovTimelock, newTimelock);
     }
 
+    /**
+     * @notice Process a message sent from the governing timelock (across a bridge)
+     * @param messageSender Address of the contract that sent the bridged message
+     * @param data ABI-encoded bytes containing the transactions to be queued on the local timelock
+     */
     function processMessage(
         address messageSender,
         bytes calldata data
@@ -112,6 +148,10 @@ contract BaseBridgeReceiver {
         emit ProposalCreated(messageSender, proposal.id, targets, values, signatures, calldatas, eta);
     }
 
+    /**
+     * @notice Execute a queued proposal
+     * @param proposalId The id of the proposal to execute
+     */
     function executeProposal(uint proposalId) external {
         if (state(proposalId) != ProposalState.Queued) revert ProposalNotQueued();
         Proposal storage proposal = proposals[proposalId];
@@ -122,6 +162,11 @@ contract BaseBridgeReceiver {
         emit ProposalExecuted(proposalId);
     }
 
+    /**
+     * @notice Get the state of a proposal
+     * @param proposalId Id of the proposal
+     * @return The state of the given proposal (queued, expired or executed)
+     */
     function state(uint proposalId) public view returns (ProposalState) {
         if (proposalId > proposalCount || proposalId == 0) revert InvalidProposalId();
         Proposal memory proposal = proposals[proposalId];
