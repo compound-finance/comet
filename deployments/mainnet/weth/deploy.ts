d337d6af57279929f6463542e638a12933d6959b
Scott Silver <scott@compound.finance>
2022-10-31 15:15:01 -0700

    update mainnet deploy script

--- deployments/mainnet/weth/deploy.ts
+++ deployments/mainnet/weth/deploy.ts
@@ -1,6 +1,8 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { DeploySpec, deployComet } from '../../../src/deploy';
-import { getConfiguration, NetworkConfiguration } from '../../../src/deploy/NetworkConfiguration';
+
+const stETH = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84';
+const wstETH = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0';
 
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
   // Deploy WstETHPriceFeed
@@ -9,7 +11,7 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
     'WstETHPriceFeed.sol',
     [
       '0xcfe54b5cd566ab89272946f602d76ea879cab4a8', // stETHtoUSDPriceFeed
-      '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0'  // wstETH
+      wstETH                                        // wstETH
     ]
   );
 
@@ -20,8 +22,13 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   // Deploy Bulker
   const bulker = await deploymentManager.deploy(
     'bulker',
-    'Bulker.sol',
-    [await comet.governor(), await comet.baseToken()]
+    'bulkers/MainnetBulker.sol',
+    [
+      await comet.governor(),  // admin_
+      await comet.baseToken(), // weth_
+      stETH,                   // steth_
+      wstETH                   // wsteth_
+    ]
   );
 
   return { ...deployed, bulker };
