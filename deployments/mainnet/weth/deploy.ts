54006bedb308181e8108b54cb3f1a024fc6625fb
Scott Silver <scott@compound.finance>
2022-10-31 17:58:39 -0700

    update deploy script

--- deployments/mainnet/weth/deploy.ts
+++ deployments/mainnet/weth/deploy.ts
@@ -1,17 +1,17 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { DeploySpec, deployComet } from '../../../src/deploy';
 
-const stETH = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84';
-const wstETH = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0';
-
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
+  const stETH = await deploymentManager.existing('stETH', '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84');
+  const wstETH = await deploymentManager.existing('wstETH', '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0');
+
   // Deploy WstETHPriceFeed
   const wstETHPriceFeed = await deploymentManager.deploy(
     'wstETH:priceFeed',
     'WstETHPriceFeed.sol',
     [
       '0xcfe54b5cd566ab89272946f602d76ea879cab4a8', // stETHtoUSDPriceFeed
-      wstETH                                        // wstETH
+      wstETH.address                                // wstETH
     ]
   );
 
@@ -26,8 +26,8 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
     [
       await comet.governor(),  // admin_
       await comet.baseToken(), // weth_
-      stETH,                   // steth_
-      wstETH                   // wsteth_
+      stETH.address,           // steth_
+      wstETH.address           // wsteth_
     ]
   );
 
