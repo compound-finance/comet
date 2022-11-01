c88b730ab13f945ea87a2c00aa5be10e251340cb
Scott Silver <scott@compound.finance>
2022-11-01 11:00:22 -0700

    withdraw scenario

--- deployments/mainnet/weth/relations.ts
+++ deployments/mainnet/weth/relations.ts
@@ -4,6 +4,7 @@ import baseRelationConfig from '../../relations';
 export default {
   ...baseRelationConfig,
   'wstETH': {
+    artifact: 'contracts/bulkers/IWstETH.sol',
     relations: {
       stETH: {
         field: async (wstETH) => wstETH.stETH()
--- scenario/MainnetBulkerScenario.ts
+++ scenario/MainnetBulkerScenario.ts
@@ -2,11 +2,14 @@ import { utils } from 'ethers';
 import { expect } from 'chai';
 import { scenario } from './context/CometContext';
 import CometAsset from './context/CometAsset';
-import { ERC20 } from '../build/types';
+import {
+  ERC20,
+  IWstETH
+} from '../build/types';
 import { exp } from '../test/helpers';
-import { isBulkerSupported, matchesDeployment } from './utils';
+import { expectApproximately, isBulkerSupported, matchesDeployment } from './utils';
 
-scenario.only(
+scenario(
   'MainnetBulker > wraps stETH before supplying',
   {
     filter: async (ctx) => await isBulkerSupported(ctx) && matchesDeployment(ctx, [{deployment: 'weth'}]),
@@ -40,10 +43,52 @@ scenario.only(
     const txn = await albert.invoke({ actions, calldata });
 
     expect(await wstETH.balanceOf(albert.address)).to.be.equal(0n);
-    // XXX pretty weak exepectation
+    // XXX pretty weak expectation
     expect(await comet.collateralBalanceOf(albert.address, wstETH.address)).to.be.within(
       0,
       toSupplyStEth
     );
+  }
+);
+
+scenario(
+  'MainnetBulker > wraps wstETH before withdrawing',
+  {
+    filter: async (ctx) => await isBulkerSupported(ctx) && matchesDeployment(ctx, [{deployment: 'weth'}]),
+    tokenBalances: {
+      albert: { $asset1: 2 },
+      $comet: { $asset1: 5 },
+    },
+  },
+  async ({ comet, actors, assets, bulker }, context) => {
+    const { albert } = actors;
+
+    const stETH = await context.world.deploymentManager.contract('stETH') as ERC20;
+    const wstETH = await context.world.deploymentManager.contract('wstETH') as IWstETH;
+
+    const toWithdrawStEth = exp(1, 18);
+
+    // approvals/allowances
+    await albert.allow(bulker.address, true);
+    await wstETH.connect(albert.signer).approve(comet.address, toWithdrawStEth);
+
+    // supply wstETH
+    await albert.supplyAsset({asset: wstETH.address, amount: toWithdrawStEth });
+
+    const withdrawStEthCalldata = utils.defaultAbiCoder.encode(
+      ['address', 'address', 'uint'],
+      [comet.address, albert.address, toWithdrawStEth]
+    );
+    const calldata = [withdrawStEthCalldata];
+    const actions = [await bulker.ACTION_WITHDRAW_STETH()];
+
+    const txn = await albert.invoke({ actions, calldata });
+
+    expectApproximately(
+      await stETH.balanceOf(albert.address),
+      await wstETH.getStETHByWstETH(toWithdrawStEth),
+      1n
+    );
+    expect(await comet.collateralBalanceOf(albert.address, wstETH.address)).to.equal(0);
   }
 );
