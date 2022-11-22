f2e84d8608d7b730f4514f298ac0fe96f8ed292d
kevincheng96 <kevincheng96@hotmail.com>
2022-11-21 16:41:10 -0800

    Update wsteth price feed unit tests

--- test/wsteth-price-feed.ts
+++ test/wsteth-price-feed.ts
@@ -7,7 +7,7 @@ import {
 
 export async function makeWstETH({ stEthPrice, tokensPerStEth }) {
   const SimplePriceFeedFactory = (await ethers.getContractFactory('SimplePriceFeed')) as SimplePriceFeed__factory;
-  const stETHPriceFeed = await SimplePriceFeedFactory.deploy(stEthPrice, 8);
+  const stETHPriceFeed = await SimplePriceFeedFactory.deploy(stEthPrice, 18);
 
   const SimpleWstETHFactory = (await ethers.getContractFactory('SimpleWstETH')) as SimpleWstETH__factory;
   const simpleWstETH = await SimpleWstETHFactory.deploy(tokensPerStEth);
@@ -28,38 +28,78 @@ export async function makeWstETH({ stEthPrice, tokensPerStEth }) {
 
 const testCases = [
   {
-    stEthPrice: exp(1300, 8),
+    stEthPrice: exp(1300, 18),
     tokensPerStEth: exp(.9, 18),
-    result: 144444444444n
+    result: 1444444444444444444444n
   },
   {
-    stEthPrice: exp(1000, 8),
+    stEthPrice: exp(1000, 18),
     tokensPerStEth: exp(.9, 18),
-    result: 111111111111n
+    result: 1111111111111111111111n
   },
   {
-    stEthPrice: exp(1000, 8),
+    stEthPrice: exp(1000, 18),
     tokensPerStEth: exp(.2, 18),
-    result: exp(5000, 8)
+    result: exp(5000, 18)
   },
   {
-    stEthPrice: exp(1000, 8),
+    stEthPrice: exp(1000, 18),
     tokensPerStEth: exp(.5, 18),
-    result: exp(2000, 8)
+    result: exp(2000, 18)
   },
   {
-    stEthPrice: exp(1000, 8),
+    stEthPrice: exp(1000, 18),
     tokensPerStEth: exp(.8, 18),
-    result: exp(1250, 8)
+    result: exp(1250, 18)
   },
   {
-    stEthPrice: exp(-1000, 8),
+    stEthPrice: exp(-1000, 18),
     tokensPerStEth: exp(.8, 18),
-    result: exp(-1250, 8)
+    result: exp(-1250, 18)
   },
 ];
 
 describe('wstETH price feed', function () {
+  describe('getRoundData', function () {
+    for (const { stEthPrice, tokensPerStEth, result } of testCases) {
+      it(`stEthPrice (${stEthPrice}), tokensPerStEth (${tokensPerStEth}) -> ${result}`, async () => {
+        const { wstETHPriceFeed } = await makeWstETH({ stEthPrice, tokensPerStEth });
+        const roundId = 999;
+        const latestRoundData = await wstETHPriceFeed.getRoundData(roundId);
+        const price = latestRoundData.answer.toBigInt();
+
+        expect(price).to.eq(result);
+      });
+    }
+
+    it('passes along roundId, startedAt, updatedAt and answeredInRound values from stETH price feed', async () => {
+      const { stETHPriceFeed, wstETHPriceFeed } = await makeWstETH({
+        stEthPrice: exp(1000, 18),
+        tokensPerStEth: exp(.8, 18),
+      });
+
+      await stETHPriceFeed.setRoundData(
+        exp(15, 18), // roundId_,
+        1,           // answer_,
+        exp(16, 8),  // startedAt_,
+        exp(17, 8),  // updatedAt_,
+        exp(18, 18)  // answeredInRound_
+      );
+
+      const {
+        roundId,
+        startedAt,
+        updatedAt,
+        answeredInRound
+      } = await wstETHPriceFeed.getRoundData(exp(15, 18));
+
+      expect(roundId.toBigInt()).to.eq(exp(15, 18));
+      expect(startedAt.toBigInt()).to.eq(exp(16, 8));
+      expect(updatedAt.toBigInt()).to.eq(exp(17, 8));
+      expect(answeredInRound.toBigInt()).to.eq(exp(18, 18));
+    });
+  });
+
   describe('latestRoundData', function () {
     for (const { stEthPrice, tokensPerStEth, result } of testCases) {
       it(`stEthPrice (${stEthPrice}), tokensPerStEth (${tokensPerStEth}) -> ${result}`, async () => {
@@ -73,7 +113,7 @@ describe('wstETH price feed', function () {
 
     it('passes along roundId, startedAt, updatedAt and answeredInRound values from stETH price feed', async () => {
       const { stETHPriceFeed, wstETHPriceFeed } = await makeWstETH({
-        stEthPrice: exp(1000, 8),
+        stEthPrice: exp(1000, 18),
         tokensPerStEth: exp(.8, 18),
       });
 
@@ -97,16 +137,5 @@ describe('wstETH price feed', function () {
       expect(updatedAt.toBigInt()).to.eq(exp(17, 8));
       expect(answeredInRound.toBigInt()).to.eq(exp(18, 18));
     });
-  });
-
-  it(`getRoundData > always reverts`, async () => {
-    const { wstETHPriceFeed } = await makeWstETH({
-      stEthPrice: exp(1000, 8),
-      tokensPerStEth: exp(.2, 18)
-    });
-
-    await expect(
-      wstETHPriceFeed.getRoundData(1)
-    ).to.be.revertedWith("custom error 'NotImplemented()'");
   });
 });
