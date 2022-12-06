26dd67777c2195a9628e3c7cf54f0ca9e0705b8b
Scott Silver <scott@compound.finance>
2022-12-06 08:58:09 -0800

    progress as of 12/6

---
+++ forge/scripts/get-1inch-swap.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const chainId = 1;
+const apiBaseUrl = 'https://api.1inch.io/v5.0/' + chainId;
+
+function apiRequestUrl(methodName, queryParams) {
+  return apiBaseUrl + methodName + '?' + (new URLSearchParams(queryParams)).toString();
+}
+
+async function main() {
+  const [
+    _nodeCmd,
+    _filename,
+    liquidatorAddress,
+    fromTokenAddress,
+    toTokenAddress,
+    assetAmount
+  ] = process.argv;
+
+  const swapParams = {
+    fromTokenAddress,
+    toTokenAddress,
+    amount: assetAmount,
+    fromAddress: liquidatorAddress,
+    slippage: 2,
+    disableEstimate: true,
+    allowPartialFill: false,
+  };
+  const url = apiRequestUrl('/swap', swapParams);
+  const { data } = await axios.get(url);
+
+  // console.log({
+  //   target: data.tx.to,
+  //   tx: data.tx.data
+  // });
+
+  process.stdout.write(JSON.stringify({
+    target: data.tx.to,
+    tx: data.tx.data
+  }));
+}
+
+main();
