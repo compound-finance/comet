5ae45aaa4cfe9a8e2a0ba221e1ff087bf5d87c4d
Scott Silver <scott@compound.finance>
2022-12-08 10:26:16 -0800

    make scenario work across multiple assets

--- scenario/LiquidationBotV2Scenario.ts
+++ scenario/LiquidationBotV2Scenario.ts
@@ -2,66 +2,81 @@ import { scenario } from './context/CometContext';
 import { expect } from 'chai';
 import { LiquidatorV2 } from '../build/types';
 import { attemptLiquidation } from '../scripts/liquidation_bot/liquidateUnderwaterBorrowers';
-import CometActor from './context/CometActor';
-import { CometInterface } from '../build/types';
 import { isValidAssetIndex, MAX_ASSETS, timeUntilUnderwater } from './utils';
-import { exp } from '../test/helpers';
 
 const LIQUIDATOR_EOA = "0x5a13D329A193ca3B1fE2d7B459097EdDba14C28F";
 const UNISWAP_V3_FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
 const WETH9 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
-scenario.only(
-  'LiquidationBotV2 > XXX TEST',
-  {
-    tokenBalances: {
-      albert: {
-        $asset1: ' == 120'
+for (let i = 0; i < MAX_ASSETS; i++) {
+  const assetAmounts = [
+    // COMP
+    500,
+    // WBTC
+    120,
+    // WETH
+    5000,
+    // UNI
+    150000,
+    // LINK
+    250000,
+  ];
+
+  scenario.only(
+    `LiquidationBotV2 > liquidates an underwater position of $asset${i} ${assetAmounts[i] || ''} with no maxCollateralPurchase`,
+    {
+      filter: async (ctx) => ctx.world.base.network === 'mainnet' && await isValidAssetIndex(ctx, i),
+      tokenBalances: {
+        albert: {
+          [`$asset${i}`]: assetAmounts[i] ? ` == ${assetAmounts[i]}` : 0
+        },
       },
     },
-  },
-  async ({ comet, actors, assets }, context, world) => {
-    const { albert, betty, charles } = actors;
-    const { USDC, COMP, WBTC, WETH, UNI, LINK } = assets;
+    async ({ comet, actors, assets }, _context, world) => {
+      const { albert, betty } = actors;
+      const { USDC } = assets;
 
-    const liquidator = await world.deploymentManager.deploy(
-      'liquidator',
-      'liquidator/LiquidatorV2.sol',
-      [
+      const liquidator = await world.deploymentManager.deploy(
+        'liquidator',
+        'liquidator/LiquidatorV2.sol',
+        [
+          comet.address,
+          UNISWAP_V3_FACTORY_ADDRESS, // _factory
+          WETH9, // _WETH9
+          LIQUIDATOR_EOA // recipient
+        ]
+      ) as LiquidatorV2;
+
+      const initialRecipientBalance = await USDC.balanceOf(LIQUIDATOR_EOA);
+      const initialReserves = (await comet.getReserves()).toBigInt();
+
+      const { asset: collateralAssetAddress, scale } = await comet.getAssetInfo(i);
+
+      // transfer an amount of the asset to the protocol, so it will sell
+      await albert.transferErc20(
+        collateralAssetAddress,
         comet.address,
-        UNISWAP_V3_FACTORY_ADDRESS, // _factory
-        WETH9, // _WETH9
-        LIQUIDATOR_EOA // recipient
-      ]
-    ) as LiquidatorV2;
+        scale.mul(assetAmounts[i]).toBigInt()
+      );
 
-    const initialRecipientBalance = await USDC.balanceOf(LIQUIDATOR_EOA);
-    const initialReserves = (await comet.getReserves()).toBigInt();
+      await attemptLiquidation(
+        comet,
+        liquidator,
+        [],
+        {
+          signer: betty.signer
+        },
+        'mainnet'
+      );
 
-    // XXX delete
-    const i = 1;
+      // confirm that protocol reserves have increased
+      expect(await comet.getReserves()).to.be.above(initialReserves);
 
-    const { asset: collateralAssetAddress, scale } = await comet.getAssetInfo(i);
+      // confirm is not holding a significant amount of the collateral asset
+      expect(await comet.getCollateralReserves(collateralAssetAddress)).to.be.below(scale);
 
-    await albert.transferErc20(WBTC.address, comet.address, exp(120, 8));
-
-    await attemptLiquidation(
-      comet,
-      liquidator,
-      [],
-      {
-        signer: charles.signer
-      },
-      'mainnet'
-    );
-
-    // confirm that protocol reserves have increased
-    expect(await comet.getReserves()).to.be.above(initialReserves);
-
-    // confirm is not holding a significant amount of the collateral asset
-    expect(await comet.getCollateralReserves(collateralAssetAddress)).to.be.below(scale);
-
-    // check that recipient balance increased
-    expect(await USDC.balanceOf(LIQUIDATOR_EOA)).to.be.greaterThan(Number(initialRecipientBalance));
-  }
-)
+      // check that recipient balance increased
+      expect(await USDC.balanceOf(LIQUIDATOR_EOA)).to.be.greaterThan(Number(initialRecipientBalance));
+    }
+  )
+}
