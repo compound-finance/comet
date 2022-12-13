cdf71acccf84176c1de89503e3a0f9d243e108d0
Scott Silver <scott@compound.finance>
2022-12-13 14:52:03 -0800

    separate getSwapInfo function

--- scripts/liquidation_bot/liquidateUnderwaterBorrowers.ts
+++ scripts/liquidation_bot/liquidateUnderwaterBorrowers.ts
@@ -42,6 +42,59 @@ function apiRequestUrl(methodName, queryParams) {
   return apiBaseUrl + methodName + '?' + (new URLSearchParams(queryParams)).toString();
 }
 
+export async function getSwapInfo(
+  comet: CometInterface,
+  liquidator: LiquidatorV2,
+  targetAddresses: string[]
+) {
+  // get the amount of collateral available for sale (using static call)
+  const [
+    assets,
+    collateralReserves,
+    collateralReservesInBase
+  ] = await liquidator.callStatic.availableCollateral(comet.address, targetAddresses);
+
+  const baseToken = await comet.baseToken();
+
+  let swapAssets = [];
+  let swapTargets = [];
+  let swapCallDatas = [];
+
+  // for each amount, if it is high enough, get a quote
+  for (const i in assets) {
+    const asset = assets[i];
+    const collateralReserveAmount = collateralReserves[i];
+    const collateralReserveAmountInBase = collateralReservesInBase[i];
+
+    // check if collateralReserveAmountInBase is greater than threshold
+    const liquidationThreshold = 1e6; // XXX increase, denominate in base scale
+
+    if (collateralReserveAmountInBase.toBigInt() > liquidationThreshold) {
+      const swapParams = {
+        fromTokenAddress: asset,
+        toTokenAddress: baseToken,
+        amount: collateralReserveAmount,
+        fromAddress: liquidator.address,
+        slippage: 2,
+        disableEstimate: true,
+        allowPartialFill: false,
+      };
+      const url = apiRequestUrl('/swap', swapParams);
+      const { data } = await axios.get(url);
+
+      swapAssets.push(asset);
+      swapTargets.push(data.tx.to);
+      swapCallDatas.push(data.tx.data);
+    }
+  }
+
+  return {
+    swapAssets,
+    swapTargets,
+    swapCallDatas
+  };
+}
+
 export async function attemptLiquidation(
   comet: CometInterface,
   liquidator: LiquidatorV2,
@@ -54,46 +107,7 @@ export async function attemptLiquidation(
 
     const flashLoanPool = flashLoanPools[network];
 
-    // get the amount of collateral available for sale (using static call)
-    const [
-      assets,
-      collateralReserves,
-      collateralReservesInBase
-    ] = await liquidator.callStatic.availableCollateral(comet.address, targetAddresses);
-
-    const baseToken = await comet.baseToken();
-
-    let swapAssets = [];
-    let swapTargets = [];
-    let swapCallDatas = [];
-
-    // for each amount, if it is high enough, get a quote
-    for (const i in assets) {
-      const asset = assets[i];
-      const collateralReserveAmount = collateralReserves[i];
-      const collateralReserveAmountInBase = collateralReservesInBase[i];
-
-      // check if collateralReserveAmountInBase is greater than threshold
-      const liquidationThreshold = 1e6; // XXX increase, denominate in base scale
-
-      if (collateralReserveAmountInBase.toBigInt() > liquidationThreshold) {
-        const swapParams = {
-          fromTokenAddress: asset,
-          toTokenAddress: baseToken,
-          amount: collateralReserveAmount,
-          fromAddress: liquidator.address,
-          slippage: 2,
-          disableEstimate: true,
-          allowPartialFill: false,
-        };
-        const url = apiRequestUrl('/swap', swapParams);
-        const { data } = await axios.get(url);
-
-        swapAssets.push(asset);
-        swapTargets.push(data.tx.to);
-        swapCallDatas.push(data.tx.data);
-      }
-    }
+    const { swapAssets, swapTargets, swapCallDatas } = await getSwapInfo(comet, liquidator, targetAddresses);
 
     const args: [
       string,
@@ -146,6 +160,7 @@ async function getUniqueAddresses(comet: CometInterface): Promise<Set<string>> {
   return new Set(withdrawEvents.map(event => event.args.src));
 }
 
+// XXX
 export async function hasPurchaseableCollateral(comet: CometInterface, assets: Asset[], minUsdValue: number = 100): Promise<boolean> {
   let totalValue = 0n;
   const minValue = exp(minUsdValue, 8);
