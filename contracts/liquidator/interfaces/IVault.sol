6ad6f9e6384c2331450cf970acf769a3f68d1c74
scott-silver <scott@compound.finance>
2023-01-05 11:26:20 -0800

    OnChainLiquidator (#642)
    Update the reference liquidator to source from more pools and generally be more robust

---
+++ contracts/liquidator/interfaces/IVault.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+/**
+ * @dev Interfaces for interacting with Balancer Vaults
+ * Note Not comprehensive
+ */
+interface IVault {
+    enum SwapKind { GIVEN_IN, GIVEN_OUT }
+
+    struct BatchSwapStep {
+        bytes32 poolId;
+        uint256 assetInIndex;
+        uint256 assetOutIndex;
+        uint256 amount;
+        bytes userData;
+    }
+
+    struct FundManagement {
+        address sender;
+        bool fromInternalBalance;
+        address payable recipient;
+        bool toInternalBalance;
+    }
+
+    function batchSwap(
+        SwapKind kind,
+        BatchSwapStep[] memory swaps,
+        IAsset[] memory assets,
+        FundManagement memory funds,
+        int256[] memory limits,
+        uint256 deadline
+    ) external payable returns (int256[] memory);
+}
+
+interface IAsset {}
