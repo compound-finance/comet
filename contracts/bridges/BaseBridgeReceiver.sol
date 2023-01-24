4b9bcb51fb598639a2e26b77ab9306cd9d2110a4
Scott Silver <scott@compound.finance>
2023-01-23 17:20:52 -0800

    L-04 Lack of indexed parameter

--- contracts/bridges/BaseBridgeReceiver.sol
+++ contracts/bridges/BaseBridgeReceiver.sol
@@ -17,7 +17,7 @@ contract BaseBridgeReceiver {
     event NewLocalTimelock(address indexed oldLocalTimelock, address indexed newLocalTimelock);
     event NewGovTimelock(address indexed oldGovTimelock, address indexed newGovTimelock);
     event ProposalCreated(address indexed messageSender, uint id, address[] targets, uint[] values, string[] signatures, bytes[] calldatas, uint eta);
-    event ProposalExecuted(uint id);
+    event ProposalExecuted(uint indexed id);
 
     /** Public variables **/
 
