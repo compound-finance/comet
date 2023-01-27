c0fd8508ca4aaeeda5a85abf0917c52dfb1b92d3
Scott Silver <scott@compound.finance>
2023-01-27 14:32:10 -0800

    WMATIC as alias

--- deployments/polygon/usdc/deploy.ts
+++ deployments/polygon/usdc/deploy.ts
@@ -13,7 +13,7 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   const USDC = await deploymentManager.existing('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', 'polygon');
   const WBTC = await deploymentManager.existing('WBTC', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', 'polygon');
   const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', 'polygon');
-  const WMATIC = await deploymentManager.existing('WETH', '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 'polygon');
+  const WMATIC = await deploymentManager.existing('WMATIC', '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 'polygon');
 
   const fxChild = await deploymentManager.existing('fxChild', '0x8397259c983751DAf40400790063935a11afa28a', 'polygon');
 
