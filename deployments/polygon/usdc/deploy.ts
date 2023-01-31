ceb93c8be08e4f61c315d79a08b4f97a32354270
scott-silver <scott@compound.finance>
2023-01-30 16:19:49 -0800

    Polygon collateral assets (#668)
    * update assets for polygon
    
    * update deploy script
    
    * WMATIC as alias
    
    * reorder assets
    
    * update bulker scens
    
    * lint
    
    * use WBTC / USD price feed
    
    * supply caps to 0
    
    * bump supply cap for WMATIC

--- deployments/polygon/usdc/deploy.ts
+++ deployments/polygon/usdc/deploy.ts
@@ -11,10 +11,9 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
 
   // pull in existing assets
   const USDC = await deploymentManager.existing('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', 'polygon');
-  const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', 'polygon');
   const WBTC = await deploymentManager.existing('WBTC', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', 'polygon');
-  const DAI = await deploymentManager.existing('DAI', '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', 'polygon');
-  const USDT = await deploymentManager.existing('USDT', '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', 'polygon');
+  const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', 'polygon');
+  const WMATIC = await deploymentManager.existing('WMATIC', '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 'polygon');
 
   const fxChild = await deploymentManager.existing('fxChild', '0x8397259c983751DAf40400790063935a11afa28a', 'polygon');
 
@@ -58,10 +57,10 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   // Deploy Bulker
   const bulker = await deploymentManager.deploy(
     'bulker',
-    'Bulker.sol',
+    'bulkers/BaseBulker.sol',
     [
-      await comet.governor(),      // admin
-      ethers.constants.AddressZero // weth (zero address, since Polygon deployment does not include WMATIC)
+      await comet.governor(), // admin
+      WMATIC.address          // wrappedNativeToken
     ]
   );
 
