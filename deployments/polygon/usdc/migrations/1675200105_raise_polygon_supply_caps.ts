f0e9f9d1010ceb0ac0a23743e8ac66aeadb2cf4a
kevincheng96 <kevincheng96@hotmail.com>
2023-02-13 13:08:08 -0800

    Set entire Comet configuration

--- deployments/polygon/usdc/migrations/1675200105_raise_polygon_supply_caps.ts
+++ deployments/polygon/usdc/migrations/1675200105_raise_polygon_supply_caps.ts
@@ -1,6 +1,6 @@
 import { DeploymentManager } from '../../../../plugins/deployment_manager/DeploymentManager';
 import { migration } from '../../../../plugins/deployment_manager/Migration';
-import { exp, proposal } from '../../../../src/deploy';
+import { calldata, exp, getConfigurationStruct, proposal } from '../../../../src/deploy';
 import { expect } from 'chai';
 
 export default migration('1675200105_raise_polygon_supply_caps', {
@@ -8,15 +8,15 @@ export default migration('1675200105_raise_polygon_supply_caps', {
     return {};
   },
 
-  enact: async (deploymentManager: DeploymentManager, governanceDeploymentManager: DeploymentManager) => {
+  enact: async (
+    deploymentManager: DeploymentManager,
+    governanceDeploymentManager: DeploymentManager
+  ) => {
     const trace = deploymentManager.tracer();
     const ethers = deploymentManager.hre.ethers;
     const { utils } = ethers;
 
-    const {
-      fxRoot,
-      governor
-    } = await governanceDeploymentManager.getContracts();
+    const { fxRoot, governor } = await governanceDeploymentManager.getContracts();
 
     const {
       bridgeReceiver,
@@ -28,17 +28,10 @@ export default migration('1675200105_raise_polygon_supply_caps', {
       WMATIC
     } = await deploymentManager.getContracts();
 
-    const wbtcCalldata = utils.defaultAbiCoder.encode(
-      ['address', 'address', 'uint128'],
-      [comet.address, WBTC.address, exp(10_000, 8)] // XXX add actual amount
-    );
-    const wethCalldata = utils.defaultAbiCoder.encode(
-      ['address', 'address', 'uint128'],
-      [comet.address, WETH.address, exp(10_000, 18)] // XXX add actual amount
-    );
-    const wmaticCalldata = utils.defaultAbiCoder.encode(
-      ['address', 'address', 'uint128'],
-      [comet.address, WMATIC.address, exp(10_000, 18)] // XXX add actual amount
+    const configuration = await getConfigurationStruct(deploymentManager);
+
+    const setConfigurationCalldata = await calldata(
+      configurator.populateTransaction.setConfiguration(comet.address, configuration)
     );
     const deployAndUpgradeToCalldata = utils.defaultAbiCoder.encode(
       ['address', 'address'],
@@ -48,25 +41,13 @@ export default migration('1675200105_raise_polygon_supply_caps', {
     const l2ProposalData = utils.defaultAbiCoder.encode(
       ['address[]', 'uint256[]', 'string[]', 'bytes[]'],
       [
+        [configurator.address, cometAdmin.address],
+        [0, 0],
         [
-          configurator.address,
-          configurator.address,
-          configurator.address,
-          cometAdmin.address
+          'setConfiguration(address,(address,address,address,address,address,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint104,uint104,uint104,(address,address,uint8,uint64,uint64,uint64,uint128)[]))',
+          'deployAndUpgradeTo(address,address)'
         ],
-        [0, 0, 0, 0],
-        [
-          "updateAssetSupplyCap(address,address,uint128)",
-          "updateAssetSupplyCap(address,address,uint128)",
-          "updateAssetSupplyCap(address,address,uint128)",
-          "deployAndUpgradeTo(address,address)",
-        ],
-        [
-          wbtcCalldata,
-          wethCalldata,
-          wmaticCalldata,
-          deployAndUpgradeToCalldata
-        ]
+        [setConfigurationCalldata, deployAndUpgradeToCalldata]
       ]
     );
 
@@ -74,13 +55,13 @@ export default migration('1675200105_raise_polygon_supply_caps', {
       {
         contract: fxRoot,
         signature: 'sendMessageToChild(address,bytes)',
-        args: [bridgeReceiver.address, l2ProposalData],
+        args: [bridgeReceiver.address, l2ProposalData]
       }
     ];
 
-    const description = ""; // XXX add description
-    const txn = await governanceDeploymentManager.retry(
-      async () => trace((await governor.propose(...await proposal(mainnetActions, description))))
+    const description = ''; // XXX add description
+    const txn = await governanceDeploymentManager.retry(async () =>
+      trace(await governor.propose(...(await proposal(mainnetActions, description))))
     );
 
     const event = txn.events.find(event => event.event === 'ProposalCreated');
@@ -90,12 +71,7 @@ export default migration('1675200105_raise_polygon_supply_caps', {
   },
 
   async verify(deploymentManager: DeploymentManager) {
-    const {
-      comet,
-      WBTC,
-      WETH,
-      WMATIC
-    } = await deploymentManager.getContracts();
+    const { comet, WBTC, WETH, WMATIC } = await deploymentManager.getContracts();
 
     const wbtcInfo = await comet.getAssetInfoByAddress(WBTC.address);
     const wethInfo = await comet.getAssetInfoByAddress(WETH.address);
@@ -104,5 +80,5 @@ export default migration('1675200105_raise_polygon_supply_caps', {
     expect(await wbtcInfo.supplyCap).to.be.eq(exp(10_000, 8));
     expect(await wethInfo.supplyCap).to.be.eq(exp(10_000, 18));
     expect(await wmaticInfo.supplyCap).to.be.eq(exp(10_000, 18));
-  },
+  }
 });
