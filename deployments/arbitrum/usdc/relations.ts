0c2152646fbb8d3ab67476b3831ebce5495a2c00
Scott Silver <scott@compound.finance>
2023-03-09 19:03:24 -0800

    add TransparentUpgradeableProxy to relations

--- deployments/arbitrum/usdc/relations.ts
+++ deployments/arbitrum/usdc/relations.ts
@@ -6,4 +6,12 @@ export default {
   'governor': {
     artifact: 'contracts/bridges/arbitrum/ArbitrumBridgeReceiver.sol:ArbitrumBridgeReceiver',
   },
+  TransparentUpgradeableProxy: {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
+      }
+    },
+  },
 };
