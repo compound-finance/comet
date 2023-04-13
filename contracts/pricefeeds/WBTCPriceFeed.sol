b31b0165ec572d2f08c26a771e0e960bfc1d290b
kevincheng96 <kevincheng96@hotmail.com>
2023-04-13 15:50:24 -0700

    Update docstrings

--- contracts/pricefeeds/WBTCPriceFeed.sol
+++ contracts/pricefeeds/WBTCPriceFeed.sol
@@ -35,12 +35,17 @@ contract WBTCPriceFeed is IPriceFeed {
     /// @notice Scale of this price feed
     int public immutable priceFeedScale;
 
+    /**
+     * @notice Construct a new WBTC / USD price feed
+     * @param WBTCToBTCPriceFeed_ The address of the WBTC / BTC price feed to fetch prices from
+     * @param BTCToUSDPriceFeed_ The address of the BTC / USD price feed to fetch prices from
+     * @param decimals_ The number of decimals for the returned prices
+     **/
     constructor(address WBTCToBTCPriceFeed_, address BTCToUSDPriceFeed_, uint8 decimals_) {
         WBTCToBTCPriceFeed = WBTCToBTCPriceFeed_;
         BTCToUSDPriceFeed = BTCToUSDPriceFeed_;
         uint8 WBTCToBTCPriceFeedDecimals = AggregatorV3Interface(WBTCToBTCPriceFeed_).decimals();
         uint8 BTCToUSDPriceFeedDecimals = AggregatorV3Interface(BTCToUSDPriceFeed_).decimals();
-
         combinedScale = signed256(10 ** (WBTCToBTCPriceFeedDecimals + BTCToUSDPriceFeedDecimals));
 
         if (decimals_ > 18) revert BadDecimals();
@@ -48,18 +53,13 @@ contract WBTCPriceFeed is IPriceFeed {
         priceFeedScale = int256(10 ** decimals);
     }
 
-    function signed256(uint256 n) internal pure returns (int256) {
-        if (n > uint256(type(int256).max)) revert InvalidInt256();
-        return int256(n);
-    }
-
     /**
      * @notice WBTC price for the latest round
-     * @return roundId Round id from the WBTC price feed
+     * @return roundId Round id from the BTC / USD price feed
      * @return answer Latest price for WBTC / USD
-     * @return startedAt Timestamp when the round was started; passed on from WBTC price feed
-     * @return updatedAt Timestamp when the round was last updated; passed on from WBTC price feed
-     * @return answeredInRound Round id in which the answer was computed; passed on from WBTC price feed
+     * @return startedAt Timestamp when the round was started; passed on from the BTC / USD price feed
+     * @return updatedAt Timestamp when the round was last updated; passed on from the BTC / USD price feed
+     * @return answeredInRound Round id in which the answer was computed; passed on from the BTC / USD price feed
      **/
     function latestRoundData() override external view returns (
         uint80 roundId,
@@ -71,10 +71,15 @@ contract WBTCPriceFeed is IPriceFeed {
         (, int256 WBTCToBTCPrice, , , ) = AggregatorV3Interface(WBTCToBTCPriceFeed).latestRoundData();
         (uint80 roundId_, int256 BTCToUSDPrice, uint256 startedAt_, uint256 updatedAt_, uint80 answeredInRound_) = AggregatorV3Interface(BTCToUSDPriceFeed).latestRoundData();
 
-        // We use return the round data of the BTC / USD price feed because of its shorter heartbeat (1hr vs 24hr)
+        // We return the round data of the BTC / USD price feed because of its shorter heartbeat (1hr vs 24hr)
         if (WBTCToBTCPrice <= 0 || BTCToUSDPrice <= 0) return (roundId_, 0, startedAt_, updatedAt_, answeredInRound_);
 
         int256 price = WBTCToBTCPrice * BTCToUSDPrice * priceFeedScale / combinedScale;
         return (roundId_, price, startedAt_, updatedAt_, answeredInRound_);
+    }
+
+    function signed256(uint256 n) internal pure returns (int256) {
+        if (n > uint256(type(int256).max)) revert InvalidInt256();
+        return int256(n);
     }
 }
