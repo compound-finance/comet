a5b7514f24bcb79a1cbb7494ec7c88f8539c33b0
kevincheng96 <kevincheng96@hotmail.com>
2023-04-13 15:39:51 -0700

    Move price feeds to /pricefeeds sub-directory

--- contracts/pricefeeds/ConstantPriceFeed.sol
+++ contracts/pricefeeds/ConstantPriceFeed.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.15;
 
-import "./IPriceFeed.sol";
+import "../IPriceFeed.sol";
 
 /**
  * @title Constant price feed
--- contracts/pricefeeds/WstETHPriceFeed.sol
+++ contracts/pricefeeds/WstETHPriceFeed.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.15;
 
-import "./vendor/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./IPriceFeed.sol";
-import "./IWstETH.sol";
+import "../vendor/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../IPriceFeed.sol";
+import "../IWstETH.sol";
 
 /**
  * @title wstETH price feed
