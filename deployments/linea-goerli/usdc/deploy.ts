4fa8b88b0b887800b34b93a84a6b6671ae580981
Arthur <arthur@coeos.xyz>
2023-05-29 15:35:09 +0200

    feat: deployment script - adapt for Linea

--- deployments/linea-goerli/usdc/deploy.ts
+++ deployments/linea-goerli/usdc/deploy.ts
@@ -1,21 +1,11 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { DeploySpec, deployComet, exp, wait } from '../../../src/deploy';
 
-const clone = {
-  op: '0x4200000000000000000000000000000000000042'
-};
-
 const secondsPerDay = 24 * 60 * 60;
 
 const GOERLI_TIMELOCK = '0x8Fa336EB4bF58Cfc508dEA1B0aeC7336f55B1399';
 
-export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
-  const deployed = await deployContracts(deploymentManager, deploySpec);
-  await mintTokens(deploymentManager);
-  return deployed;
-}
-
-async function deployContracts(
+export default async function deploy(
   deploymentManager: DeploymentManager,
   deploySpec: DeploySpec
 ): Promise<Deployed> {
@@ -23,43 +13,20 @@ async function deployContracts(
   const ethers = deploymentManager.hre.ethers;
 
   // Pull in existing assets
-  const USDC = await deploymentManager.existing(
-    'USDC',
-    '0x7E07E15D2a87A24492740D16f5bdF58c16db0c4E',
-    'optimism-goerli'
-  );
-  const WETH = await deploymentManager.existing(
-    'WETH',
-    '0x4200000000000000000000000000000000000006',
-    'optimism-goerli'
-  );
-  const WBTC = await deploymentManager.existing(
-    'WBTC',
-    '0xe0a592353e81a94Db6E3226fD4A99F881751776a',
-    'optimism-goerli',
-    'contracts/ERC20.sol:ERC20'
+  const USDC = await deploymentManager.existing('USDC', '0x5F4c18bF60F2A757E058EfB1A155637A596347cE', 'linea-goerli');
+  const WETH = await deploymentManager.existing('WETH', '0x2C1b868d6596a18e32E61B901E4060C872647b6C', 'linea-goerli');
+
+  const l2MessageService = await deploymentManager.existing(
+    'l2MessageService',
+    '0xA59477f7742Ba7d51bb1E487a8540aB339d6801d',
+    'linea-goerli'
   );
 
-  // Clone OP so we can mint it
-  const OP = await deploymentManager.clone('OP', clone.op, [], 'optimism');
-
-  const l2CrossDomainMessenger = await deploymentManager.existing(
-    'l2CrossDomainMessenger',
-    ['0xC0d3c0d3c0D3c0D3C0d3C0D3C0D3c0d3c0d30007', '0x4200000000000000000000000000000000000007'],
-    'optimism-goerli'
-  );
-
-  const l2StandardBridge = await deploymentManager.existing(
-    'l2StandardBridge',
-    ['0xC0d3c0d3c0D3c0d3C0D3c0D3C0d3C0D3C0D30010', '0x4200000000000000000000000000000000000010'],
-    'optimism-goerli'
-  );
-  
-  // Deploy OptimismBridgeReceiver
+  // Deploy LineaBridgeReceiver
   const bridgeReceiver = await deploymentManager.deploy(
     'bridgeReceiver',
-    'bridges/optimism/OptimismBridgeReceiver.sol',
-    [l2CrossDomainMessenger.address]
+    'bridges/linea-goerli/LineaBridgeReceiver.sol',
+    [l2MessageService.address]
   );
 
   // Deploy Local Timelock
@@ -75,7 +42,7 @@ async function deployContracts(
     ]
   );
 
-  // Initialize PolygonBridgeReceiver
+  // Initialize BridgeReceiver
   await deploymentManager.idempotent(
     async () => !(await bridgeReceiver.initialized()),
     async () => {
@@ -102,34 +69,9 @@ async function deployContracts(
     ]
   );
 
-  // Deploy fauceteer
-  const fauceteer = await deploymentManager.deploy('fauceteer', 'test/Fauceteer.sol', []);
-
   return {
     ...deployed,
     bridgeReceiver,
-    l2CrossDomainMessenger,
-    l2StandardBridge,
-    bulker,
-    fauceteer
+    bulker
   };
-}
-
-async function mintTokens(deploymentManager: DeploymentManager) {
-  const trace = deploymentManager.tracer();
-  const signer = await deploymentManager.getSigner();
-  const fauceteer = await deploymentManager.getContractOrThrow('fauceteer');
-
-  trace(`Attempting to mint as ${signer.address}...`);
-
-  const OP = await deploymentManager.getContractOrThrow('OP');
-  await deploymentManager.idempotent(
-    async () => (await OP.balanceOf(fauceteer.address)).eq(0),
-    async () => {
-      trace(`Minting 50M OP to fauceteer`);
-      const amount = exp(50_000_000, await OP.decimals());
-      trace(await wait(OP.connect(signer).mint(fauceteer.address, amount)));
-      trace(`OP.balanceOf(${fauceteer.address}): ${await OP.balanceOf(fauceteer.address)}`);
-    }
-  );
 }
