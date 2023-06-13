28bc93d9b51e9216d7fb8fa178a81a60850e2b77
julink-eth <julien.fontanel12@gmail.com>
2023-06-13 11:58:01 +0200

    feat: mage LineaBridgeReceiver fallback payable and removed comments

--- contracts/bridges/linea/LineaBridgeReceiver.sol
+++ contracts/bridges/linea/LineaBridgeReceiver.sol
@@ -9,13 +9,10 @@ contract LineaBridgeReceiver is BaseBridgeReceiver {
     IMessageService public messageService;
 
     constructor(address _messageService) {
-        // Can we keep this a constructor or should we extend the initialize
-        // function of BaseBridgeReceiver?
         messageService = IMessageService(_messageService);
     }
 
-    fallback() external {
-        // Should we keep it payable (see Arb)?
+    fallback() external payable {
         if (msg.sender != address(messageService)) revert Unauthorized();
         processMessage(messageService.sender(), msg.data);
     }
