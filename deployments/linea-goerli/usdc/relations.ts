0c2ea9518c25f08f2b682c6cf9c6e0f094567982
kevincheng96 <kevincheng96@hotmail.com>
2023-06-22 11:02:54 -0700

    More fixes

--- deployments/linea-goerli/usdc/relations.ts
+++ deployments/linea-goerli/usdc/relations.ts
@@ -20,13 +20,6 @@ export default {
       }
     }
   },
-  l2MessageService: {
-    delegates: {
-      field: {
-        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-      }
-    }
-  },
   l2TokenBridge: {
     delegates: {
       field: {
