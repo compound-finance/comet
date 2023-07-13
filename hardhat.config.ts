02ab794a4b669c82276422186ae2745fdf75d2a5
Manoj Patra <manoj9april@gmail.com>
2023-07-13 14:17:41 +0530

    add verify block

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -45,7 +45,6 @@ const {
   ARBISCAN_KEY,
   LINEASCAN_KEY,
   INFURA_KEY,
-  NODEREAL_KEY,
   MNEMONIC = 'myth like bonus scare over problem client lizard pioneer submit female collect',
   REPORT_GAS = 'false',
   NETWORK_PROVIDER = '',
@@ -94,7 +93,7 @@ const networkConfigs: NetworkConfig[] = [
   {
     network: 'polygon',
     chainId: 137,
-    url: `https://polygon-mainnet.nodereal.io/v1/${NODEREAL_KEY}`,
+       url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
   },
   {
     network: 'arbitrum',
