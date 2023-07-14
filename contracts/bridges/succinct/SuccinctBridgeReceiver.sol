0bd94f2ffb285463867f3ac58084d58b71814f5a
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-07-14 13:43:23 -0700

    chore: rm ITelepathy

--- contracts/bridges/succinct/SuccinctBridgeReceiver.sol
+++ contracts/bridges/succinct/SuccinctBridgeReceiver.sol
@@ -2,21 +2,26 @@
 pragma solidity 0.8.15;
 
 import "../SweepableBridgeReceiver.sol";
-import "./ITelepathy.sol";
+
+interface ITelepathyHandler {
+    function handleTelepathy(uint32 _sourceChainId, address _sourceAddress, bytes calldata _data)
+        external
+        returns (bytes4);
+}
 
 contract SuccinctBridgeReceiver is SweepableBridgeReceiver, ITelepathyHandler {
     error InvalidSourceChain();
 
-    ITelepathyReceiver public telepathyReceiver;
+    address public telepathyReceiver;
     uint32 public sourceChainId;
 
     constructor(address _telepathyReceiver, uint32 _sourceChainId) {
-        telepathyReceiver = ITelepathyReceiver(_telepathyReceiver);
+        telepathyReceiver = _telepathyReceiver;
         sourceChainId = _sourceChainId;
     }
 
     function handleTelepathy(uint32 _sourceChainId, address _sourceAddress, bytes calldata _data) external returns (bytes4)  {
-        if (msg.sender != address(telepathyReceiver)) revert Unauthorized();
+        if (msg.sender != telepathyReceiver) revert Unauthorized();
 
         if (_sourceChainId != sourceChainId) revert InvalidSourceChain();
 
