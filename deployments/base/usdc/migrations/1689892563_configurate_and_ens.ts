6164ddfec371277f4de0c57ed9a198d76de59aca
kevincheng96 <kevincheng96@hotmail.com>
2023-07-20 15:50:53 -0700

    Create proposal

---
+++ deployments/base/usdc/migrations/1689892563_configurate_and_ens.ts
@@ -0,0 +1,201 @@
+import { DeploymentManager } from '../../../../plugins/deployment_manager/DeploymentManager';
+import { diffState, getCometConfig } from '../../../../plugins/deployment_manager/DiffState';
+import { migration } from '../../../../plugins/deployment_manager/Migration';
+import { calldata, exp, getConfigurationStruct, proposal } from '../../../../src/deploy';
+import { expect } from 'chai';
+
+const ENSName = 'compound-community-licenses.eth';
+const ENSResolverAddress = '0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41';
+const ENSRegistryAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
+const ENSSubdomainLabel = 'v3-additional-grants';
+const ENSSubdomain = `${ENSSubdomainLabel}.${ENSName}`;
+const ENSTextRecordKey = 'v3-official-markets';
+const baseCOMPAddress = '0xA29b548056c3fD0f68BAd9d4829EC4E66f22f796'; // TODO
+
+export default migration('1679592519_configurate_and_ens', {
+  prepare: async (deploymentManager: DeploymentManager) => {
+    return {};
+  },
+
+  enact: async (deploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager) => {
+    const trace = deploymentManager.tracer();
+    const ethers = deploymentManager.hre.ethers;
+    const { utils } = ethers;
+
+    const {
+      bridgeReceiver,
+      comet,
+      cometAdmin,
+      configurator,
+      rewards,
+    } = await deploymentManager.getContracts();
+
+    const {
+      baseL1CrossDomainMessenger,
+      baseL1StandardBridge,
+      governor,
+      COMP: mainnetCOMP,
+    } = await govDeploymentManager.getContracts();
+
+    // ENS Setup
+    // See also: https://docs.ens.domains/contract-api-reference/name-processing
+    const ENSResolver = await govDeploymentManager.existing('ENSResolver', ENSResolverAddress);
+    const subdomainHash = ethers.utils.namehash(ENSSubdomain);
+    const baseChainId = (await deploymentManager.hre.ethers.provider.getNetwork()).chainId.toString();
+    const newMarketObject = { baseSymbol: 'USDC', cometAddress: comet.address };
+    const officialMarketsJSON = JSON.parse(await ENSResolver.text(subdomainHash, ENSTextRecordKey));
+    if (officialMarketsJSON[baseChainId]) {
+      officialMarketsJSON[baseChainId].push(newMarketObject);
+    } else {
+      officialMarketsJSON[baseChainId] = [newMarketObject];
+    }
+
+    const configuration = await getConfigurationStruct(deploymentManager);
+
+    const setConfigurationCalldata = await calldata(
+      configurator.populateTransaction.setConfiguration(comet.address, configuration)
+    );
+    const deployAndUpgradeToCalldata = utils.defaultAbiCoder.encode(
+      ['address', 'address'],
+      [configurator.address, comet.address]
+    );
+    const setRewardConfigCalldata = utils.defaultAbiCoder.encode(
+      ['address', 'address'],
+      [comet.address, baseCOMPAddress]
+    );
+    const l2ProposalData = utils.defaultAbiCoder.encode(
+      ['address[]', 'uint256[]', 'string[]', 'bytes[]'],
+      [
+        [configurator.address, cometAdmin.address, rewards.address],
+        [0, 0, 0],
+        [
+          'setConfiguration(address,(address,address,address,address,address,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint64,uint104,uint104,uint104,(address,address,uint8,uint64,uint64,uint64,uint128)[]))',
+          'deployAndUpgradeTo(address,address)',
+          'setRewardConfig(address,address)'
+        ],
+        [setConfigurationCalldata, deployAndUpgradeToCalldata, setRewardConfigCalldata]
+      ]
+    );
+
+    const COMPAmountToBridge = exp(12_500, 18);
+
+    // TODO: Bridge over USDC as well
+    const actions = [
+      // 1. Set Comet configuration + deployAndUpgradeTo new Comet and set reward config on Base.
+      {
+        contract: baseL1CrossDomainMessenger,
+        signature: 'sendMessage(address,bytes,uint32)',
+        args: [bridgeReceiver.address, l2ProposalData, 2_500_000]
+      },
+
+      // 2. Approve Ethereum's L1StandardBridge to take Timelock's COMP (for bridging)
+      {
+        contract: mainnetCOMP,
+        signature: 'approve(address,uint256)',
+        args: [baseL1StandardBridge.address, COMPAmountToBridge]
+      },
+      // 3. Bridge COMP from Ethereum to Base Comet using L1StandardBridge
+      {
+        contract: baseL1StandardBridge,
+        // function depositERC20To(address _l1Token, address _l2Token, address _to, uint256 _amount, uint32 _l2Gas,bytes calldata _data)
+        signature: 'depositERC20To(address,address,address,uint256,uint32,bytes)',
+        args: [mainnetCOMP.address, baseCOMPAddress, rewards.address, COMPAmountToBridge, 200_000, '0x']
+      },
+
+      // 4. Update the list of official markets
+      {
+        target: ENSResolverAddress,
+        signature: 'setText(bytes32,string,string)',
+        calldata: ethers.utils.defaultAbiCoder.encode(
+          ['bytes32', 'string', 'string'],
+          [subdomainHash, ENSTextRecordKey, JSON.stringify(officialMarketsJSON)]
+        )
+      },
+    ];
+
+    const description = "TODO"; // TODO
+    const txn = await govDeploymentManager.retry(async () =>
+      trace(await governor.propose(...(await proposal(actions, description))))
+    );
+
+    const event = txn.events.find(event => event.event === 'ProposalCreated');
+    const [proposalId] = event.args;
+
+    trace(`Created proposal ${proposalId}.`);
+  },
+
+  async enacted(deploymentManager: DeploymentManager): Promise<boolean> {
+    return true;
+  },
+
+  async verify(deploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager, preMigrationBlockNumber: number) {
+    const ethers = deploymentManager.hre.ethers;
+    await deploymentManager.spider(); // We spider here to pull in Base COMP now that reward config has been set
+
+    const {
+      comet,
+      rewards,
+      COMP,
+    } = await deploymentManager.getContracts();
+
+    // 1.
+    const stateChanges = await diffState(comet, getCometConfig, preMigrationBlockNumber);
+    // TODO
+    expect(stateChanges).to.deep.equal({
+      pauseGuardian: '0xBA5e81fD6811E2699b478d1Bcde62a585bC9b6f7',
+      baseTrackingSupplySpeed: exp(34.74 / 86400, 15, 18),
+      baseTrackingBorrowSpeed: exp(34.74 / 86400, 15, 18),
+      baseBorrowMin: exp(1, 6),
+      WETH: {
+        supplyCap: exp(1000, 18)
+      },
+      cbETH: {
+        supplyCap: exp(800, 18)
+      }
+    })
+
+    const config = await rewards.rewardConfig(comet.address);
+    expect(config.token).to.be.equal(COMP.address);
+    expect(config.rescaleFactor).to.be.equal(exp(1, 12));
+    expect(config.shouldUpscale).to.be.equal(true);
+
+    // 2. & 3.
+    expect(await COMP.balanceOf(rewards.address)).to.be.equal(exp(12_500, 18));
+
+    // 4.
+    const ENSResolver = await govDeploymentManager.existing('ENSResolver', ENSResolverAddress);
+    const subdomainHash = ethers.utils.namehash(ENSSubdomain);
+    const officialMarketsJSON = await ENSResolver.text(subdomainHash, ENSTextRecordKey);
+    const officialMarkets = JSON.parse(officialMarketsJSON);
+    expect(officialMarkets).to.deep.equal({
+      1: [
+        {
+          baseSymbol: 'USDC',
+          cometAddress: '0xc3d688B66703497DAA19211EEdff47f25384cdc3',
+        },
+        {
+          baseSymbol: 'WETH',
+          cometAddress: '0xA17581A9E3356d9A858b789D68B4d866e593aE94',
+        },
+      ],
+      137: [
+        {
+          baseSymbol: 'USDC',
+          cometAddress: '0xF25212E676D1F7F89Cd72fFEe66158f541246445',
+        },
+      ],
+      42161: [
+        {
+          baseSymbol: 'USDC',
+          cometAddress: '0xA5EDBDD9646f8dFF606d7448e414884C7d905dCA',
+        }
+      ],
+      8453: [
+        {
+          baseSymbol: 'USDC',
+          cometAddress: comet.address,
+        },
+      ],
+    });
+  }
+});
