376ec85027a50139ccd8c45d2565466042542f14
Hans Wang <hans@compound.finance>
2023-08-08 15:31:13 -0700

    USDT fork from mainnet for better consistent scenario cases

--- deployments/goerli/usdt/deploy.ts
+++ deployments/goerli/usdt/deploy.ts
@@ -1,12 +1,18 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { debug, DeploySpec, deployComet, exp, sameAddress, wait } from '../../../src/deploy';
 
+const clone = {
+  usdt: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+};
+
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
   const ethers = deploymentManager.hre.ethers;
   const signer = await deploymentManager.getSigner();
 
+  // Clone/fork USDT from mainnet which is non-standard erc20 to testnet
+  const USDT = await deploymentManager.clone('USDT', clone.usdt, [100_000_000_000_000, 'Tether USD', 'USDT', 6]);
+
   // Declare existing assets as aliases
-  const USDT = await deploymentManager.existing('USDT', '0x79C950C7446B234a6Ad53B908fBF342b01c4d446', 'goerli');
   const COMP = await deploymentManager.existing('COMP', '0x3587b2F7E0E2D6166d6C14230e7Fe160252B0ba4', 'goerli');
   const WBTC = await deploymentManager.existing('WBTC', '0xAAD4992D949f9214458594dF92B44165Fb84dC19', 'goerli');
   const WETH = await deploymentManager.existing('WETH', '0x42a71137C09AE83D8d05974960fd607d40033499', 'goerli');
