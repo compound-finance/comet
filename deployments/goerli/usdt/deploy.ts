3e0d000e0a7cfd2ea77a14b85309fd7b4d180991
Hans Wang <hans@compound.finance>
2023-08-08 18:25:17 -0700

    fork mainnet's usdt instead of compound's test usdt to match unstandard erc20 contract

--- deployments/goerli/usdt/deploy.ts
+++ deployments/goerli/usdt/deploy.ts
@@ -6,6 +6,7 @@ const clone = {
 };
 
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
+  const trace = deploymentManager.tracer();
   const ethers = deploymentManager.hre.ethers;
   const signer = await deploymentManager.getSigner();
 
@@ -27,6 +28,19 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   const fxRoot = await deploymentManager.fromDep('fxRoot', 'goerli', 'usdc');
   const bulker = await deploymentManager.fromDep('bulker', 'goerli', 'usdc');
   const timelock = await deploymentManager.fromDep('timelock', 'goerli', 'usdc');
+
+  // Send some forked USDT to timelock
+  await deploymentManager.idempotent(
+    async () => await USDT.connect(signer).balanceOf(timelock.address) == 0,
+    async () => {
+      trace(`Sending USDC to timelock`);
+      await USDT.connect(signer).transfer(
+        timelock.address,
+        exp(50_000_000, 6),
+      );
+      trace(`Sent USDC to timelock completed`);
+    }
+  );
 
   // Deploy all Comet-related contracts
   const deployed = await deployComet(deploymentManager, deploySpec);
--- scenario/ApproveThisScenario.ts
+++ scenario/ApproveThisScenario.ts
@@ -17,19 +17,22 @@ scenario('Comet#approveThis > allows governor to authorize and rescind authoriza
   expect(await comet.isAllowed(comet.address, timelock.address)).to.be.false;
 });
 
-scenario('Comet#approveThis > allows governor to authorize and rescind authorization for non-Comet ERC20', {}, async ({ comet, timelock, actors }, context) => {
+scenario.only('Comet#approveThis > allows governor to authorize and rescind authorization for non-Comet ERC20', {}, async ({ comet, timelock, actors }, context) => {
   const { admin } = actors;
   const baseTokenAddress = await comet.baseToken();
   const baseToken = context.getAssetByAddress(baseTokenAddress);
 
   const newAllowance = 999_888n;
   await context.setNextBaseFeeToZero();
+  console.log("Try Arppove#1");
   await admin.approveThis(timelock.address, baseTokenAddress, newAllowance, { gasPrice: 0 });
-
+  console.log("Approve#1")
   expect(await baseToken.allowance(comet.address, timelock.address)).to.be.equal(newAllowance);
 
   await context.setNextBaseFeeToZero();
+  console.log("Try Approve#2");
   await admin.approveThis(timelock.address, baseTokenAddress, 0, { gasPrice: 0 });
+  console.log("Approve#2")
 
   expect(await baseToken.allowance(comet.address, timelock.address)).to.be.equal(0n);
 });
