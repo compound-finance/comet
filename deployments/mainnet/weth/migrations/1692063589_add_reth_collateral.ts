7b0d81a742dfabb2f965ec3c11ac0093e495f464
langers <github@darrenlangley.co.uk>
2023-08-23 20:52:14 +1000

    Added scaling price feed and config verification

--- deployments/mainnet/weth/migrations/1692063589_add_reth_collateral.ts
+++ deployments/mainnet/weth/migrations/1692063589_add_reth_collateral.ts
@@ -1,4 +1,3 @@
-import { debug } from 'console';
 import { DeploymentManager } from '../../../../plugins/deployment_manager/DeploymentManager';
 import { migration } from '../../../../plugins/deployment_manager/Migration';
 import { exp, proposal } from '../../../../src/deploy'
@@ -9,26 +8,35 @@ interface Vars {};
 
 export default migration('1692063589_add_reth_collateral', {
   prepare: async (deploymentManager: DeploymentManager) => {
-    return {};
+    // Deploy scaling price feed for rETH
+    const rETHScalingPriceFeed = await deploymentManager.deploy(
+      'rETH:priceFeed',
+      'pricefeeds/ScalingPriceFeed.sol',
+      [
+        '0x536218f9E9Eb48863970252233c8F271f554C2d0', // rETH / ETH price feed
+        8                                             // decimals
+      ]
+    );
+    return { rETHPriceFeedAddress: rETHScalingPriceFeed.address };
   },
 
-  enact: async (governanceDeploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager, vars: Vars) => {
-    const trace = governanceDeploymentManager.tracer();
+  enact: async (deploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager, vars: Vars) => {
+    const trace = deploymentManager.tracer();
 
-    const reth = await governanceDeploymentManager.existing('rETH', '0xae78736Cd615f374D3085123A210448E74Fc6393');
-    const rethPricefeed = await governanceDeploymentManager.existing('rETH:priceFeed', '0x536218f9E9Eb48863970252233c8F271f554C2d0');
+    const rETH = await deploymentManager.existing('rETH', '0xae78736Cd615f374D3085123A210448E74Fc6393');
+    const rETHPricefeed = await deploymentManager.existing('rETH:priceFeed', vars.rETHPriceFeedAddress);
 
     const {
       governor,
       comet,
       configurator,
       cometAdmin,
-    } = await governanceDeploymentManager.getContracts();
+    } = await deploymentManager.getContracts();
 
     const newAssetConfig = {
-      asset: reth.address,
-      priceFeed: rethPricefeed.address,
-      decimals: await reth.decimals(),
+      asset: rETH.address,
+      priceFeed: rETHPricefeed.address,
+      decimals: await rETH.decimals(),
       borrowCollateralFactor: exp(0.9, 18),
       liquidateCollateralFactor: exp(0.93, 18),
       liquidationFactor: exp(0.975, 18),
@@ -52,8 +60,8 @@ export default migration('1692063589_add_reth_collateral', {
       },
 
     ];
-    const description = "# Add rETH as Collateral to wETHv3 Mainnet\nThis proposal adds rETH as collateral.\n";
-    const txn = await governanceDeploymentManager.retry(
+    const description = '# Add rETH as Collateral to wETHv3 Mainnet\nSee proposal and parameter recommendations here: https://www.comp.xyz/t/add-reth-as-collateral-to-wethv3-on-mainnet/4498';
+    const txn = await deploymentManager.retry(
       async () => governor.propose(...await proposal(actions, description))
     );
     trace(txn);
@@ -70,16 +78,19 @@ export default migration('1692063589_add_reth_collateral', {
   async verify(deploymentManager: DeploymentManager) {
     const {
       comet,
-      reth,
+      rETH,
+      'rETH:priceFeed': rETHPriceFeed
     } = await deploymentManager.getContracts();
 
-    const rethInfo = await comet.getAssetInfoByAddress(reth.address);
+    const rethInfo = await comet.getAssetInfoByAddress(rETH.address);
 
-    // priceFeed: rethPricefeed.address,
-    //   decimals: await reth.decimals(),
-    //   borrowCollateralFactor: exp(0.9, 18),
-    //   liquidateCollateralFactor: exp(0.93, 18),
-    //   liquidationFactor: exp(0.975, 18),
+    // check pricefeed
+    expect(await rethInfo.priceFeed).to.be.eq(rETHPriceFeed.address);
+    expect(await rETHPriceFeed.decimals()).to.be.eq(8);
+    // check config
+    expect(await rethInfo.borrowCollateralFactor).to.be.eq(exp(0.9, 18));
+    expect(await rethInfo.liquidateCollateralFactor).to.be.eq(exp(0.93, 18));
+    expect(await rethInfo.liquidationFactor).to.be.eq(exp(0.975, 18));
     expect(await rethInfo.supplyCap).to.be.eq(exp(30_000, 18));
   },
 });
