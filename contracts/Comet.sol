f7bf99a9e1d133efbb8527c67f4cf7a5bd9fb829
Hans Wang <hans@compound.finance>
2023-10-28 20:27:30 -0700

    use ethcall instead of assembly

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -765,22 +765,10 @@ contract Comet is CometMainInterface {
      */
     function doTransferIn(address asset, address from, uint amount) internal returns (uint) {
         uint256 preTransferBalance = IERC20NonStandard(asset).balanceOf(address(this));
-        IERC20NonStandard(asset).transferFrom(from, address(this), amount);
-        bool success;
-        assembly ("memory-safe") {
-            switch returndatasize()
-                case 0 {                       // This is a non-standard ERC-20
-                    success := not(0)          // set success to true
-                }
-                case 32 {                      // This is a compliant ERC-20
-                    returndatacopy(0, 0, 32)
-                    success := mload(0)        // Set `success = returndata` of override external call
-                }
-                default {                      // This is an excessively non-compliant ERC-20, revert.
-                    revert(0, 0)
-                }
+        (bool success, bytes memory returndata) = asset.call(abi.encodeWithSelector(IERC20NonStandard.transferFrom.selector, from, address(this), amount));
+        if (!success || !(returndata.length == 0 || abi.decode(returndata, (bool)))) {
+            revert TransferInFailed();
         }
-        if (!success) revert TransferInFailed();
         return IERC20NonStandard(asset).balanceOf(address(this)) - preTransferBalance;
     }
 
@@ -789,22 +777,10 @@ contract Comet is CometMainInterface {
      * @dev Note: Safely handles non-standard ERC-20 tokens that do not return a value. See here: https://medium.com/coinmonks/missing-return-value-bug-at-least-130-tokens-affected-d67bf08521ca
      */
     function doTransferOut(address asset, address to, uint amount) internal {
-        IERC20NonStandard(asset).transfer(to, amount);
-        bool success;
-        assembly ("memory-safe") {
-            switch returndatasize()
-                case 0 {                       // This is a non-standard ERC-20
-                    success := not(0)          // set success to true
-                }
-                case 32 {                      // This is a compliant ERC-20
-                    returndatacopy(0, 0, 32)
-                    success := mload(0)        // Set `success = returndata` of override external call
-                }
-                default {                      // This is an excessively non-compliant ERC-20, revert.
-                    revert(0, 0)
-                }
+        (bool success, bytes memory returndata) = asset.call(abi.encodeWithSelector(IERC20NonStandard.transfer.selector, to, amount));
+        if (!success || !(returndata.length == 0 || abi.decode(returndata, (bool)))) {
+            revert TransferOutFailed();
         }
-        if (!success) revert TransferOutFailed();
     }
 
     /**
