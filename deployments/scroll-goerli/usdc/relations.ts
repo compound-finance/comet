9a62f93f7f5f7aab5f5b5e92d92f142fe8c70d16
isabelle <isabelle@scroll.io>
2023-11-04 11:33:38 +0900

    add configurator to scroll relations

--- deployments/scroll-goerli/usdc/relations.ts
+++ deployments/scroll-goerli/usdc/relations.ts
@@ -32,5 +32,13 @@ export default {
         slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
       }
     }
+  },
+  '0x477dF03CB8C83Ec241dF01302a9d13102676EEE4': {
+    artifact: 'contracts/Configurator.sol:Configurator',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
   }
 };
