0577a5ba4ebb954a0dacfe049b38c70d919b0314
isabelle <isabelle.wei@yahoo.ca>
2024-02-14 17:06:56 -0500

    update scroll relations for wsteth

--- deployments/scroll/usdc/relations.ts
+++ deployments/scroll/usdc/relations.ts
@@ -39,5 +39,14 @@ export default {
         slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
       }
     }
-  }
+  },
+  // wstETH
+  '0xf610a9dfb7c89644979b4a0f27063e9e7d7cda32': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
 };
