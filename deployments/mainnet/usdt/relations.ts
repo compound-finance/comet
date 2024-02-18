b621671750a52f4de64738bb42be81d3ee1944bf
Vincent Tiu <vincetiu8@gmail.com>
2024-02-18 17:14:49 -0500

    add roots

--- deployments/mainnet/usdt/relations.ts
+++ deployments/mainnet/usdt/relations.ts
@@ -1,5 +1,48 @@
 import baseRelationConfig from '../../relations';
 
 export default {
-  ...baseRelationConfig
+  ...baseRelationConfig,
+  arbitrumInbox: {
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
+      },
+    },
+    relations: {
+      arbitrumBridge: {
+        field: async (inbox) => inbox.bridge()
+      }
+    }
+  },
+  arbitrumL1GatewayRouter: {
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
+      }
+    },
+  },
+  baseL1CrossDomainMessenger: {
+    delegates: {
+      // Not great, but this address shouldn't change and is very difficult to grab on-chain (private methods)
+      field: async () => '0x81C4Bd600793EBd1C0323604E1F455fE50A951F8',
+    },
+  },
+  baseL1StandardBridge: {
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  'wstETH': {
+    artifact: 'contracts/bulkers/IWstETH.sol',
+    relations: {
+      stETH: {
+        field: async (wstETH) => wstETH.stETH()
+      }
+    }
+  },
+  'AppProxyUpgradeable': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+  }
 };
