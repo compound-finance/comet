2abd324f2b0b408581b152c5eee2b624d2c09bc0
dmitriy-bergman-works <dmitriy@bergman.works>
2024-03-26 23:04:10 +0200

    remove wstEth from relations. update configuration

--- deployments/optimism/usdc/relations.ts
+++ deployments/optimism/usdc/relations.ts
@@ -23,8 +23,5 @@ export default {
           '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
       },
     },
-  },
-  '0x1f32b1c2345538c0c6f582fcb022739c4a194ebb': {
-    artifact: 'contracts/ERC20.sol:ERC20'
-  },
+  }
 };
