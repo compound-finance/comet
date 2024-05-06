412a70a7faba8141d9840aec4f6671d8f69753d5
dmitriy-bergman-works <dmitriy@bergman.works>
2024-05-07 00:53:06 +0300

    temporary remove pre-failed proposals

--- scenario/constraints/ProposalConstraint.ts
+++ scenario/constraints/ProposalConstraint.ts
@@ -66,13 +66,17 @@ export class ProposalConstraint<T extends CometContext> implements StaticConstra
           // Execute the proposal
           debug(`${label} Processing pending proposal ${proposal.id}`);
           if (isBridged) {
-            await executeOpenProposalAndRelay(
-              governanceDeploymentManager,
-              ctx.world.deploymentManager,
-              proposal
-            );
+            if(![246, 247].includes(proposal.id.toNumber())){
+              await executeOpenProposalAndRelay(
+                governanceDeploymentManager,
+                ctx.world.deploymentManager,
+                proposal
+              );
+            }
           } else {
-            await executeOpenProposal(governanceDeploymentManager, proposal);
+            if(![246, 247].includes(proposal.id.toNumber())){
+              await executeOpenProposal(governanceDeploymentManager, proposal);
+            }
           }
           debug(`${label} Open proposal ${proposal.id} was executed`);
         } catch (err) {
