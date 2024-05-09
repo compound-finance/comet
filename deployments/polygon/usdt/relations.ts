b1be1cd7bcb9729b5ebb4e93b0823ed02dffbd66
Mikhailo Shabodyash <67977488+EviLord032@users.noreply.github.com>
2024-05-09 12:00:35 +0300

    feat: add new assets

--- deployments/polygon/usdt/relations.ts
+++ deployments/polygon/usdt/relations.ts
@@ -6,6 +6,23 @@ export default {
   'governor': {
     artifact: 'contracts/bridges/polygon/PolygonBridgeReceiver.sol:PolygonBridgeReceiver',
   },
+  InitializableImmutableAdminUpgradeabilityProxy: {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  // InitializableImmutableAdminUpgradeabilityProxy: {
+  aPolMATICX: {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
   UChildERC20Proxy: {
     artifact: 'contracts/ERC20.sol:ERC20',
     delegates: {
@@ -13,14 +30,5 @@ export default {
         slot: '0xbaab7dbf64751104133af04abc7d9979f0fda3b059a322a8333f533d3f32bf7f',
       }
     },
-    // aPolMATICX
-    '0x80cA0d8C38d2e2BcbaB66aA1648Bd1C7160500FE': {
-      artifact: 'contracts/ERC20.sol:ERC20',
-      delegates: {
-        field: {
-          slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-        }
-      }
-    }
   }
 };
