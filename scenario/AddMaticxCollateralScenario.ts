3cdb26327dc2c4b0edf4d77954f51d1d05e0ebb3
Mikhailo Shabodyash <67977488+EviLord032@users.noreply.github.com>
2024-05-10 14:24:38 +0300

    fix

--- scenario/AddMaticxCollateralScenario.ts
+++ scenario/AddMaticxCollateralScenario.ts
@@ -9,8 +9,7 @@ import { createCrossChainProposal, matchesDeployment } from './utils';
 const MATICX_ADDRESS = '0xfa68FB4628DFF1028CFEc22b4162FCcd0d45efb6';
 const MATICX_PRICE_FEED_ADDRESS = '0x5d37E4b374E6907de8Fc7fb33EE3b0af403C7403';
 const MATICX_WHALES = {
-  polygon: [
-    '0x68B9220B8E617b7700aCAE1a5Ff43F3eb29257F3'],
+  polygon: ['0x68B9220B8E617b7700aCAE1a5Ff43F3eb29257F3'],
 };
 
 // TODO: add ability to run ad hoc scenarios against a single migration, to avoid needing the scenario to do all this setup of
