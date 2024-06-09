22ad8a1d69931b7897bff7ba6d5055fcd16c39f3
dmitriy-bergman-works <dmitriy@bergman.works>
2024-06-09 19:35:09 +0200

    feat: add migration

---
+++ deployments/arbitrum/usdt/relations.ts
@@ -0,0 +1,48 @@
+import baseRelationConfig from '../../relations';
+
+export default {
+  ...baseRelationConfig,
+  governor: {
+    artifact: 'contracts/bridges/arbitrum/ArbitrumBridgeReceiver.sol:ArbitrumBridgeReceiver'
+  },
+  // WBTC
+  ClonableBeaconProxy: {
+    artifact: 'contracts/ERC20.sol:ERC20'
+  },
+  // ARB
+  '0x912ce59144191c1204e64559fe8253a0e49e6548': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  // WETH
+  '0x82af49447d8a07e3bd95bd0d56f35241523fbab1': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  // USDT
+  '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  // wstETH
+  '0x5979D7b546E38E414F7E9822514be443A4800529': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  }
+};
