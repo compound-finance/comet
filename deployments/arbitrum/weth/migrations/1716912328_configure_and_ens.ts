cb825058b5c66890c12a0dc94b8fb3d7882c0b67
Mikhailo Shabodyash <misha.sh@woof.software>
2024-06-17 13:30:05 +0300

    enacted true: manually added due to migration failure

--- deployments/arbitrum/weth/migrations/1716912328_configure_and_ens.ts
+++ deployments/arbitrum/weth/migrations/1716912328_configure_and_ens.ts
@@ -189,7 +189,7 @@ export default migration('1713517203_configurate_and_ens', {
   },
 
   async enacted(deploymentManager: DeploymentManager): Promise<boolean> {
-    return false;
+    return true;
   },
 
   async verify(deploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager) {
