b93d760f1ca6747bd1fb12ff484256c45f8ae0ba
dmitriy-woof-software <dmitriy@woof.software>
2024-06-22 20:19:26 +0200

    feat: make code cleaner

--- deployments/polygon/usdt/deploy.ts
+++ deployments/polygon/usdt/deploy.ts
@@ -1,11 +1,6 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { DeploySpec, deployComet } from '../../../src/deploy';
 
-const HOUR = 60 * 60;
-const DAY = 24 * HOUR;
-
-const MAINNET_TIMELOCK = '0x6d903f6003cca6255d85cca4d3b5e5146dc33925';
-
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
   const trace = deploymentManager.tracer()
   const ethers = deploymentManager.hre.ethers;
@@ -15,7 +10,7 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   const WBTC = await deploymentManager.existing('WBTC', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', 'polygon');
   const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', 'polygon');
   const WMATIC = await deploymentManager.existing('WMATIC', '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 'polygon');
-  const aPolMATICX = await deploymentManager.existing('aPolMATICX', '0x80cA0d8C38d2e2BcbaB66aA1648Bd1C7160500FE', 'polygon');
+  const MaticX = await deploymentManager.existing('MaticX', '0xfa68FB4628DFF1028CFEc22b4162FCcd0d45efb6', 'polygon');
   const stMATIC = await deploymentManager.existing('stMATIC', '0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4', 'polygon');
   const COMP = await deploymentManager.existing('COMP', '0x8505b9d2254A7Ae468c0E9dd10Ccea3A837aef5c', 'polygon');
 
