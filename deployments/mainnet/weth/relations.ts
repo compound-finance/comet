8108fa6c0e0da35bbc3ae6bc690422b93ab671f9
Dmitriy Babenko <159453675+dmitriy-woof-software@users.noreply.github.com>
2024-07-01 20:13:56 +0200

    On-chain add weETH collateral into Mainnet WETH market (#869)

--- deployments/mainnet/weth/relations.ts
+++ deployments/mainnet/weth/relations.ts
@@ -14,7 +14,7 @@ export default {
   'AppProxyUpgradeable': {
     artifact: 'contracts/ERC20.sol:ERC20',
   },
-  TransparentUpgradeableProxy: {
+  UUPSProxy: {
     artifact: 'contracts/ERC20.sol:ERC20',
     delegates: {
       field: {
@@ -22,4 +22,12 @@ export default {
       }
     }
   },
+  TransparentUpgradeableProxy: {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  }
 };
