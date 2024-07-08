df7bab0051b6183572124c9609c098a5b50188c4
Mikhailo Shabodyash <misha.sh@woof.software>
2024-07-08 11:05:22 +0300

    fix: audit fixes

--- contracts/pricefeeds/EzETHExchangeRatePriceFeed.sol
+++ contracts/pricefeeds/EzETHExchangeRatePriceFeed.sol
@@ -12,6 +12,7 @@ import "../IPriceFeed.sol";
 contract EzETHExchangeRatePriceFeed is IPriceFeed {
     /** Custom errors **/
     error InvalidInt256();
+    error BadDecimals();
 
     /// @notice Version of the price feed
     uint public constant override version = 1;
@@ -38,6 +39,7 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
      **/
     constructor(address ezETHRateProvider, uint8 decimals_, string memory description_) {
         underlyingPriceFeed = ezETHRateProvider;
+        if (decimals_ > 18) revert BadDecimals();
         decimals = decimals_;
         description = description_;
 
@@ -68,7 +70,7 @@ contract EzETHExchangeRatePriceFeed is IPriceFeed {
         uint256 rate = IBalancerRateProvider(underlyingPriceFeed).getRate();
         // protocol uses only the answer value. Other data fields are not provided by the underlying pricefeed and are not used in Comet protocol
         // https://etherscan.io/address/0x387dBc0fB00b26fb085aa658527D5BE98302c84C#readProxyContract
-        return (0, scalePrice(int256(rate)), 0, 0, 0);
+        return (1, scalePrice(signed256(rate)), block.timestamp, block.timestamp, 1);
     }
 
     function signed256(uint256 n) internal pure returns (int256) {
