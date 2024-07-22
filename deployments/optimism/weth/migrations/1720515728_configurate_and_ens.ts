bf93e51425a58ebf03de775d0e87615d2570dca9
Dmitriy Babenko <159453675+dmitriy-woof-software@users.noreply.github.com>
2024-07-22 11:31:51 +0200

    Manual enact

--- deployments/optimism/weth/migrations/1720515728_configurate_and_ens.ts
+++ deployments/optimism/weth/migrations/1720515728_configurate_and_ens.ts
@@ -169,7 +169,7 @@ export default migration('1720515728_configurate_and_ens', {
   },
 
   async enacted(): Promise<boolean> {
-    return false;
+    return true;
   },
 
   async verify(deploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager, preMigrationBlockNumber: number) {
