8df38e60ca737b0b1956e7efdb34ab6198eb59e7
Mikhailo Shabodyash <misha.sh@woof.software>
2024-08-26 18:02:22 +0300

    fix: remove unused iterface

--- contracts/AssetList.sol
+++ contracts/AssetList.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.15;
 
-import "./CometConfiguration.sol";
 import "./IPriceFeed.sol";
 import "./IERC20NonStandard.sol";
 import "./CometMainInterface.sol";
