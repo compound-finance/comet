01ce0c400f30fd55b3ccfc0d985bddedfe2213ca
Dmitriy Babenko <159453675+dmitriy-woof-software@users.noreply.github.com>
2024-10-18 15:48:54 -0500

    Init AERO on Base market (#937)
    Co-authored-by: Mikhailo Shabodyash <misha.sh@woof.software>
    Co-authored-by: GitHub Actions Bot <>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,6 +38,7 @@ import arbitrumUsdtRelationConfigMap from './deployments/arbitrum/usdt/relations
 import baseUsdbcRelationConfigMap from './deployments/base/usdbc/relations';
 import baseWethRelationConfigMap from './deployments/base/weth/relations';
 import baseUsdcRelationConfigMap from './deployments/base/usdc/relations';
+import baseAeroRelationConfigMap from './deployments/base/aero/relations';
 import baseGoerliRelationConfigMap from './deployments/base-goerli/usdc/relations';
 import baseGoerliWethRelationConfigMap from './deployments/base-goerli/weth/relations';
 import lineaGoerliRelationConfigMap from './deployments/linea-goerli/usdc/relations';
@@ -72,7 +73,7 @@ const {
   REMOTE_ACCOUNTS = ''
 } = process.env;
 
-function *deriveAccounts(pk: string, n: number = 10) {
+function* deriveAccounts(pk: string, n: number = 10) {
   for (let i = 0; i < n; i++)
     yield (BigInt('0x' + pk) + BigInt(i)).toString(16);
 }
@@ -188,7 +189,7 @@ function setupDefaultNetworkProviders(hardhatConfig: HardhatUserConfig) {
         getDefaultProviderURL(netConfig.network),
       gas: netConfig.gas || 'auto',
       gasPrice: netConfig.gasPrice || 'auto',
-      accounts: REMOTE_ACCOUNTS ? 'remote' : ( ETH_PK ? [...deriveAccounts(ETH_PK)] : { mnemonic: MNEMONIC } ),
+      accounts: REMOTE_ACCOUNTS ? 'remote' : (ETH_PK ? [...deriveAccounts(ETH_PK)] : { mnemonic: MNEMONIC }),
     };
   }
 }
@@ -374,7 +375,8 @@ const config: HardhatUserConfig = {
       'base': {
         usdbc: baseUsdbcRelationConfigMap,
         weth: baseWethRelationConfigMap,
-        usdc: baseUsdcRelationConfigMap
+        usdc: baseUsdcRelationConfigMap,
+        aero: baseAeroRelationConfigMap
       },
       'base-goerli': {
         usdc: baseGoerliRelationConfigMap,
@@ -520,6 +522,12 @@ const config: HardhatUserConfig = {
         name: 'base-usdc',
         network: 'base',
         deployment: 'usdc',
+        auxiliaryBase: 'mainnet'
+      },
+      {
+        name: 'base-aero',
+        network: 'base',
+        deployment: 'aero',
         auxiliaryBase: 'mainnet'
       },
       {
