efca8e6e9c3da71deab82184ae2ab9d9a6aaeb4b
Mikhailo Shabodyash <misha.sh@woof.software>
2025-01-29 22:09:46 +0200

    feat: new comet version support

--- deployments/mainnet/wbtc/deploy.ts
+++ deployments/mainnet/wbtc/deploy.ts
@@ -1,14 +1,14 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
-import { DeploySpec, deployComet, exp } from '../../../src/deploy';
+import { DeploySpec, deployComet } from '../../../src/deploy';
 
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
-  const WBTC = await deploymentManager.existing('WBTC', '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599');
-  const solvBTC_BBN = await deploymentManager.existing('SolvBTC.BBN', '0xd9D920AA40f578ab794426F5C90F6C731D159DEf');
-  const LBTC = await deploymentManager.existing('LBTC', '0x8236a87084f8B84306f72007F36F2618A5634494');
-  const pumpBTC = await deploymentManager.existing('pumpBTC', '0xF469fBD2abcd6B9de8E169d128226C0Fc90a012e');
+  const _WBTC = await deploymentManager.existing('WBTC', '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599');
+  const _solvBTC_BBN = await deploymentManager.existing('SolvBTC.BBN', '0xd9D920AA40f578ab794426F5C90F6C731D159DEf');
+  const _LBTC = await deploymentManager.existing('LBTC', '0x8236a87084f8B84306f72007F36F2618A5634494');
+  const _pumpBTC = await deploymentManager.existing('pumpBTC', '0xF469fBD2abcd6B9de8E169d128226C0Fc90a012e');
   const COMP = await deploymentManager.existing('COMP', '0xc00e94Cb662C3520282E6f5717214004A7f26888');
 
-  const solvBTC_BBNPriceFeed = await deploymentManager.deploy(
+  const _solvBTC_BBNPriceFeed = await deploymentManager.deploy(
     'SolvBTC.BBN:priceFeed',
     'pricefeeds/MultiplicativePriceFeed.sol',
     [
@@ -21,7 +21,7 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   );
 
   // Deploy scaling price feed for pumpBTC
-  const pumpBTCScalingPriceFeed = await deploymentManager.deploy(
+  const _pumpBTCScalingPriceFeed = await deploymentManager.deploy(
     'pumpBTC:priceFeed',
     'pricefeeds/ScalingPriceFeed.sol',
     [
@@ -32,15 +32,15 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   );
 
   // Import shared contracts from cUSDCv3
-  const cometAdmin = await deploymentManager.fromDep('cometAdmin', 'mainnet', 'usdc');
-  const cometFactory = await deploymentManager.fromDep('cometFactory', 'mainnet', 'usdc');
-  const $configuratorImpl = await deploymentManager.fromDep('configurator:implementation', 'mainnet', 'usdc');
-  const configurator = await deploymentManager.fromDep('configurator', 'mainnet', 'usdc');
+  const _cometAdmin = await deploymentManager.fromDep('cometAdmin', 'mainnet', 'usdc');
+  const _cometFactory = await deploymentManager.fromDep('cometFactory', 'mainnet', 'usdc');
+  const _$configuratorImpl = await deploymentManager.fromDep('configurator:implementation', 'mainnet', 'usdc');
+  const _configurator = await deploymentManager.fromDep('configurator', 'mainnet', 'usdc');
   const rewards = await deploymentManager.fromDep('rewards', 'mainnet', 'usdc');
   const bulker = await deploymentManager.fromDep('bulker', 'mainnet', 'weth');
 
   // Deploy Comet
-  const deployed = await deployComet(deploymentManager, deploySpec);
+  const deployed = await deployComet(deploymentManager, deploySpec, {}, true);
 
   return {
     ...deployed,
