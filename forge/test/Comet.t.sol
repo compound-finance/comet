344e8c0107880cb79e23ea27812791b3ea2a923b
Mikhailo Shabodyash <misha.sh@woof.software>
2025-02-06 13:35:13 +0200

    fix: forge fix

--- forge/test/Comet.t.sol
+++ forge/test/Comet.t.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../../contracts/Comet.sol";
 import "../../contracts/CometConfiguration.sol";
+import { CometExtAssetList } from "../../contracts/CometExtAssetList.sol";
+import { AssetListFactory } from "../../contracts/AssetListFactory.sol";
+
 
 contract CometTest is Test {
     Comet public comet;
@@ -13,13 +16,15 @@ contract CometTest is Test {
     }
 
     function testFailXXX() public {
+        AssetListFactory factory = new AssetListFactory();
+        CometExtAssetList assetList = new CometExtAssetList(CometConfiguration.ExtConfiguration("name", "symbol"), address(factory));
         CometConfiguration.AssetConfig[] memory assets = new CometConfiguration.AssetConfig[](0);
         CometConfiguration.Configuration memory config =
             CometConfiguration.Configuration(address(0),
                           address(0),
                           address(0),
                           address(0),
-                          address(0),
+                          address(assetList),
                           0,
                           0,
                           0,
