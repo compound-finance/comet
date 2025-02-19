16b691fb25aa326192f188f17266806fe51b11bc
artemwoofsoftware <artem.m@woof.software>
2025-02-19 16:08:26 -0600

    feat: squash added weeth as collateral to usds, usdc and usdt market

--- deployments/mainnet/usdc/relations.ts
+++ deployments/mainnet/usdc/relations.ts
@@ -11,6 +11,7 @@ export default {
       }
     }
   },
+  
   'AppProxyUpgradeable': {
     artifact: 'contracts/ERC20.sol:ERC20',
   },
@@ -92,5 +93,20 @@ export default {
         slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
       }
     }
-  }
+  },
+  baseL1USDSBridge: {
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
+  'UUPSProxy': {
+    artifact: 'contracts/ERC20.sol:ERC20',
+    delegates: {
+      field: {
+        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
+      }
+    }
+  },
 };
