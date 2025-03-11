32ce458ab0eb8f6c60334eab349cf39de6586af2
Mikhailo Shabodyash <misha.sh@woof.software>
2025-03-11 13:59:47 +0200

    fix: review fix

--- contracts/pricefeeds/ERC4626CorrelatedAssetsPriceOracle.sol
+++ contracts/pricefeeds/ERC4626CorrelatedAssetsPriceOracle.sol
@@ -9,23 +9,38 @@ import { PriceCapAdapterBase } from "./utils/PriceCapAdapterBase.sol";
  * @author Compound
  */
 contract ERC4626CorrelatedAssetsPriceOracle is PriceCapAdapterBase {
+  /// @notice Version of the price feed
+  uint public constant VERSION = 1;
+  
   /**
-   * @param capAdapterParams parameters to create cap adapter
+   * @param _manager address of the manager
+   * @param _baseAggregatorAddress address of the base aggregator
+   * @param _ratioProviderAddress address of the ratio provider
+   * @param _description description of the pair
+   * @param _ratioDecimals number of decimals for the ratio
+   * @param _priceFeedDecimals number of decimals for the price feed
+   * @param _minimumSnapshotDelay minimum time that should have passed from the snapshot timestamp to the current block.timestamp
+   * @param _priceCapSnapshot parameters to set price cap
    */
   constructor(
-    CapAdapterParams memory capAdapterParams
+    address _manager,
+    address _baseAggregatorAddress,
+    address _ratioProviderAddress,
+    string memory _description,
+    uint8 _ratioDecimals,
+    uint8 _priceFeedDecimals,
+    uint48 _minimumSnapshotDelay,
+    PriceCapSnapshot memory _priceCapSnapshot
   )
     PriceCapAdapterBase(
-      CapAdapterBaseParams({
-        manager: capAdapterParams.manager,
-        baseAggregatorAddress: capAdapterParams.baseAggregatorAddress,
-        ratioProviderAddress: capAdapterParams.ratioProviderAddress,
-        description: capAdapterParams.description,
-        ratioDecimals: capAdapterParams.ratioDecimals,
-        priceFeedDecimals: capAdapterParams.priceFeedDecimals,
-        minimumSnapshotDelay: capAdapterParams.minimumSnapshotDelay,
-        priceCapParams: capAdapterParams.priceCapParams
-      })
+      _manager,
+      _baseAggregatorAddress,
+      _ratioProviderAddress,
+      _description,
+      _ratioDecimals,
+      _priceFeedDecimals,
+      _minimumSnapshotDelay,
+      _priceCapSnapshot
     )
   {}
 
@@ -33,6 +48,14 @@ contract ERC4626CorrelatedAssetsPriceOracle is PriceCapAdapterBase {
    * @notice Returns the current exchange ratio of lst to the underlying(base) asset
    */
   function getRatio() public view override returns (int256) {
-    return int256(IERC4626(RATIO_PROVIDER).convertToAssets(10 ** RATIO_DECIMALS));
+    return int256(IERC4626(ratioProvider).convertToAssets(10 ** ratioDecimals));
+  }
+  
+  /**
+   * @notice Price for the latest round
+   * @return The version of the price feed contract
+   **/
+  function version() external pure returns (uint256) {
+      return VERSION;
   }
 }
--- contracts/pricefeeds/utils/PriceCapAdapterBase.sol
+++ contracts/pricefeeds/utils/PriceCapAdapterBase.sol
@@ -9,55 +9,20 @@ import "../../vendor/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interf
  * @notice Price adapter to cap the price of the underlying asset.
  */
 abstract contract PriceCapAdapterBase {
-  /**
-   * @dev Emitted when the cap parameters are updated
-   * @param snapshotRatio the ratio at the time of snapshot
-   * @param snapshotTimestamp the timestamp at the time of snapshot
-   * @param maxRatioGrowthPerSecond max ratio growth per second
-   * @param maxYearlyRatioGrowthPercent max yearly ratio growth percent
-   **/
-  event CapParametersUpdated(
-    uint256 snapshotRatio,
+  event NewPriceCapSnapshot(
+    uint256 indexed snapshotRatio,
     uint256 snapshotTimestamp,
-    uint256 maxRatioGrowthPerSecond,
-    uint32 maxYearlyRatioGrowthPercent
+    uint256 indexed maxRatioGrowthPerSecond,
+    uint32 indexed maxYearlyRatioGrowthPercent
   );
 
   /**
-   * @notice Parameters to create adapter
-   * @param capAdapterParams parameters to create adapter
+   * @notice Price cap snapshot
+   * @param snapshotRatio ratio at the time of snapshot
+   * @param snapshotTimestamp timestamp at the time of snapshot
+   * @param maxYearlyRatioGrowthPercent max yearly growth percent
    */
-  struct CapAdapterBaseParams {
-    address manager;
-    address baseAggregatorAddress;
-    address ratioProviderAddress;
-    string description;
-    uint8 ratioDecimals;
-    uint8 priceFeedDecimals;
-    uint48 minimumSnapshotDelay;
-    PriceCapUpdateParams priceCapParams;
-  }
-
-  /**
-   * @notice Parameters to create CL cap adapter
-   * @param clCapAdapterParams parameters to create CL cap adapter
-   */
-  struct CapAdapterParams {
-    address manager;
-    address baseAggregatorAddress;
-    address ratioProviderAddress;
-    uint8 ratioDecimals;
-    uint8 priceFeedDecimals;
-    string description;
-    uint48 minimumSnapshotDelay;
-    PriceCapUpdateParams priceCapParams;
-  }
-
-  /**
-   * @notice Parameters to update price cap
-   * @param priceCapParams parameters to set price cap
-   */
-  struct PriceCapUpdateParams {
+  struct PriceCapSnapshot {
     uint104 snapshotRatio;
     uint48 snapshotTimestamp;
     uint32 maxYearlyRatioGrowthPercent;
@@ -65,80 +30,50 @@ abstract contract PriceCapAdapterBase {
   
   error ManagerIsZeroAddress();
   error SnapshotRatioIsZero();
-  error SnapshotMayOverflowSoon(uint104 snapshotRatio, uint32 maxYearlyRatioGrowthPercent);
+  error SnapshotCloseToOverflow(uint104 snapshotRatio, uint32 maxYearlyRatioGrowthPercent);
   error InvalidRatioTimestamp(uint48 timestamp);
   error OnlyManager();
   error InvalidInt256();
+  error InvalidAddress();
 
-  /**
-   * @notice Maximum percentage factor (100.00%)
-   */
-  uint256 public constant PERCENTAGE_FACTOR = 1e4;
+  /// @notice Decimal factor for percentage
+  uint256 public constant PERCENTAGE_DECIMALS = 1e2;
 
-  /**
-   * @notice Minimal time while ratio should not overflow, in years
-   */
-  uint256 public constant MINIMAL_RATIO_INCREASE_LIFETIME = 3;
-
-  /**
-   * @notice Number of seconds per year (365 days)
-   */
+  /// @notice Number of seconds per year (365 days)
   uint256 public constant SECONDS_PER_YEAR = 365 days;
 
-  /**
-   * @notice Price feed for (ASSET / BASE) pair
-   */
-  AggregatorV3Interface public immutable ASSET_TO_BASE_AGGREGATOR;
+  /// @notice Price feed for (ASSET / BASE) pair
+  AggregatorV3Interface public immutable assetToBaseAggregator;
 
-  /**
-   * @notice Manager address
-   */
+  /// @notice Manager address
   address public manager;
 
-  /**
-   * @notice Ratio feed for (LST_ASSET / BASE_ASSET) pair
-   */
-  address public immutable RATIO_PROVIDER;
+  /// @notice Ratio feed for (LST_ASSET / BASE_ASSET) pair
+  address public immutable ratioProvider;
 
-  /**
-   * @notice Number of decimals in the output of this price adapter
-   */
+  /// @notice Number of decimals in the output of this price feed
   uint8 public immutable decimals;
 
-  /**
-   * @notice Number of decimals for (lst asset / underlying asset) ratio
-   */
-  uint8 public immutable RATIO_DECIMALS;
+  /// @notice Number of decimals for (lst asset / underlying asset) ratio
+  uint8 public immutable ratioDecimals;
 
-  /**
-   * @notice Minimum time (in seconds) that should have passed from the snapshot timestamp to the current block.timestamp
-   */
-  uint48 public immutable MINIMUM_SNAPSHOT_DELAY;
+  /// @notice Minimum time (in seconds) that should have passed from the snapshot timestamp to the current block.timestamp
+  uint48 public immutable minimumSnapshotDelay;
 
-  /**
-   * @notice Description of the pair
-   */
+  /// @notice Description of the pair
   string public description;
 
-  /**
-   * @notice Ratio at the time of snapshot
-   */
-  uint104 private _snapshotRatio;
+  /// @notice Ratio at the time of snapshot
+  uint104 public snapshotRatio;
 
-  /**
-   * @notice Timestamp at the time of snapshot
-   */
-  uint48 private _snapshotTimestamp;
+  /// @notice Timestamp at the time of snapshot
+  uint48 public snapshotTimestamp;
 
-  /**
-   * @notice Ratio growth per second
-   */
-  uint104 private _maxRatioGrowthPerSecond;
+  /// @notice Ratio growth per second
+  uint104 public maxRatioGrowthPerSecond;
 
-  /**
-   * @notice Max yearly growth percent
-   */
-  uint32 private _maxYearlyRatioGrowthPercent;
+  /// @notice Max yearly growth percent
+  uint32 public maxYearlyRatioGrowthPercent;
 
   /// @notice Whether or not the price should be upscaled
   bool internal immutable shouldUpscale;
@@ -147,68 +82,60 @@ abstract contract PriceCapAdapterBase {
   int256 internal immutable rescaleFactor;
 
   /**
-   * @param capAdapterBaseParams parameters to create adapter
+   * @param _manager address of the manager
+   * @param _baseAggregatorAddress address of the base aggregator
+   * @param _ratioProviderAddress address of the ratio provider
+   * @param _description description of the pair
+   * @param _ratioDecimals number of decimals for the ratio
+   * @param _priceFeedDecimals number of decimals for the price feed
+   * @param _minimumSnapshotDelay minimum time that should have passed from the snapshot timestamp to the current block.timestamp
+   * @param _priceCapSnapshot parameters to set price cap
    */
-  constructor(CapAdapterBaseParams memory capAdapterBaseParams) {
-    if (address(capAdapterBaseParams.manager) == address(0)) {
+  constructor(
+    address _manager,
+    address _baseAggregatorAddress,
+    address _ratioProviderAddress,
+    string memory _description,
+    uint8 _ratioDecimals,
+    uint8 _priceFeedDecimals,
+    uint48 _minimumSnapshotDelay,
+    PriceCapSnapshot memory _priceCapSnapshot
+  ) {
+    if (_manager == address(0)) {
       revert ManagerIsZeroAddress();
     }
-    manager = capAdapterBaseParams.manager;
-    ASSET_TO_BASE_AGGREGATOR = AggregatorV3Interface(capAdapterBaseParams.baseAggregatorAddress);
-    RATIO_PROVIDER = capAdapterBaseParams.ratioProviderAddress;
-    uint8 underlyingPriceFeedDecimals = ASSET_TO_BASE_AGGREGATOR.decimals();
+    if(_baseAggregatorAddress == address(0) || _ratioProviderAddress == address(0)) {
+      revert InvalidAddress();
+    }
+    manager = _manager;
+    assetToBaseAggregator = AggregatorV3Interface(_baseAggregatorAddress);
+    ratioProvider = _ratioProviderAddress;
+    uint8 underlyingPriceFeedDecimals = assetToBaseAggregator.decimals();
         // Note: Solidity does not allow setting immutables in if/else statements
-        shouldUpscale = underlyingPriceFeedDecimals < capAdapterBaseParams.priceFeedDecimals ? true : false;
+        shouldUpscale = underlyingPriceFeedDecimals < _priceFeedDecimals ? true : false;
         rescaleFactor = (shouldUpscale
-            ? signed256(10 ** (capAdapterBaseParams.priceFeedDecimals - underlyingPriceFeedDecimals))
-            : signed256(10 ** (underlyingPriceFeedDecimals - capAdapterBaseParams.priceFeedDecimals))
+            ? signed256(10 ** (_priceFeedDecimals - underlyingPriceFeedDecimals))
+            : signed256(10 ** (underlyingPriceFeedDecimals - _priceFeedDecimals))
         );
-    decimals = capAdapterBaseParams.priceFeedDecimals;
-    RATIO_DECIMALS = capAdapterBaseParams.ratioDecimals;
-    MINIMUM_SNAPSHOT_DELAY = capAdapterBaseParams.minimumSnapshotDelay;
+    decimals = _priceFeedDecimals;
+    ratioDecimals = _ratioDecimals;
+    minimumSnapshotDelay = _minimumSnapshotDelay;
 
-    description = capAdapterBaseParams.description;
+    description = _description;
 
-    _setCapParameters(capAdapterBaseParams.priceCapParams);
-  }
-
-  /**
-   * @notice Returns the latest snapshot ratio
-   */  function getSnapshotRatio() external view returns (uint256) {
-    return _snapshotRatio;
-  }
-
-  /**
-   * @notice Returns the latest snapshot timestamp
-   */
-  function getSnapshotTimestamp() external view returns (uint256) {
-    return _snapshotTimestamp;
-  }
-
-  /**
-   * @notice Returns the max yearly ratio growth
-   */
-  function getMaxYearlyGrowthRatePercent() external view returns (uint256) {
-    return _maxYearlyRatioGrowthPercent;
-  }
-
-  /**
-   * @notice Returns the max ratio growth per second
-   */
-  function getMaxRatioGrowthPerSecond() external view returns (uint256) {
-    return _maxRatioGrowthPerSecond;
+    _setSnapshot(_priceCapSnapshot);
   }
 
   /**
    * @notice Updates price cap parameters
    * @param priceCapParams parameters to set price cap
    */
-  function setCapParameters(PriceCapUpdateParams memory priceCapParams) external {
+  function updateSnapshot(PriceCapSnapshot memory priceCapParams) external {
     if (msg.sender != manager) {
       revert OnlyManager();
     }
 
-    _setCapParameters(priceCapParams);
+    _setSnapshot(priceCapParams);
   }
 
   function setManager(address newManager) external {
@@ -234,24 +161,27 @@ abstract contract PriceCapAdapterBase {
         uint256 updatedAt,
         uint80 answeredInRound
     ) {
-    // get the current lst to underlying ratio
+
     int256 currentRatio = getRatio();
-    // get the base price
-    (uint80 roundId_, int256 _price, uint256 startedAt_, uint256 updatedAt_, uint80 answeredInRound_) = ASSET_TO_BASE_AGGREGATOR.latestRoundData();
+    (
+      uint80 roundId_,
+      int256 _price,
+      uint256 startedAt_,
+      uint256 updatedAt_,
+      uint80 answeredInRound_
+    ) = assetToBaseAggregator.latestRoundData();
 
     if (_price <= 0 || currentRatio <= 0) {
         return (roundId_, 0, startedAt_, updatedAt_, answeredInRound_);
     }
 
-    // calculate the ratio based on snapshot ratio and max growth rate
     int256 maxRatio = _getMaxRatio();
 
     if (maxRatio < currentRatio) {
       currentRatio = maxRatio;
     }
 
-    // calculate the price of the underlying asset
-    int256 price = (_price * currentRatio) / int256(10 ** RATIO_DECIMALS);
+    int256 price = (_price * currentRatio) / int256(10 ** ratioDecimals);
 
     return (roundId_, scalePrice(price), startedAt_, updatedAt_, answeredInRound_);
   }
@@ -267,10 +197,10 @@ abstract contract PriceCapAdapterBase {
   }
 
   /**
-   * @notice Updates price cap parameters
+   * @notice Updates price cap parameters from recent snapshot
    * @param priceCapParams parameters to set price cap
    */
-  function _setCapParameters(PriceCapUpdateParams memory priceCapParams) internal {
+  function _setSnapshot(PriceCapSnapshot memory priceCapParams) internal {
     // if snapshot ratio is 0 then growth will not work as expected
     if (priceCapParams.snapshotRatio == 0) {
       revert SnapshotRatioIsZero();
@@ -278,62 +208,52 @@ abstract contract PriceCapAdapterBase {
 
     // new snapshot timestamp should be gt then stored one, but not gt then timestamp of the current block
     if (
-      _snapshotTimestamp >= priceCapParams.snapshotTimestamp ||
-      priceCapParams.snapshotTimestamp > block.timestamp - MINIMUM_SNAPSHOT_DELAY
+      snapshotTimestamp >= priceCapParams.snapshotTimestamp ||
+      priceCapParams.snapshotTimestamp > block.timestamp - minimumSnapshotDelay
     ) {
       revert InvalidRatioTimestamp(priceCapParams.snapshotTimestamp);
     }
-    _snapshotRatio = priceCapParams.snapshotRatio;
-    _snapshotTimestamp = priceCapParams.snapshotTimestamp;
-    _maxYearlyRatioGrowthPercent = priceCapParams.maxYearlyRatioGrowthPercent;
+    snapshotRatio = priceCapParams.snapshotRatio;
+    snapshotTimestamp = priceCapParams.snapshotTimestamp;
+    maxYearlyRatioGrowthPercent = priceCapParams.maxYearlyRatioGrowthPercent;
 
-    _maxRatioGrowthPerSecond = uint104(
+    maxRatioGrowthPerSecond = uint104(
       (uint256(priceCapParams.snapshotRatio) * priceCapParams.maxYearlyRatioGrowthPercent) /
-        PERCENTAGE_FACTOR /
+        (100 * PERCENTAGE_DECIMALS) /
         SECONDS_PER_YEAR
     );
 
-    // if the ratio on the current growth speed can overflow less then in a MINIMAL_RATIO_INCREASE_LIFETIME years, revert
+    // if the ratio on the current growth speed can overflow less then in a 3 years, revert
     if (
-      uint256(_snapshotRatio) +
-        (_maxRatioGrowthPerSecond * SECONDS_PER_YEAR * MINIMAL_RATIO_INCREASE_LIFETIME) >
+      uint256(snapshotRatio) +
+        (maxRatioGrowthPerSecond * SECONDS_PER_YEAR * 3) >
       type(uint104).max
     ) {
-      revert SnapshotMayOverflowSoon(
+      revert SnapshotCloseToOverflow(
         priceCapParams.snapshotRatio,
         priceCapParams.maxYearlyRatioGrowthPercent
       );
     }
 
-    emit CapParametersUpdated(
+    emit NewPriceCapSnapshot(
       priceCapParams.snapshotRatio,
       priceCapParams.snapshotTimestamp,
-      _maxRatioGrowthPerSecond,
+      maxRatioGrowthPerSecond,
       priceCapParams.maxYearlyRatioGrowthPercent
     );
   }
 
-  /**
-   * @notice Returns the current exchange ratio of lst to the underlying(base) asset
-   */
+  /// @notice Returns the current exchange ratio of lst to the underlying(base) asset
   function getRatio() public view virtual returns (int256);
 
-  /**
-   * @notice Returns if the price is currently capped
-   */
+  /// @notice Returns if the price is currently capped
   function isCapped() public view returns (bool) {
-    // get the current lst to underlying ratio
-    int256 currentRatio = getRatio();
-
-    // calculate the ratio based on snapshot ratio and max growth rate
-    int256 maxRatio = _getMaxRatio();
-
-    return currentRatio > maxRatio;
+    return getRatio() > _getMaxRatio();
   }
 
   function _getMaxRatio() internal view returns (int256) {
     return
-      int256(_snapshotRatio + _maxRatioGrowthPerSecond * (block.timestamp - _snapshotTimestamp));
+      int256(snapshotRatio + maxRatioGrowthPerSecond * (block.timestamp - snapshotTimestamp));
   }
 
   function signed256(uint256 n) internal pure returns (int256) {
