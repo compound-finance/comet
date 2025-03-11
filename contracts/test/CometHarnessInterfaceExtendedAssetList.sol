900dfd07e6921779058b672b03c282efcb13a4e9
Mikhailo Shabodyash <misha.sh@woof.software>
2025-03-11 15:01:28 +0200

    fix

--- contracts/test/CometHarnessInterfaceExtendedAssetList.sol
+++ contracts/test/CometHarnessInterfaceExtendedAssetList.sol
@@ -13,5 +13,5 @@ abstract contract CometHarnessInterfaceExtendedAssetList is CometInterface {
     function setCollateralBalance(address account, address asset, uint128 balance) virtual external;
     function updateAssetsInExternal(address account, address asset, uint128 initialUserBalance, uint128 finalUserBalance) virtual external;
     function getAssetList(address account) virtual external view returns (address[] memory);
-    function assetList() virtual external view returns (address);
+    function assetList() virtual external override view returns (address);
 }
--- deployments/linea/usdc/migrations/1736257010_configurate_and_ens.ts
+++ deployments/linea/usdc/migrations/1736257010_configurate_and_ens.ts
@@ -148,7 +148,7 @@ export default migration('1736257010_configurate_and_ens', {
   },
 
   async enacted(): Promise<boolean> {
-    return false;
+    return true;
   },
 
   async verify(deploymentManager: DeploymentManager, govDeploymentManager: DeploymentManager, preMigrationBlockNumber: number) {
