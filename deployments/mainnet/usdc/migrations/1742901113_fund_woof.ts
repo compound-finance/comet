f201059acf8bbc438d1ae84c168934459216436f
dmitriy-woof-software <dmitriy@woof.software>
2025-05-15 02:40:23 -0400

    feat: update COMP amount; update description; remove COMP delegation;

--- deployments/mainnet/usdc/migrations/1742901113_fund_woof.ts
+++ deployments/mainnet/usdc/migrations/1742901113_fund_woof.ts
@@ -2,13 +2,8 @@ import { expect } from 'chai';
 import { DeploymentManager } from '../../../../plugins/deployment_manager/DeploymentManager';
 import { migration } from '../../../../plugins/deployment_manager/Migration';
 import { exp, proposal } from '../../../../src/deploy';
-import { Contract, utils, constants } from 'ethers';
 
 const STREAM_RECEIVER = '0xd36025E1e77069aA991DC24f0E6287b4A35c89Ad';
-const DELEGATE_RECEIVER = '0xd2a79f263ec55dbc7b724ecc20fc7448d4795a0c';
-const FRANCHISER_FACTORY = '0xE696d89f4F378772f437F01FaaD70240abdf1854';
-
-let delegateBefore;
 
 export default migration('1742901113_fund_woof', {
   async prepare(deploymentManager: DeploymentManager) {
@@ -25,18 +20,15 @@ export default migration('1742901113_fund_woof', {
   enact: async (deploymentManager: DeploymentManager, _, { streamer }) => {
     const trace = deploymentManager.tracer();
     const {
-      timelock,
       governor,
       comptrollerV2,
-      COMP
     } = await deploymentManager.getContracts();
-    delegateBefore = await COMP.getCurrentVotes(DELEGATE_RECEIVER);
     const mainnetActions = [
       // 1. Withdraw the stream amount from Comptroller to streamer
       {
         contract: comptrollerV2,
         signature: '_grantComp(address,uint256)',
-        args: [streamer, exp(53475, 18)], // about $2.4m
+        args: [streamer, exp(53011, 18)], // about $2.4m
       },
       // 2. Initialize the streamer
       {
@@ -44,26 +36,8 @@ export default migration('1742901113_fund_woof', {
         signature: 'initialize()',
         calldata: '0x',
       },
-      // 3. Withdraw the delegate amount from Comptroller to the timelock
-      {
-        contract: comptrollerV2,
-        signature: '_grantComp(address,uint256)',
-        args:  [timelock.address, exp(50_000, 18)],
-      },
-      // 4. Approve the franchiser factory to spend the delegate amount
-      {
-        contract: COMP,
-        signature: 'approve(address,uint256)',
-        args:  [FRANCHISER_FACTORY, exp(50_000, 18)],
-      },
-      // 5. Create a Franchiser to delegate
-      {
-        target: FRANCHISER_FACTORY,
-        signature: 'fund(address,uint256)',
-        calldata:  utils.defaultAbiCoder.encode(['address', 'uint256'], [DELEGATE_RECEIVER, exp(50_000, 18)]),
-      },
     ];
-    const description = 'DESCRIPTION';
+    const description = '# WOOF! <> Compound Renewal 2025\n\n## Background\n\nSince 2023, WOOF! has worked with Compound to support the new and ongoing developments.\n\nFull details of the proposal can be found [here](https://www.comp.xyz/t/woof-compound-2025/6724/).\n\n## Proposal\n\nA proposal to renew WOOF!’s 12-month engagement with Compound to support core Compound Protocol developments, addressing the evolving needs of the community, including Compound v4, staked COMP, partial liquidations, and more, while also maintaining and supporting the existing infrastructure.\n\n## Compensation Structure\n\nThe service fee structure will be a fixed annual fee of 2,000,000 USD using the COMP spot price.\n\nCOMP will be streamed via a custom solution developed by WOOF! and audited by OpenZeppelin\n\n.Specification:\n\nAllocation: 53,011 COMP\n\nStreaming Duration: 1 year (continuously, second-by-second distribution)\n\nDistribution Currency: COMP, valued in USDC via Chainlink’s COMP/USDC price feed at the moment of claim.\n\nClaiming Cap: 2,000,000 USDC.\n\nSlippage Tolerance: 0.5% applied at the time of claim.\n\nClaim Safeguard: To protect the DAO from unfavorable claim conditions, any address may execute a claim on behalf of the stream if the last claim occurred more than 7 days ago.\n\nAnyone can return unclaimed COMP to the Comptroller after the end of the stream, plus a 10-day grace period.\n\nThe stream can be canceled early via an on-chain governance vote.\n\nWOOF! will be able to withdraw the vested funds at any time.\n\nMore details can be found in the forum post.';
     const txn = await deploymentManager.retry(async () =>
       trace(
         await governor.propose(...(await proposal(mainnetActions, description)))
@@ -83,27 +57,11 @@ export default migration('1742901113_fund_woof', {
   async verify(deploymentManager: DeploymentManager) {
     const {
       COMP,
-      timelock,
       streamer,
     } = await deploymentManager.getContracts();
 
-    const franchiserFactory = new Contract(
-      FRANCHISER_FACTORY,
-      [
-        'function fundMany(address[] calldata delegatees, uint256[] calldata amounts) external returns(address[] memory franchisers)',
-        'function getFranchiser(address,address) external view returns(address)',
-      ],
-      await deploymentManager.getSigner()
-    );
-    const franchiserAddress = await deploymentManager.retry(
-      async () => await franchiserFactory.getFranchiser(timelock.address, DELEGATE_RECEIVER)
-    );
-    expect(franchiserAddress).to.be.not.equal(constants.AddressZero);
-    expect(await COMP.balanceOf(franchiserAddress)).to.be.equal(exp(50_000, 18));
-    expect(await COMP.getCurrentVotes(DELEGATE_RECEIVER)).to.be.equal(BigInt(delegateBefore) + exp(50_000, 18));
-  
     expect(await streamer.startTimestamp()).to.be.gt(0);
-    expect(await COMP.balanceOf(streamer.address)).to.be.equal(exp(53475, 18));
+    expect(await COMP.balanceOf(streamer.address)).to.be.equal(exp(53011, 18));
   },
 });
 
