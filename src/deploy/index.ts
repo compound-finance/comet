7ee30cf41aa69a8098393ba0affeeb63c51cb99f
artemwoofsoftware <artem.m@woof.software>
2025-07-02 17:36:52 -0500

    fix: eigen whale

--- src/deploy/index.ts
+++ src/deploy/index.ts
@@ -108,6 +108,7 @@ export const WHALES = {
     '0x8bc93498b861fd98277c3b51d240e7E56E48F23c', // solvBTC.BBN whale
     '0xD5cf704dC17403343965b4F9cd4D7B5e9b20CC52', // solvBTC.BBN whale
     '0x3154Cf16ccdb4C6d922629664174b904d80F2C35', // cbETH whale
+    "0x5f556Cc5C294D7D3EfFaFFeb0B1195256a7A19D7", // EIGEN whale
   ],
   polygon: [
     '0xF977814e90dA44bFA03b6295A0616a897441aceC', // USDT whale
