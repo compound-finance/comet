bd8ec0f3825cee824440cff32de7a42d7e97798a
Mikhailo Shabodyash <misha.sh@woof.software>
2025-07-04 00:38:08 +0300

    fix: no network fix

--- deployments/mainnet/weth/migrations/1750958707_add_eigen_collateral.ts
+++ deployments/mainnet/weth/migrations/1750958707_add_eigen_collateral.ts
@@ -89,7 +89,7 @@ export default migration('1750958707_add_eigen_collateral', {
     trace(`Created proposal ${proposalId}.`);
   },
 
-  async enacted(deploymentManager: DeploymentManager): Promise<boolean> {
+  async enacted(): Promise<boolean> {
     return false;
   },
 
