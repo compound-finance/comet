e551cc90535bd3caf47f588b0c52392d9a91bf1f
Mikhailo Shabodyash <misha.sh@woof.software>
2025-07-05 16:28:46 +0300

    fix

--- plugins/deployment_manager/Deploy.ts
+++ plugins/deployment_manager/Deploy.ts
@@ -48,13 +48,11 @@ async function doDeploy<C extends Contract>(
   args: any[],
   opts: DeployOpts,
   src: string,
-  nonce?: number,
   gasPrice?: bigint
 ): Promise<C> {
   const trace = opts.trace ?? debug;
   trace(`Deploying ${name} with args ${stringifyJson(args)} via ${src}`);
   const contract = await factory.deploy(...args, {
-    nonce,
     gasPrice,
   });
   await contract.deployed();
@@ -110,8 +108,7 @@ export async function deploy<C extends Contract>(
   contractFile: string,
   deployArgs: any[],
   hre: HardhatRuntimeEnvironment,
-  deployOpts: DeployOpts,
-  nonce?: number
+  deployOpts: DeployOpts
 ): Promise<C> {
   const contractFileName = contractFile.split('/').reverse()[0];
   const contractName = contractFileName.replace('.sol', '');
@@ -121,7 +118,7 @@ export async function deploy<C extends Contract>(
   }
 
   const gasPrice = await hre.ethers.provider.getGasPrice();
-  const contract = await doDeploy(contractName, factory, deployArgs, deployOpts, 'artifact', nonce, gasPrice.toBigInt() * 12n / 10n);
+  const contract = await doDeploy(contractName, factory, deployArgs, deployOpts, 'artifact', gasPrice.toBigInt() * 12n / 10n);
   const buildFile = await getBuildFileFromArtifacts(contractFile, contractFileName);
   if (!buildFile.contract) {
     // This is just to make it clear which contract was deployed, when reading the build file
