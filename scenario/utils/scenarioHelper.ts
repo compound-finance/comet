9bd66e73f38624c67b989dbca1b9d6b8c28cf7f9
Mikhailo Shabodyash <misha.sh@woof.software>
2025-07-30 15:55:13 +0300

    fix

--- scenario/utils/scenarioHelper.ts
+++ scenario/utils/scenarioHelper.ts
@@ -96,7 +96,11 @@ export function getConfigForScenario(ctx: CometContext) {
   }
 
   if (ctx.world.base.network === 'arbitrum' && ctx.world.base.deployment === 'usdt') {
-    config.withdrawAsset = 3500;
+    config.withdrawAsset = 5000;
+    config.bulkerAsset = 10000;
+    config.bulkerAsset1 = 10000;
+    config.transferAsset = 10000;
+    config.transferAsset1 = 10000;
   }
 
   if (ctx.world.base.network === 'arbitrum' && ctx.world.base.deployment === 'usdc.e') {
