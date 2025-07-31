156272186341f9c5cab3acd9499c2b4e51f6d4de
Mikhailo Shabodyash <misha.sh@woof.software>
2025-07-31 16:34:43 +0300

    Update Network.ts

--- src/deploy/Network.ts
+++ src/deploy/Network.ts
@@ -125,7 +125,7 @@ export async function deployNetworkComet(
   const cometAdmin = await deploymentManager.deploy(
     'cometAdmin',
     'CometProxyAdmin.sol',
-    [],
+    [admin.address],
     maybeForce()
   );
 
