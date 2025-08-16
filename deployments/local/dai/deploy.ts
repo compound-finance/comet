3880e8ffcc3647d52c769319bff59b3937dab78b
Rodrigo Pintos <rpintos@spacedev.io>
2025-08-15 23:08:18 -0300

    feat: working but configurator was not properly set when deployed dai

--- deployments/local/dai/deploy.ts
+++ deployments/local/dai/deploy.ts
@@ -16,21 +16,6 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   // Deploy all Comet-related contracts
   const deployed = await deployComet(deploymentManager, deploySpec);
 
-  // Mint rewards tokens to the rewards contract
-  const { rewards } = deployed;
-  const COMP = await deploymentManager.getContractOrThrow('COMP');
-  const signer = await deploymentManager.getSigner();
-  const trace = deploymentManager.tracer();
-
-  await deploymentManager.idempotent(
-    async () => (await COMP.balanceOf(rewards.address)).eq(0),
-    async () => {
-      trace(`Sending some COMP to CometRewards`);
-      const amount = 1000000e8; // 1 million COMP tokens
-      trace(await COMP.connect(signer).transfer(rewards.address, amount));
-      trace(`COMP.balanceOf(${rewards.address}): ${await COMP.balanceOf(rewards.address)}`);
-    }
-  );
 
   return { ...deployed };
 }
