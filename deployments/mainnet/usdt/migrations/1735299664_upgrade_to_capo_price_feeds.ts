2d60955eb93fc095f97ffe79d5a4177b80ffed97
Artem Martiukhin <artem.m@woof.software>
2025-08-19 21:39:32 -0400

    feat: description

--- deployments/mainnet/usdt/migrations/1735299664_upgrade_to_capo_price_feeds.ts
+++ deployments/mainnet/usdt/migrations/1735299664_upgrade_to_capo_price_feeds.ts
@@ -168,7 +168,7 @@ export default migration('1735299664_upgrade_to_capo_price_feeds', {
       },
     ];
  
-    const description = '';
+    const description = '# Update wstETH, sFRAX, wBTC and WETH price feeds in cUSDTv3 on Mainnet with CAPO and OEV implementation.\n\n## Proposal summary\n\nThis proposal updates existing price feeds for wstETH, sFRAX, wBTC and WETH market on Mainnet implementing CAPO and OEV. CAPO is a price oracle adapter designed to support assets that grow gradually relative to a base asset - such as liquid staking tokens that accumulate yield over time. It provides a mechanism to track this expected growth while protecting downstream protocol from sudden or manipulated price spikes. OEV utililizes Chinlink SVR oracle solution that allows to recapture the non-toxic Maximal Extractable Value (MEV) derived from their use of Chainlink Price Feeds. Further detailed information can be found on the corresponding [proposal pull request](https://github.com/compound-finance/comet/pull/1015),  [forum discussion for CAPO](https://www.comp.xyz/t/woof-correlated-assets-price-oracle-capo/6245) and [forum discussion for OEV](https://www.comp.xyz/t/request-for-proposal-rfp-oracle-extractable-value-oev-solution-for-compound-protocol/6786).\n\n\n##';
  
     const txn = await deploymentManager.retry(async () =>
       trace(
