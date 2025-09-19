c561ff1978a570726aeffb8b56fc4ea8d07ea8a3
Mikhailo Shabodyash <misha.sh@woof.software>
2025-09-19 20:18:29 +0300

    fix: use specific block and add description

--- deployments/mainnet/usdt/migrations/1758288462_update_sfrax_snapshot.ts
+++ deployments/mainnet/usdt/migrations/1758288462_update_sfrax_snapshot.ts
@@ -8,6 +8,7 @@ const SFRAX_ADDRESS = '0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32';
 const SFRAX_PRICE_FEED_ADDRESS = '0x8C74B2811D2F1aD65517ADB5C65773c1E520ed2f';
 
 let oldSnapshot: any;
+const blockNumberToFetchFrom = 23397862;
 
 export default migration('1758288462_update_sfrax_snapshot', {
   async prepare() {
@@ -39,20 +40,18 @@ export default migration('1758288462_update_sfrax_snapshot', {
       throw new Error('Failed to fetch latest block');
     }
 
-    const blockNumberFromOneHourAgo = latestBlock.number - Math.floor(3600 / 11); // assuming average block time of 11 seconds
-    const blockFromOneHourAgo = await deploymentManager.hre.ethers.provider.getBlock(blockNumberFromOneHourAgo);
+    const blockToFetchFrom = await deploymentManager.hre.ethers.provider.getBlock(blockNumberToFetchFrom);
 
-    if (!blockFromOneHourAgo) {
-      throw new Error('Failed to fetch block from one hour ago');
+    if (!blockToFetchFrom) {
+      throw new Error('Failed to fetch block');
     }
-    expect(latestBlock.timestamp - blockFromOneHourAgo.timestamp).to.be.greaterThan(3600); // ensure it's at least one hour ago
     oldSnapshot = await sFraxToUsdPriceFeed.snapshotRatio();
 
-    const snapshotValue = await sFraxToFraxPriceFeed.convertToAssets(exp(1, 18), { blockTag: blockNumberFromOneHourAgo });
+    const snapshotValue = await sFraxToFraxPriceFeed.convertToAssets(exp(1, 18), { blockTag: blockToFetchFrom.number });
 
     const updateSnapshotCalldata = utils.defaultAbiCoder.encode(
       ['(uint256,uint48,uint32)'],
-      [[snapshotValue, blockFromOneHourAgo.timestamp, currentMaxYearlyRatioGrowthPercent]]
+      [[snapshotValue, blockToFetchFrom.timestamp, currentMaxYearlyRatioGrowthPercent]]
     );
 
     const {
@@ -68,7 +67,16 @@ export default migration('1758288462_update_sfrax_snapshot', {
       },
     ];
 
-    const description = 'DESCRIPTION';
+    const description = `# Update sFRAX CAPO snapshot value on Mainnet
+
+## Proposal summary
+
+WOOF! Proposes to update snapshot value for sFRAX CAPO price feed to the correct value, since it was incorrectly set up before. The new and correct value is taken directly from the sFRAX contract on the 23397862 block.
+
+## Proposed actions
+
+The first proposal action updates the snapshot value for CAPO price feed for sFRAX.
+`;
     const txn = await deploymentManager.retry(async () =>
       trace(
         await governor.propose(...(await proposal(mainnetActions, description)))
@@ -89,7 +97,7 @@ export default migration('1758288462_update_sfrax_snapshot', {
   async verify(deploymentManager: DeploymentManager) {
     expect(oldSnapshot).to.not.be.undefined;
     expect(oldSnapshot).to.equal('1213274845207191283');
-    const sFraxToFraxPriceFeed = new ethers.Contract(
+    const sFraxToFraxPriceFeed = new Contract(
       SFRAX_PRICE_FEED_ADDRESS,
       [
         'function maxYearlyRatioGrowthPercent() external view returns (uint32)',
