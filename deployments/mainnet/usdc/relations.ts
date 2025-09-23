806de0bde19d809e6c49538c8fd5a4f2e3bf50cb
Mikhailo Shabodyash <misha.sh@woof.software>
2025-09-23 20:31:14 +0300

    Update relations.ts

--- deployments/mainnet/usdc/relations.ts
+++ deployments/mainnet/usdc/relations.ts
@@ -11,14 +11,6 @@ export default {
       }
     }
   },
-  TransparentUpgradeableProxy: {
-    artifact: 'contracts/ERC20.sol:ERC20',
-    delegates: {
-      field: {
-        slot: '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-      }
-    }
-  },
   'AppProxyUpgradeable': {
     artifact: 'contracts/ERC20.sol:ERC20',
   },
