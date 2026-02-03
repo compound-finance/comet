fb276ce32517788134b5120178f503620296df15
Mikhailo Shabodyash <misha.sh@woof.software>
2026-02-03 13:57:31 +0200

    feat: add capo to proposal

--- deployments/arbitrum/weth/migrations/1766663003_change_price_feeds_to_svr.ts
+++ deployments/arbitrum/weth/migrations/1766663003_change_price_feeds_to_svr.ts
@@ -9,11 +9,23 @@ const USDC_TO_USD_SVR_PRICE_FEED_ADDRESS = '0xe4c892BE702F8e0771122CCaAA0E50BF96
 const USDT_TO_USD_SVR_PRICE_FEED_ADDRESS = '0x6AA147E11E423F529BEDAed75F3128D5fbE67939';
 const ETH_TO_USD_SVR_PRICE_FEED_ADDRESS = '0xe4dF63Bf89fD868A899F2422B030709FD79Be921';
 
+const WSTETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS = '0x311930889C61E141E15a61D11BE974D749390E7A';
+const ETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS = '0xA2699232B341881B1Ed85d91592b7c259E029aCf';
+const RSETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS = '0xA4F2e977CAb3177D61E2e7eAEcd257Bf09F2f915';
+const WETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS = '0x4F12633d511dC3049DE1ea923b7047fBeD0070D2';
+const RETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS = '0x60F2058379716A64a7A5d29219397e79bC552194';
+
 let newPriceFeedUSDCAddress: string;
 let newPriceFeedUSDTAddress: string;
 
 let oldUSDCPriceFeed: string;
 let oldUSDTPriceFeed: string;
+
+let oldWstETHPriceFeed: string;
+let oldEzETHPriceFeed: string;
+let oldRsETHPriceFeed: string;
+let oldWeETHPriceFeed: string;
+let oldRETHPriceFeed: string;
 
 export default migration('1766663003_change_price_feeds_to_svr', {
   async prepare(deploymentManager: DeploymentManager) {
@@ -66,7 +78,12 @@ export default migration('1766663003_change_price_feeds_to_svr', {
       cometAdmin,
       configurator,
       USDC,
-      'USD₮0':USDT
+      'USD₮0':USDT,
+      wstETH,
+      ezETH,
+      rsETH,
+      weETH,
+      rETH
     } = await deploymentManager.getContracts();
 
     const { governor, timelock, arbitrumInbox } = await govDeploymentManager.getContracts();
@@ -87,6 +104,46 @@ export default migration('1766663003_change_price_feeds_to_svr', {
       )
     );
 
+    const updateWstETHPriceFeedCalldata = await calldata(
+      configurator.populateTransaction.updateAssetPriceFeed(
+        comet.address,
+        wstETH.address,
+        WSTETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS
+      )
+    );
+
+    const updateEzETHPriceFeedCalldata = await calldata(
+      configurator.populateTransaction.updateAssetPriceFeed(
+        comet.address,
+        ezETH.address,
+        ETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS
+      )
+    );
+
+    const updateRsETHPriceFeedCalldata = await calldata(
+      configurator.populateTransaction.updateAssetPriceFeed(
+        comet.address,
+        rsETH.address,
+        RSETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS
+      )
+    );
+
+    const updateWeETHPriceFeedCalldata = await calldata(
+      configurator.populateTransaction.updateAssetPriceFeed(
+        comet.address,
+        weETH.address,
+        WETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS
+      )
+    );
+
+    const updateRETHPriceFeedCalldata = await calldata(
+      configurator.populateTransaction.updateAssetPriceFeed(
+        comet.address,
+        rETH.address,
+        RETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS
+      )
+    );
+
     const deployAndUpgradeToCalldata = await calldata(
       cometAdmin.populateTransaction.deployAndUpgradeTo(
         configurator.address,
@@ -100,10 +157,20 @@ export default migration('1766663003_change_price_feeds_to_svr', {
         [
           configurator.address,
           configurator.address,
+          configurator.address,
+          configurator.address,
+          configurator.address,
+          configurator.address,
+          configurator.address,
           cometAdmin.address
         ],
-        [0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
         [
+          'updateAssetPriceFeed(address,address,address)',
+          'updateAssetPriceFeed(address,address,address)',
+          'updateAssetPriceFeed(address,address,address)',
+          'updateAssetPriceFeed(address,address,address)',
+          'updateAssetPriceFeed(address,address,address)',
           'updateAssetPriceFeed(address,address,address)',
           'updateAssetPriceFeed(address,address,address)',
           'deployAndUpgradeTo(address,address)'
@@ -111,12 +178,22 @@ export default migration('1766663003_change_price_feeds_to_svr', {
         [
           updateUSDCPriceFeedCalldata,
           updateUSDTPriceFeedCalldata,
+          updateWstETHPriceFeedCalldata,
+          updateEzETHPriceFeedCalldata,
+          updateRsETHPriceFeedCalldata,
+          updateWeETHPriceFeedCalldata,
+          updateRETHPriceFeedCalldata,
           deployAndUpgradeToCalldata],
       ]
     );
 
     [,, oldUSDCPriceFeed] = await comet.getAssetInfoByAddress(USDC.address);
     [,, oldUSDTPriceFeed] = await comet.getAssetInfoByAddress(USDT.address);
+    [,, oldWstETHPriceFeed] = await comet.getAssetInfoByAddress(wstETH.address);
+    [,, oldEzETHPriceFeed] = await comet.getAssetInfoByAddress(ezETH.address);
+    [,, oldRsETHPriceFeed] = await comet.getAssetInfoByAddress(rsETH.address);
+    [,, oldWeETHPriceFeed] = await comet.getAssetInfoByAddress(weETH.address);
+    [,, oldRETHPriceFeed] = await comet.getAssetInfoByAddress(rETH.address);
     
     const createRetryableTicketGasParams = await estimateL2Transaction(
       {
@@ -147,13 +224,22 @@ export default migration('1766663003_change_price_feeds_to_svr', {
       },
     ];
 
-    const description = `# Update price feeds in cWETHv3 on Arbitrum with SVR price feeds.
+    const description = `# Update price feeds in cWETHv3 on Arbitrum with SVR and CAPO price feeds.
 
 ## Proposal summary
 
-This proposal updates existing price feeds for USDC and USDT assets on the WETH market on Arbitrum.
+This proposal updates existing price feeds for USDC, USDT, wstETH, ezETH, rsETH, weETH, and rETH assets on the WETH market on Arbitrum.
 
-Further detailed information can be found on the corresponding [proposal pull request](https://github.com/compound-finance/comet/pull/1080) and [forum discussion for SVR](https://www.comp.xyz/t/request-for-proposal-rfp-oracle-extractable-value-oev-solution-for-compound-protocol/6786).
+Further detailed information can be found on the corresponding [proposal pull request](https://github.com/compound-finance/comet/pull/1080), [forum discussion for CAPO](https://www.comp.xyz/t/woof-correlated-assets-price-oracle-capo/6245) and [forum discussion for SVR](https://www.comp.xyz/t/request-for-proposal-rfp-oracle-extractable-value-oev-solution-for-compound-protocol/6786).
+
+### CAPO summary
+
+CAPO is a price oracle adapter designed to support assets that grow gradually relative to a base asset - such as liquid staking tokens that accumulate yield over time. It provides a mechanism to track this expected growth while protecting downstream protocol from sudden or manipulated price spikes. wstETH, ezETH, rsETH, weETH, and rETH price feeds are updated to their CAPO implementations.
+
+### CAPO audit
+
+CAPO has been audited by [OpenZeppelin](https://www.comp.xyz/t/capo-price-feed-audit/6631, as well as the LST / LRT implementation [here](https://www.comp.xyz/t/capo-lst-lrt-audit/7118).
+
 
 ### SVR fee recipient
 
@@ -185,7 +271,12 @@ The first action updates USDC and USDT price feeds to the SVR implementation. Th
       comet,
       configurator,
       USDC,      
-      'USD₮0':USDT
+      'USD₮0':USDT,
+      wstETH,
+      ezETH,
+      rsETH,
+      weETH,
+      rETH,
     } = await deploymentManager.getContracts();
 
     // 1. USDC
@@ -216,5 +307,75 @@ The first action updates USDC and USDT price feeds to the SVR implementation. Th
     expect(USDTInCometInfo.priceFeed).to.eq(newPriceFeedUSDTAddress);
     expect(USDTInConfiguratorInfoUSDTComet.priceFeed).to.eq(newPriceFeedUSDTAddress);
     expect(await comet.getPrice(newPriceFeedUSDTAddress)).to.be.closeTo(await comet.getPrice(oldUSDTPriceFeed), 1e6);
-  },
+    
+    // 3. wstETH
+    const wstETHIndexInComet = await configurator.getAssetIndex(
+      comet.address,
+      wstETH.address
+    );
+    const wstETHInCometInfo = await comet.getAssetInfoByAddress(wstETH.address);
+    const wstETHInConfiguratorInfoUSDTComet = (
+      await configurator.getConfiguration(comet.address)
+    ).assetConfigs[wstETHIndexInComet];
+
+    expect(wstETHInCometInfo.priceFeed).to.eq(WSTETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(wstETHInConfiguratorInfoUSDTComet.priceFeed).to.eq(WSTETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(await comet.getPrice(WSTETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS)).to.be.closeTo(await comet.getPrice(oldWstETHPriceFeed), 1e6);
+
+    // 4. ezETH
+    const ezETHIndexInComet = await configurator.getAssetIndex(
+      comet.address,
+      ezETH.address
+    );
+    const ezETHInCometInfo = await comet.getAssetInfoByAddress(ezETH.address);
+    const ezETHInConfiguratorInfoUSDTComet = (
+      await configurator.getConfiguration(comet.address)
+    ).assetConfigs[ezETHIndexInComet];
+
+    expect(ezETHInCometInfo.priceFeed).to.eq(ETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(ezETHInConfiguratorInfoUSDTComet.priceFeed).to.eq(ETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(await comet.getPrice(ETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS)).to.be.closeTo(await comet.getPrice(oldEzETHPriceFeed), 1e6);
+
+    // 5. rsETH
+    const rsETHIndexInComet = await configurator.getAssetIndex(
+      comet.address,
+      rsETH.address
+    );
+    const rsETHInCometInfo = await comet.getAssetInfoByAddress(rsETH.address);
+    const rsETHInConfiguratorInfoUSDTComet = (
+      await configurator.getConfiguration(comet.address)
+    ).assetConfigs[rsETHIndexInComet];
+
+    expect(rsETHInCometInfo.priceFeed).to.eq(RSETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(rsETHInConfiguratorInfoUSDTComet.priceFeed).to.eq(RSETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(await comet.getPrice(RSETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS)).to.be.closeTo(await comet.getPrice(oldRsETHPriceFeed), 1e6);
+
+    // 6. weETH
+    const weETHIndexInComet = await configurator.getAssetIndex(
+      comet.address,
+      weETH.address
+    );
+    const weETHInCometInfo = await comet.getAssetInfoByAddress(weETH.address);
+    const weETHInConfiguratorInfoUSDTComet = (
+      await configurator.getConfiguration(comet.address)
+    ).assetConfigs[weETHIndexInComet];
+
+    expect(weETHInCometInfo.priceFeed).to.eq(WETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(weETHInConfiguratorInfoUSDTComet.priceFeed).to.eq(WETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(await comet.getPrice(WETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS)).to.be.closeTo(await comet.getPrice(oldWeETHPriceFeed), 1e6);
+
+    // 7. rETH
+    const rETHIndexInComet = await configurator.getAssetIndex(
+      comet.address,
+      rETH.address
+    );
+    const rETHInCometInfo = await comet.getAssetInfoByAddress(rETH.address);
+    const rETHInConfiguratorInfoUSDTComet = (
+      await configurator.getConfiguration(comet.address)
+    ).assetConfigs[rETHIndexInComet];
+
+    expect(rETHInCometInfo.priceFeed).to.eq(RETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(rETHInConfiguratorInfoUSDTComet.priceFeed).to.eq(RETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS);
+    expect(await comet.getPrice(RETH_TO_ETH_CAPO_PRICE_FEED_ADDRESS)).to.be.closeTo(await comet.getPrice(oldRETHPriceFeed), 1e6);
+  }
 });
